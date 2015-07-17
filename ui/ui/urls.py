dad15fbfa5a12dad86181c6250a04a8b2cdf4ec4
c.brett <c.brett@ocado.com>
2015-07-17 14:54:19 +0100

    added account links

--- ui/ui/urls.py
+++ ui/ui/urls.py
@@ -5,7 +5,7 @@ from players import urls as players_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^', include(players_urls)),
 ]
