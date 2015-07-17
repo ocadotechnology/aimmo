3a3e49ed14c797ea56c829f27e8b80f5fb7ddcaf
OliverCartz <oliver.cartz@ocado.com>
2015-07-17 13:35:33 +0100

    Add avatar to avatars map if it isn't there when code is changed

--- simulation/avatar_manager.py
+++ simulation/avatar_manager.py
@@ -3,6 +3,9 @@ class AvatarManager(object):
         self.avatarsById = {p.id: p for p in initial_avatars}
 
     def player_changed_code(playerId, code):
-        avatar = self.avatarsById[playerId]
+        avatar = self.avatarsById.get(playerId)
+        if avatar is None:
+            avatar = AvatarRunner(Location(0, 0), code, playerId)
+            self.avatarsById[playerId] = avatar
         avatar.set_code(code)
 
