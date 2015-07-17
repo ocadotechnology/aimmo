527166c21684028686301a818b0011259697f2e0
dsoid <renotekk+githubdsoid@gmail.com>
2015-07-17 13:39:54 +0100

    Updating AvatarRunner

--- simulation/test/test_turn_manager.py
+++ simulation/test/test_turn_manager.py
@@ -3,36 +3,43 @@ import sys
 sys.path.append(os.path.abspath('.'))
 
 import unittest
+from simulation.level import Level
 
 from simulation.location import Location
 from simulation.avatar_manager import AvatarManager
 from simulation.turn_manager import TurnManager
 from simulation.world_map import WorldMap
 from simulation.world_state import WorldState
-from simulation.avatar import AvatarRunner
+from simulation.avatar import AvatarRunner, AvatarAppearance
 from simulation.test.dummy_player import dummy
 
 class TestTurnManager(unittest.TestCase):
+    def construct_default_avatar_appearance(self):
+        return AvatarAppearance("#000", "#ddd", "#777", "#fff")
+
+    def construct_default_level(self):
+        return Level(15, 15, 0.1, 0.1)
+
     def construct_turn_manager(self, *avatars):
         self.avatar_manager = AvatarManager(avatars)
-        self.world_state = WorldState(WorldMap(), self.avatar_manager)
+        self.world_state = WorldState(WorldMap(self.construct_default_level()), self.avatar_manager)
         self.turn_manager = TurnManager(self.world_state)
 
     def test_run_turn(self):
-        avatar = AvatarRunner(Location(0, 0), dummy, 0)
+        avatar = AvatarRunner(Location(0, 0), dummy, 0, self.construct_default_avatar_appearance())
         self.construct_turn_manager(avatar)
         self.turn_manager.run_turn()
         self.assertEqual(avatar.location, Location(1, 0))
 
     def test_run_several_turns(self):
-        avatar = AvatarRunner(Location(0, 0), dummy, 0)
+        avatar = AvatarRunner(Location(0, 0), dummy, 0, self.construct_default_avatar_appearance())
         self.construct_turn_manager(avatar)
         [self.turn_manager.run_turn() for _ in xrange(5)]
         self.assertEqual(avatar.location, Location(5, 0))
 
     def test_run_several_turns_and_avatars(self):
-        avatar1 = AvatarRunner(Location(0, 0), dummy, 0)
-        avatar2 = AvatarRunner(Location(0, 1), dummy, 1)
+        avatar1 = AvatarRunner(Location(0, 0), dummy, 0, self.construct_default_avatar_appearance())
+        avatar2 = AvatarRunner(Location(0, 1), dummy, 1, self.construct_default_avatar_appearance())
         self.construct_turn_manager(avatar1, avatar2)
         [self.turn_manager.run_turn() for _ in xrange(5)]
         self.assertEqual(avatar1.location, Location(5, 0))
