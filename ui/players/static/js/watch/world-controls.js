94ff06c843f3deca245f9c05375b6dca547ac004
georgesimms <simms.george.gs@googlemail.com>
2015-10-01 21:36:23 +0100

    Make code JSLint-compliant.

--- ui/players/static/js/watch/world-controls.js
+++ ui/players/static/js/watch/world-controls.js
@@ -1,60 +1,63 @@
 // World Manipulation
-const CONTROLS = Object.create({
-    init: function (world, viewer) {
-        this.world = world;
-        this.viewer = viewer;
-    },
 
-    initialiseWorld: function (width, height, worldLayout) {
-        // TODO is width/height ever incosnistent with layout?!
-        this.viewer.reDrawWorldLayout({width: width, height: height, layout: worldLayout});
-        return height;
-    },
-    setState: function (players, scoreLocations, pickupLocations, height, drawnElements) {
-        return this.viewer.reDrawState(drawnElements, 
-            {players: players, pickupLocations: pickupLocations, height: height});
-    }
-});
+'use strict';
+var Promise = window.Promise,
+    $ = window.$,
+    VIEWER = window.VIEWER,
+    APPEARANCE = window.APPEARANCE,
+    CONTROLS = Object.create({
+        init: function (viewer) {
+            this.viewer = viewer;
+        },
+
+        initialiseWorld: function (width, height, worldLayout) {
+            // TODO is width/height ever incosnistent with layout?!
+            this.viewer.reDrawWorldLayout({width: width, height: height, layout: worldLayout});
+            return height;
+        },
+        setState: function (players, pickupLocations, height, drawnElements) {
+            return this.viewer.reDrawState(drawnElements,
+                {players: players, pickupLocations: pickupLocations, height: height});
+        }
+    });
 
 function jsonAsync(url, timeout) {
-    return new Promise(function(res, rej) {
-        setTimeout(function() {
+    return new Promise(function (res, rej) {
+        setTimeout(function () {
             $.ajax(url, {
-                success: function(data) {
+                success: function (data) {
                     res(data);
                 },
-                error: function(jqXHR, textStatus, errorThrown) {
+                error: function (jqXHR, textStatus, errorThrown) {
                     rej({jqXHR: jqXHR, textStatus: textStatus, errorThrown: errorThrown});
                 }
             });
         }, timeout);
     });
-};
+}
 
 function refreshState(eventualState) {
     // TODO should be 500ms timeout for error.
-    var newPromise = Promise.all([eventualState, jsonAsync("/api/watch/state", 200)]).then(function(arr) {
-        var data = arr[1];
-        var height = data.map_changed ?
-            CONTROLS.initialiseWorld(data.width, data.height, data.layout) :
-            arr[0].height;
+    var newPromise = Promise.all([eventualState, jsonAsync("/api/watch/state", 200)]).then(function (arr) {
+        var data = arr[1],
+            height = data.map_changed ?
+                    CONTROLS.initialiseWorld(data.width, data.height, data.layout) :
+                    arr[0].height;
         return CONTROLS.setState(data.players,
-            data.score_locations,
             data.pickup_locations,
             height,
             arr[0].drawnElements);
-    }).catch(function(er) {
+    }).catch(function (er) {
         console.error(er);
         return eventualState;
     });
-    setTimeout(function() {refreshState(newPromise)}, 0);
+    setTimeout(function () {refreshState(newPromise); }, 0);
 
-};
+}
 
-$(function(){
-    var world = {};
+$(function () {
     var eventualState = Promise.resolve({drawnElements: {players: [], pickups: []}});
-    VIEWER.init(document.getElementById("watch-world-canvas"), world, APPEARANCE);
-    CONTROLS.init(world, VIEWER);
+    VIEWER.init(document.getElementById("watch-world-canvas"));
+    CONTROLS.init(VIEWER);
     refreshState(eventualState);
 });
--- ui/players/static/js/watch/world-viewer.js
+++ ui/players/static/js/watch/world-viewer.js
@@ -1,135 +1,136 @@
 // All calls to paper.* should call invertY to get from simulation coordinate system into visualisation coordinate system, then scale up by appearance.cellSize
-
-const APPEARANCE = Object.create({
-    cellSize: 50,
-    worldColours: {
-        "GRASS": "#efe",
-        "WALL": "#777",
-        "SCORE": "#fbb"
-    }
-});
-
-const VIEWER = Object.create({
-    init: function(canvasDomElement, world, appearance) {
-        this.world = world;
-        this.appearance = appearance;
-        this.paper = Raphael(canvasDomElement);
-    },
-
-    drawnElements: {
-        players: [],
-        pickups: [],
-    },
-
-    invertY: function(height, y) {
-        if(y == undefined) {
-            debugger;
+'use strict';
+var raphael = window.Raphael, // TODO not a constructor?
+    APPEARANCE = Object.create({
+        cellSize: 50,
+        worldColours: {
+            GRASS: "#efe",
+            WALL: "#777",
+            SCORE: "#fbb"
         }
-        return height - y - 1;
-    },
+    }),
+    VIEWER = Object.create({
+        init: function (canvasDomElement, world, appearance) {
+            this.world = world;
+            this.appearance = appearance;
+            this.paper = raphael(canvasDomElement);
+        },
 
-    reDrawWorldLayout: function(world) {
-        var self = this;
-        self.paper.clear();
-        self.paper.setViewBox(0, 0, world.width * self.appearance.cellSize, world.height * self.appearance.cellSize, true);
-        for (x = 0; x < world.width; x++) {
-            for (y = 0; y < world.height; y++) {
-                currentCellValue = world.layout[x][y];
+        drawnElements: {
+            players: [],
+            pickups: [],
+        },
 
-                var square = self.paper.rect(x * self.appearance.cellSize,
-                    self.invertY(world.height, y) * self.appearance.cellSize,
-                    self.appearance.cellSize,
-                    self.appearance.cellSize);
+        invertY: function (height, y) {
+            return height - y - 1;
+        },
 
-                square.attr("fill", self.appearance.worldColours[currentCellValue]);
-                square.attr("stroke", "#000");
+        reDrawWorldLayout: function (world) {
+            var self = this,
+                x,
+                y,
+                currentCellValue,
+                square;
+            self.paper.clear();
+            self.paper.setViewBox(0, 0, world.width * self.appearance.cellSize, world.height * self.appearance.cellSize, true);
+            for (x = 0; x < world.width; x += 1) {
+                for (y = 0; y < world.height; y += 1) {
+                    currentCellValue = world.layout[x][y];
 
-                self.paper.text((x + 0.5) * self.appearance.cellSize, (self.invertY(world.height, y) + 0.5) * self.appearance.cellSize, x + ', ' + y)
+                    square = self.paper.rect(x * self.appearance.cellSize,
+                        self.invertY(world.height, y) * self.appearance.cellSize,
+                        self.appearance.cellSize,
+                        self.appearance.cellSize);
+
+                    square.attr("fill", self.appearance.worldColours[currentCellValue]);
+                    square.attr("stroke", "#000");
+
+                    self.paper.text((x + 0.5) * self.appearance.cellSize,
+                        (self.invertY(world.height, y) + 0.5) * self.appearance.cellSize, x + ', ' + y);
+                }
             }
+        },
+
+        constructNewPlayerElement: function (playerData, height) {
+            var playerX = (0.5 + playerData.x) * this.appearance.cellSize,
+                playerY = (0.5 + this.invertY(height, playerData.y)) * this.appearance.cellSize,
+                playerRadius = this.appearance.cellSize * 0.5 * 0.75,
+                playerHeadRadius = playerRadius * 0.6,
+                playerEyeRadius = playerRadius * 0.2,
+                playerBody = this.paper.circle(playerX, playerY, playerRadius),
+                playerEyeLeft = this.paper.circle(
+                    playerX + playerHeadRadius * Math.cos(playerData.rotation - 1),
+                    playerY + playerHeadRadius * Math.sin(playerData.rotation - 1),
+                    playerEyeRadius
+                ),
+                playerEyeRight = this.paper.circle(
+                    playerX + playerHeadRadius * Math.cos(playerData.rotation + 1),
+                    playerY + playerHeadRadius * Math.sin(playerData.rotation + 1),
+                    playerEyeRadius
+                ),
+                playerTextAbove = this.paper.text(playerX, playerY - 20, 'Score: ' + playerData.score),
+                playerTextBelow = this.paper.text(playerX, playerY + 20, playerData.health + 'hp, (' + playerData.x + ', ' + playerData.y + ')'),
+                player = this.paper.set();
+
+            playerBody.attr("fill", playerData.colours.bodyFill);
+            playerBody.attr("stroke", playerData.colours.bodyStroke);
+
+            playerEyeLeft.attr("fill", playerData.colours.eyeFill);
+            playerEyeLeft.attr("stroke", playerData.colours.eyeStroke);
+
+            playerEyeRight.attr("fill", playerData.colours.eyeFill);
+            playerEyeRight.attr("stroke", playerData.colours.eyeStroke);
+
+            player.push(
+                playerBody,
+                playerEyeLeft,
+                playerEyeRight,
+                playerTextAbove,
+                playerTextBelow
+            );
+            return player;
+        },
+
+        clearDrawnElements: function (elements) {
+            elements.forEach(function (elementToRemove) {
+                elementToRemove.remove();
+            });
+        },
+
+        reDrawPlayers: function (players, height) {
+            var self = this;
+            return Object.keys(players).map(function (playerKey) {
+                var playerData = players[playerKey];
+                return self.constructNewPlayerElement(playerData, height);
+            });
+        },
+
+        reDrawPickups: function (pickupLocations, height) {
+            var self = this;
+            return pickupLocations.map(function (pickupLocation) {
+                var x = (0.5 + pickupLocation[0]) * self.appearance.cellSize,
+                    y = (0.5 + self.invertY(height, pickupLocation[1])) * self.appearance.cellSize,
+                    radius = self.appearance.cellSize * 0.5 * 0.75,
+                    circle = self.paper.circle(x, y, radius),
+                    crossX = self.paper.rect(x - 10, y - 3, 20, 6).attr({fill: '#FF0000', stroke: '#FF0000'}),
+                    crossY = self.paper.rect(x - 3, y - 10, 6, 20).attr({fill: '#FF0000', stroke: '#FF0000'}),
+                    pickup = self.paper.set();
+
+                circle.attr("fill", '#FFFFFF');
+                pickup.push(circle, crossX, crossY);
+                return pickup;
+            });
+        },
+
+        reDrawState: function (drawnElements, world) {
+            this.clearDrawnElements(drawnElements.pickups);
+            this.clearDrawnElements(drawnElements.players);
+            return {
+                drawnElements: {
+                    pickups: this.reDrawPickups(world.pickupLocations, world.height),
+                    players: this.reDrawPlayers(world.players, world.height),
+                },
+                height: world.height,
+            };
         }
-    },
-
-    constructNewPlayerElement: function(playerData, height) {
-        const playerX = (0.5 + playerData.x) * this.appearance.cellSize;
-        const playerY = (0.5 + this.invertY(height, playerData.y)) * this.appearance.cellSize;
-        const playerRadius = this.appearance.cellSize * 0.5 * 0.75;
-        const playerHeadRadius = playerRadius * 0.6;
-        const playerEyeRadius = playerRadius * 0.2;
-
-        var playerBody = this.paper.circle(playerX, playerY, playerRadius);
-
-        playerBody.attr("fill", playerData.colours.bodyFill);
-        playerBody.attr("stroke", playerData.colours.bodyStroke);
-        var playerEyeLeft = this.paper.circle(
-            playerX + playerHeadRadius * Math.cos(playerData.rotation - 1),
-            playerY + playerHeadRadius * Math.sin(playerData.rotation - 1),
-            playerEyeRadius
-        );
-
-        playerEyeLeft.attr("fill", playerData.colours.eyeFill);
-        playerEyeLeft.attr("stroke", playerData.colours.eyeStroke);
-        var playerEyeRight = this.paper.circle(
-            playerX + playerHeadRadius * Math.cos(playerData.rotation + 1),
-            playerY + playerHeadRadius * Math.sin(playerData.rotation + 1),
-            playerEyeRadius
-        );
-
-        playerEyeRight.attr("fill", playerData.colours.eyeFill);
-        playerEyeRight.attr("stroke", playerData.colours.eyeStroke);
-
-        var playerTextAbove = this.paper.text(playerX, playerY - 20, 'Score: ' + playerData.score);
-        var playerTextBelow = this.paper.text(playerX, playerY + 20, playerData.health + 'hp, (' + playerData.x + ', ' + playerData.y + ')');
-
-        var player = this.paper.set();
-        player.push(
-            playerBody,
-            playerEyeLeft,
-            playerEyeRight,
-            playerTextAbove,
-            playerTextBelow
-        );
-        return player;
-    },
-
-    clearDrawnElements: function(elements) {
-        elements.forEach(function(elementToRemove) {
-            elementToRemove.remove();
-        });
-    },
-
-    reDrawPlayers: function(players, height) {
-        var self = this;
-        return Object.keys(players).map(function(playerKey) {
-            var playerData = players[playerKey];
-            return self.constructNewPlayerElement(playerData, height);
-        });
-    },
-
-    reDrawPickups: function(pickupLocations, height) {
-        var self = this;
-        return pickupLocations.map(function(pickupLocation, i) {
-            var x = (0.5 + pickupLocation[0]) * self.appearance.cellSize;
-            var y = (0.5 + self.invertY(height, pickupLocation[1])) * self.appearance.cellSize;
-            var radius = self.appearance.cellSize * 0.5 * 0.75;
-            var circle = self.paper.circle(x, y, radius);
-            circle.attr("fill", '#FFFFFF');
-            var crossX = self.paper.rect(x - 10, y - 3, 20, 6).attr({fill: '#FF0000', stroke: '#FF0000'});
-            var crossY = self.paper.rect(x - 3, y - 10, 6, 20).attr({fill: '#FF0000', stroke: '#FF0000'});
-            var pickup = self.paper.set();
-            pickup.push(circle, crossX, crossY);
-            return pickup;
-        });
-    },
-
-    reDrawState: function(drawnElements, world) {
-        this.clearDrawnElements(drawnElements.pickups);
-        this.clearDrawnElements(drawnElements.players);
-        return {
-            drawnElements: {
-                pickups: this.reDrawPickups(world.pickupLocations, world.height),
-                players: this.reDrawPlayers(world.players, world.height),
-            },
-            height: world.height,
-        };
-    }
-});
+    });
