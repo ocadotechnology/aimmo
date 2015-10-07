1012b59c613ce90f238e49f13b19edd3626a0441
George Simms <george.simms@MB17685.local>
2015-10-07 19:37:14 +0100

    switch to classes

--- ui/players/static/js/watch/world-controls.js
+++ ui/players/static/js/watch/world-controls.js
@@ -17,51 +17,52 @@
         });
     }
 
-    window.CONTROLS = Object.create({
-        init: function (viewer) {
-            this.viewer = viewer;
-        },
+    window.Controls = function (viewer) {
+        this.viewer = viewer;
+    };
 
-        initialiseWorld: function (width, height, worldLayout) {
-            // TODO is width/height ever incosnistent with layout?!
-            this.viewer.reDrawWorldLayout({width: width, height: height, layout: worldLayout});
-            return height;
-        },
-        setState: function (players, pickupLocations, height, drawnElements) {
-            return this.viewer.reDrawState(drawnElements,
-                {players: players, pickupLocations: pickupLocations, height: height});
-        }
-    });
+    window.Controls.prototype = {};
+
+    window.Controls.prototype.initialiseWorld = function (width, height, worldLayout) {
+       // TODO is width/height ever incosnistent with layout?!
+        this.viewer.reDrawWorldLayout({width: width, height: height, layout: worldLayout});
+        return height;
+    };
+
+    window.Controls.prototype.setState = function (players, pickupLocations, height, drawnElements) {
+        return this.viewer.reDrawState(drawnElements,
+            {players: players, pickupLocations: pickupLocations, height: height});
+    };
 
 
-    window.refreshState = function (eventualState) {
+    window.refreshState = function (controls, eventualState) {
         var newPromise = Promise.all([eventualState, jsonAsync("/api/watch/state")]).then(function (arr) {
             var data = arr[1],
                 height = data.map_changed ?
-                        CONTROLS.initialiseWorld(data.width, data.height, data.layout) :
+                        controls.initialiseWorld(data.width, data.height, data.layout) :
                         arr[0].height,
-                output = CONTROLS.setState(data.players,
+                output = controls.setState(data.players,
                     data.pickup_locations,
                     height,
                     arr[0].drawnElements);
-            setTimeout(function () { window.refreshState(output); }, 200);
+            setTimeout(function () { window.refreshState(controls, output); }, 200);
         }).catch(function (er) {
             console.error(er);
-            setTimeout(function () { window.refreshState(eventualState); }, 500);
+            setTimeout(function () { window.refreshState(controls, eventualState); }, 500);
         });
     };
 
 }());
 
 window.$(function () {
-    var VIEWER = window.VIEWER,
+    var Viewer = window.Viewer,
         APPEARANCE = window.APPEARANCE,
-        CONTROLS = window.CONTROLS,
+        Controls = window.Controls,
         Promise = window.Promise,
         refreshState = window.refreshState,
-        eventualState = Promise.resolve({drawnElements: {players: [], pickups: []}});
+        eventualState = Promise.resolve({drawnElements: {players: [], pickups: []}}),
+        viewer = new Viewer(document.getElementById("watch-world-canvas"), APPEARANCE),
+        controls = new Controls(viewer);
 
-    VIEWER.init(document.getElementById("watch-world-canvas"), APPEARANCE);
-    CONTROLS.init(VIEWER);
-    refreshState(eventualState);
+    refreshState(controls, eventualState);
 });
--- ui/players/static/js/watch/world-viewer.js
+++ ui/players/static/js/watch/world-viewer.js
@@ -4,7 +4,7 @@
     var raphael = window.Raphael; // TODO not a constructor?
 
     // TODO can CSS help eleminate these magic numbers?
-    window.APPEARANCE = Object.create({
+    window.APPEARANCE = {
         cellSize: 50,
         crossLengthScaleFactor: 0.4,
         crossWidthScaleFactor: 0.12,
@@ -33,162 +33,163 @@
                "#00752A"
             ]
         }
-    });
-    window.VIEWER = Object.create({
-        init: function (canvasDomElement, appearance) {
-            this.appearance = appearance;
-            this.paper = raphael(canvasDomElement);
-        },
+    };
 
-        invertY: function (height, y) {
-            return height - y - 1;
-        },
+    window.Viewer = function (canvasDomElement, appearance) {
+        this.appearance = appearance;
+        this.paper = raphael(canvasDomElement);
+    };
 
-        reDrawWorldLayout: function (world) {
-            var self = this;
-            self.paper.clear();
-            self.paper.setViewBox(0, 0, world.width * self.appearance.cellSize, world.height * self.appearance.cellSize, true);
-            world.layout.forEach(function (row, x) {
-                row.forEach(function (currentCellValue, y) {
+    window.Viewer.prototype = {};
 
-                    var square = self.paper.rect(x * self.appearance.cellSize,
-                        self.invertY(world.height, y) * self.appearance.cellSize,
-                        self.appearance.cellSize,
-                        self.appearance.cellSize);
+    window.Viewer.prototype.invertY = function (height, y) {
+        return height - y - 1;
+    };
 
-                    square.attr("fill", self.appearance.worldColours[currentCellValue]);
-                    square.attr("stroke", self.appearance.worldColours.CELL_STROKE);
+    window.Viewer.prototype.reDrawWorldLayout = function (world) {
+        var self = this;
+        self.paper.clear();
+        self.paper.setViewBox(0, 0, world.width * self.appearance.cellSize, world.height * self.appearance.cellSize, true);
+        world.layout.forEach(function (row, x) {
+            row.forEach(function (currentCellValue, y) {
 
-                    self.paper.text((x + 0.5) * self.appearance.cellSize,
-                        (self.invertY(world.height, y) + 0.5) * self.appearance.cellSize, x + ', ' + y);
-                });
+                var square = self.paper.rect(x * self.appearance.cellSize,
+                    self.invertY(world.height, y) * self.appearance.cellSize,
+                    self.appearance.cellSize,
+                    self.appearance.cellSize);
+
+                square.attr("fill", self.appearance.worldColours[currentCellValue]);
+                square.attr("stroke", self.appearance.worldColours.CELL_STROKE);
+
+                self.paper.text((x + 0.5) * self.appearance.cellSize,
+                    (self.invertY(world.height, y) + 0.5) * self.appearance.cellSize, x + ', ' + y);
             });
-        },
+        });
+    };
 
-        constructNewPlayerElement: function (playerData, height) {
-            function cycle(arr, i) {
-                var index = i % arr.length;
-                return arr[index];
-            }
-            var self = this,
-                playerX = (0.5 + playerData.x) * this.appearance.cellSize,
-                playerY = (0.5 + this.invertY(height, playerData.y)) * this.appearance.cellSize,
-                playerRadius = this.appearance.cellSize * this.appearance.playerScaleFactor,
-                playerHeadRadius = playerRadius * this.appearance.playerHeadScaleFactor,
-                playerEyeRadius = playerRadius * this.appearance.playerEyeScaleFactor,
-                playerBody = this.paper.circle(playerX, playerY, playerRadius),
-                directionToRotationRadians = {
-                    SOUTH: Math.PI / 2,
-                    EAST: 0,
-                    NORTH: -Math.PI / 2,
-                    WEST: Math.PI,
-
-                },
-                rotationRadians = directionToRotationRadians[playerData.lastMove],
-                leftEyeAngle = rotationRadians - self.appearance.playerEyeOffsetRadians,
-                rightEyeAngle = rotationRadians + self.appearance.playerEyeOffsetRadians,
-                playerEyeLeft = this.paper.circle(
-                    playerX + playerHeadRadius * Math.cos(leftEyeAngle),
-                    playerY + playerHeadRadius * Math.sin(leftEyeAngle),
-                    playerEyeRadius
-                ),
-                playerEyeRight = this.paper.circle(
-                    playerX + playerHeadRadius * Math.cos(rightEyeAngle),
-                    playerY + playerHeadRadius * Math.sin(rightEyeAngle),
-                    playerEyeRadius
-                ),
-                playerTextAbove = this.paper.text(
-                    playerX,
-                    playerY - self.appearance.playerTextOffsetScaleFactor * self.appearance.cellSize,
-                    'Score: ' + playerData.score
-                ),
-                playerTextBelow = this.paper.text(
-                    playerX,
-                    playerY + self.appearance.playerTextOffsetScaleFactor * self.appearance.cellSize,
-                    playerData.health + 'hp, (' + playerData.x + ', ' + playerData.y + ')'
-                ),
-                player = this.paper.set(),
-                playerColor = cycle(this.appearance.worldColours.PLAYERS, playerData.id);
-
-            playerBody.attr("fill", playerColor);
-            playerBody.attr("stroke", this.appearance.BODY_STROKE);
-
-            [playerEyeLeft, playerEyeRight].forEach(function(eye) {
-                eye.attr("fill", self.appearance.worldColours.EYE_FILL);
-                eye.attr("stroke", self.appearance.worldColours.EYE_STROKE);
-            });
-
-            player.push(
-                playerBody,
-                playerEyeLeft,
-                playerEyeRight,
-                playerTextAbove,
-                playerTextBelow
-            );
-            return player;
-        },
-
-        clearDrawnElements: function (elements) {
-            elements.forEach(function (elementToRemove) {
-                elementToRemove.remove();
-            });
-        },
-
-        reDrawPlayers: function (players, height) {
-            var self = this;
-            // TODO how to know which color is which player?
-            // Should indicate some sort of a key.
-            // Also how does the player know their own ID?
-            // It doesn't match their login name.
-            return Object.keys(players).map(function (playerKey, i) {
-                var playerData = players[playerKey];
-                playerData.index = i;
-                return self.constructNewPlayerElement(playerData, height);
-            });
-        },
-
-        reDrawPickups: function (pickupLocations, height) {
-            var self = this;
-            return pickupLocations.map(function (pickupLocation) {
-                var x = (0.5 + pickupLocation[0]) * self.appearance.cellSize,
-                    y = (0.5 + self.invertY(height, pickupLocation[1])) * self.appearance.cellSize,
-                    radius = self.appearance.cellSize * self.appearance.pickupScaleFactor,
-                    circle = self.paper.circle(x, y, radius),
-                    crossX = self.paper.rect(
-                        x - (self.appearance.crossLengthScaleFactor * self.appearance.cellSize / 2),
-                        y - (self.appearance.crossWidthScaleFactor * self.appearance.cellSize / 2),
-                        self.appearance.crossLengthScaleFactor * self.appearance.cellSize,
-                        self.appearance.crossWidthScaleFactor * self.appearance.cellSize).attr({
-
-                        fill: self.appearance.worldColours.HEALTH_CROSS,
-                        stroke: self.appearance.worldColours.HEALTH_CROSS
-                    }),
-                    crossY = self.paper.rect(
-                        x - (self.appearance.crossWidthScaleFactor * self.appearance.cellSize / 2),
-                        y - (self.appearance.crossLengthScaleFactor * self.appearance.cellSize / 2),
-                        self.appearance.crossWidthScaleFactor * self.appearance.cellSize,
-                        self.appearance.crossLengthScaleFactor * self.appearance.cellSize).attr({
-                        fill: self.appearance.worldColours.HEALTH_CROSS,
-                        stroke: self.appearance.worldColours.HEALTH_CROSS,
-                    }),
-                    pickup = self.paper.set();
-
-                circle.attr("fill", self.appearance.worldColours.HEALTH_BACKGROUND);
-                pickup.push(circle, crossX, crossY);
-                return pickup;
-            });
-        },
-
-        reDrawState: function (drawnElements, world) {
-            this.clearDrawnElements(drawnElements.pickups);
-            this.clearDrawnElements(drawnElements.players);
-            return {
-                drawnElements: {
-                    pickups: this.reDrawPickups(world.pickupLocations, world.height),
-                    players: this.reDrawPlayers(world.players, world.height),
-                },
-                height: world.height,
-            };
+    window.Viewer.prototype.constructNewPlayerElement = function (playerData, height) {
+        function cycle(arr, i) {
+            var index = i % arr.length;
+            return arr[index];
         }
-    });
+        var self = this,
+            playerX = (0.5 + playerData.x) * this.appearance.cellSize,
+            playerY = (0.5 + this.invertY(height, playerData.y)) * this.appearance.cellSize,
+            playerRadius = this.appearance.cellSize * this.appearance.playerScaleFactor,
+            playerHeadRadius = playerRadius * this.appearance.playerHeadScaleFactor,
+            playerEyeRadius = playerRadius * this.appearance.playerEyeScaleFactor,
+            playerBody = this.paper.circle(playerX, playerY, playerRadius),
+            directionToRotationRadians = {
+                SOUTH: Math.PI / 2,
+                EAST: 0,
+                NORTH: -Math.PI / 2,
+                WEST: Math.PI,
+
+            },
+            rotationRadians = directionToRotationRadians[playerData.lastMove],
+            leftEyeAngle = rotationRadians - self.appearance.playerEyeOffsetRadians,
+            rightEyeAngle = rotationRadians + self.appearance.playerEyeOffsetRadians,
+            playerEyeLeft = this.paper.circle(
+                playerX + playerHeadRadius * Math.cos(leftEyeAngle),
+                playerY + playerHeadRadius * Math.sin(leftEyeAngle),
+                playerEyeRadius
+            ),
+            playerEyeRight = this.paper.circle(
+                playerX + playerHeadRadius * Math.cos(rightEyeAngle),
+                playerY + playerHeadRadius * Math.sin(rightEyeAngle),
+                playerEyeRadius
+            ),
+            playerTextAbove = this.paper.text(
+                playerX,
+                playerY - self.appearance.playerTextOffsetScaleFactor * self.appearance.cellSize,
+                'Score: ' + playerData.score
+            ),
+            playerTextBelow = this.paper.text(
+                playerX,
+                playerY + self.appearance.playerTextOffsetScaleFactor * self.appearance.cellSize,
+                playerData.health + 'hp, (' + playerData.x + ', ' + playerData.y + ')'
+            ),
+            player = this.paper.set(),
+            playerColor = cycle(this.appearance.worldColours.PLAYERS, playerData.id);
+
+        playerBody.attr("fill", playerColor);
+        playerBody.attr("stroke", this.appearance.BODY_STROKE);
+
+        [playerEyeLeft, playerEyeRight].forEach(function(eye) {
+            eye.attr("fill", self.appearance.worldColours.EYE_FILL);
+            eye.attr("stroke", self.appearance.worldColours.EYE_STROKE);
+        });
+
+        player.push(
+            playerBody,
+            playerEyeLeft,
+            playerEyeRight,
+            playerTextAbove,
+            playerTextBelow
+        );
+        return player;
+    };
+
+    window.Viewer.prototype.clearDrawnElements = function (elements) {
+        elements.forEach(function (elementToRemove) {
+            elementToRemove.remove();
+        });
+    };
+
+    window.Viewer.prototype.reDrawPlayers = function (players, height) {
+        var self = this;
+        // TODO how to know which color is which player?
+        // Should indicate some sort of a key.
+        // Also how does the player know their own ID?
+        // It doesn't match their login name.
+        return Object.keys(players).map(function (playerKey, i) {
+            var playerData = players[playerKey];
+            playerData.index = i;
+            return self.constructNewPlayerElement(playerData, height);
+        });
+    };
+
+    window.Viewer.prototype.reDrawPickups = function (pickupLocations, height) {
+        var self = this;
+        return pickupLocations.map(function (pickupLocation) {
+            var x = (0.5 + pickupLocation[0]) * self.appearance.cellSize,
+                y = (0.5 + self.invertY(height, pickupLocation[1])) * self.appearance.cellSize,
+                radius = self.appearance.cellSize * self.appearance.pickupScaleFactor,
+                circle = self.paper.circle(x, y, radius),
+                crossX = self.paper.rect(
+                    x - (self.appearance.crossLengthScaleFactor * self.appearance.cellSize / 2),
+                    y - (self.appearance.crossWidthScaleFactor * self.appearance.cellSize / 2),
+                    self.appearance.crossLengthScaleFactor * self.appearance.cellSize,
+                    self.appearance.crossWidthScaleFactor * self.appearance.cellSize).attr({
+
+                    fill: self.appearance.worldColours.HEALTH_CROSS,
+                    stroke: self.appearance.worldColours.HEALTH_CROSS
+                }),
+                crossY = self.paper.rect(
+                    x - (self.appearance.crossWidthScaleFactor * self.appearance.cellSize / 2),
+                    y - (self.appearance.crossLengthScaleFactor * self.appearance.cellSize / 2),
+                    self.appearance.crossWidthScaleFactor * self.appearance.cellSize,
+                    self.appearance.crossLengthScaleFactor * self.appearance.cellSize).attr({
+                    fill: self.appearance.worldColours.HEALTH_CROSS,
+                    stroke: self.appearance.worldColours.HEALTH_CROSS,
+                }),
+                pickup = self.paper.set();
+
+            circle.attr("fill", self.appearance.worldColours.HEALTH_BACKGROUND);
+            pickup.push(circle, crossX, crossY);
+            return pickup;
+        });
+    };
+
+    window.Viewer.prototype.reDrawState = function (drawnElements, world) {
+        this.clearDrawnElements(drawnElements.pickups);
+        this.clearDrawnElements(drawnElements.players);
+        return {
+            drawnElements: {
+                pickups: this.reDrawPickups(world.pickupLocations, world.height),
+                players: this.reDrawPlayers(world.players, world.height),
+            },
+            height: world.height,
+        };
+    };
 }());
