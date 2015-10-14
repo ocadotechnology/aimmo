6fe3de774c3ab234ad192ae6fcefa30ccb735592
George Simms <george.simms@MB17685.local>
2015-10-14 20:32:40 +0100

    get rid of controls

--- ui/players/static/js/watch/world-controls.js
+++ ui/players/static/js/watch/world-controls.js
@@ -1,3 +1,4 @@
+/*jslint browser: true*/
 // World Manipulation
 'use strict';
 (function () {
@@ -17,36 +18,23 @@
         });
     }
 
-    window.Controls = function (viewer) {
-        this.viewer = viewer;
-    };
-
-    window.Controls.prototype = {};
-
-    window.Controls.prototype.initialiseWorld = function (worldLayout) {
-        return this.viewer.reDrawWorldLayout(worldLayout);
-    };
-
-    window.Controls.prototype.setState = function (players, pickupLocations, height, drawnElements) {
-        return this.viewer.reDrawState(drawnElements,
-            {players: players, pickupLocations: pickupLocations, height: height});
-    };
-
-
-    window.refreshState = function (controls, eventualState) {
-        var newPromise = Promise.all([eventualState, jsonAsync("/api/watch/state")]).then(function (arr) {
-            var data = arr[1],
-                height = data.map_changed ?
-                        controls.initialiseWorld(data.layout) :
-                        arr[0].height,
-                output = controls.setState(data.players,
-                    data.pickup_locations,
-                    height,
-                    arr[0].drawnElements);
-            setTimeout(function () { window.refreshState(controls, output); }, 200);
+    window.refreshState = function (viewer, state) {
+        debugger;
+        jsonAsync("/api/watch/state").then(function (data) {
+            var height = data.map_changed ?
+                            viewer.reDrawWorldLayout(data.layout) :
+                            state.height,
+                world = {
+                    pickupLocations: data.pickup_locations,
+                    height: height,
+                    players: data.players
+                },
+                output = viewer.reDrawState(state.drawnElements, world);
+            setTimeout(function () { window.refreshState(viewer, output); }, 500);
         }).catch(function (er) {
             var didSuccessfullyRender = $("svg > text").length,
-                status = didSuccessfullyRender ? "LOST_CONNECTION" : "GAME_NOT_STARTED";
+                status = didSuccessfullyRender ? "LOST_CONNECTION" : "GAME_NOT_STARTED",
+                showAlert = window.showAlert;
             console.error(er);
             showAlert({status: status});
         });
@@ -57,12 +45,10 @@
 window.$(function () {
     var Viewer = window.Viewer,
         APPEARANCE = window.APPEARANCE,
-        Controls = window.Controls,
         Promise = window.Promise,
         refreshState = window.refreshState,
-        eventualState = Promise.resolve({drawnElements: {players: [], pickups: []}}),
-        viewer = new Viewer(document.getElementById("watch-world-canvas"), APPEARANCE),
-        controls = new Controls(viewer);
+        initialState = {drawnElements: {players: [], pickups: []}},
+        viewer = new Viewer(document.getElementById("watch-world-canvas"), APPEARANCE);
 
-    refreshState(controls, eventualState);
+    refreshState(viewer, initialState);
 });
