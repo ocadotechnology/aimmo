jishi9/aimmo.defunct

Renamed 'generate_xx' methods to 'get_xx'

simulation/avatar_examples/health_seeker_avatar.py
@@ -23,7 +23,7 @@ class Avatar(object):
         return abs(a.x - b.x) + abs(a.y - b.y)
 
     def get_closest_pickup_location(self):
-        c = min(self.world_state.map_centred_at_me.generate_pickup_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location))
+        c = min(self.world_state.map_centred_at_me.get_pickup_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location))
         print 'targeting', c
         return c.location
 

simulation/avatar_examples/winner_avatar.py
@@ -24,7 +24,7 @@ class Avatar(object):
         return abs(a.x - b.x) + abs(a.y - b.y)
         
     def get_closest_score_location(self):
-        return min(self.world_state.map_centred_at_me.generate_score_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location)).location
+        return min(self.world_state.map_centred_at_me.get_score_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location)).location
 
     def get_possible_directions(self):
         from simulation.direction import ALL_DIRECTIONS

simulation/world_map.py
@@ -48,21 +48,21 @@ class WorldMap(object):
     def __init__(self, grid):
         self.grid = grid
 
-    def generate_all_cells(self):
+    def get_all_cells(self):
         return (cell for sublist in self.grid for cell in sublist)
 
     @property
     def all_cells(self):
-        return list(self.generate_all_cells())
+        return list(self.get_all_cells())
 
-    def generate_score_cells(self):
-        return (c for c in self.generate_all_cells() if c.generates_score)
+    def get_score_cells(self):
+        return (c for c in self.get_all_cells() if c.generates_score)
 
-    def generate_potential_spawn_locations(self):
-        return (c for c in self.generate_all_cells() if c.habitable and not c.generates_score and not c.avatar and not c.pickup)
+    def get_potential_spawn_locations(self):
+        return (c for c in self.get_all_cells() if c.habitable and not c.generates_score and not c.avatar and not c.pickup)
 
-    def generate_pickup_cells(self):
-        return (c for c in self.generate_all_cells() if c.pickup)
+    def get_pickup_cells(self):
+        return (c for c in self.get_all_cells() if c.pickup)
 
     def is_on_map(self, location):
         num_cols = len(self.grid)
@@ -81,27 +81,27 @@ class WorldMap(object):
         self.add_pickups(num_avatars)
 
     def reset_score_locations(self, num_avatars):
-        for cell in self.generate_score_cells():
+        for cell in self.get_score_cells():
             if random.random() < SCORE_DESPAWN_CHANCE:
                 cell.generates_score = False
 
-        new_num_score_locations = len(list(self.generate_score_cells()))
+        new_num_score_locations = len(list(self.get_score_cells()))
         target_num_score_locations = int(math.ceil(num_avatars * TARGET_NUM_SCORE_LOCATIONS_PER_AVATAR))
         num_score_locations_to_add = target_num_score_locations - new_num_score_locations
         if num_score_locations_to_add > 0:
-            for cell in random.sample(list(self.generate_potential_spawn_locations()), num_score_locations_to_add):
+            for cell in random.sample(list(self.get_potential_spawn_locations()), num_score_locations_to_add):
                 cell.generates_score = True
 
     def add_pickups(self, num_avatars):
         target_num_pickups = int(math.ceil(num_avatars * TARGET_NUM_PICKUPS_PER_AVATAR))
-        max_num_pickups_to_add = target_num_pickups - len(list(self.generate_pickup_cells()))
+        max_num_pickups_to_add = target_num_pickups - len(list(self.get_pickup_cells()))
         if max_num_pickups_to_add > 0:
-            for cell in random.sample(list(self.generate_potential_spawn_locations()), max_num_pickups_to_add):
+            for cell in random.sample(list(self.get_potential_spawn_locations()), max_num_pickups_to_add):
                 if random.random() < PICKUP_SPAWN_CHANCE:
                     cell.pickup = HealthPickup()
 
     def get_random_spawn_location(self):
-        return random.choice(list(self.generate_potential_spawn_locations())).location
+        return random.choice(list(self.get_potential_spawn_locations())).location
 
     # TODO: cope with negative coords (here and possibly in other places)
     def can_move_to(self, target_location):

ocadotechnology/aimmo

simulation/avatar_examples/health_seeker_avatar.py
@@ -23,7 +23,7 @@ class Avatar(object):
         return abs(a.x - b.x) + abs(a.y - b.y)
 
     def get_closest_pickup_location(self):
-        c = min(self.world_state.map_centred_at_me.generate_pickup_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location))
+        c = min(self.world_state.map_centred_at_me.pickup_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location))
         print 'targeting', c
         return c.location
 

simulation/avatar_examples/winner_avatar.py
@@ -24,7 +24,7 @@ class Avatar(object):
         return abs(a.x - b.x) + abs(a.y - b.y)
         
     def get_closest_score_location(self):
-        return min(self.world_state.map_centred_at_me.generate_score_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location)).location
+        return min(self.world_state.map_centred_at_me.score_cells(), key=lambda cell: self.distance_between(cell.location, self.world_state.my_avatar.location)).location
 
     def get_possible_directions(self):
         from simulation.direction import ALL_DIRECTIONS

simulation/world_map.py
@@ -48,21 +48,17 @@ class WorldMap(object):
     def __init__(self, grid):
         self.grid = grid
 
-    def generate_all_cells(self):
+    def all_cells(self):
         return (cell for sublist in self.grid for cell in sublist)
 
-    @property
-    def all_cells(self):
-        return list(self.generate_all_cells())
+    def score_cells(self):
+        return (c for c in self.all_cells() if c.generates_score)
 
-    def generate_score_cells(self):
-        return (c for c in self.generate_all_cells() if c.generates_score)
+    def potential_spawn_locations(self):
+        return (c for c in self.all_cells() if c.habitable and not c.generates_score and not c.avatar and not c.pickup)
 
-    def generate_potential_spawn_locations(self):
-        return (c for c in self.generate_all_cells() if c.habitable and not c.generates_score and not c.avatar and not c.pickup)
-
-    def generate_pickup_cells(self):
-        return (c for c in self.generate_all_cells() if c.pickup)
+    def pickup_cells(self):
+        return (c for c in self.all_cells() if c.pickup)
 
     def is_on_map(self, location):
         num_cols = len(self.grid)
@@ -81,27 +77,27 @@ class WorldMap(object):
         self.add_pickups(num_avatars)
 
     def reset_score_locations(self, num_avatars):
-        for cell in self.generate_score_cells():
+        for cell in self.score_cells():
             if random.random() < SCORE_DESPAWN_CHANCE:
                 cell.generates_score = False
 
-        new_num_score_locations = len(list(self.generate_score_cells()))
+        new_num_score_locations = len(list(self.score_cells()))
         target_num_score_locations = int(math.ceil(num_avatars * TARGET_NUM_SCORE_LOCATIONS_PER_AVATAR))
         num_score_locations_to_add = target_num_score_locations - new_num_score_locations
         if num_score_locations_to_add > 0:
-            for cell in random.sample(list(self.generate_potential_spawn_locations()), num_score_locations_to_add):
+            for cell in random.sample(list(self.potential_spawn_locations()), num_score_locations_to_add):
                 cell.generates_score = True
 
     def add_pickups(self, num_avatars):
         target_num_pickups = int(math.ceil(num_avatars * TARGET_NUM_PICKUPS_PER_AVATAR))
-        max_num_pickups_to_add = target_num_pickups - len(list(self.generate_pickup_cells()))
+        max_num_pickups_to_add = target_num_pickups - len(list(self.pickup_cells()))
         if max_num_pickups_to_add > 0:
-            for cell in random.sample(list(self.generate_potential_spawn_locations()), max_num_pickups_to_add):
+            for cell in random.sample(list(self.potential_spawn_locations()), max_num_pickups_to_add):
                 if random.random() < PICKUP_SPAWN_CHANCE:
                     cell.pickup = HealthPickup()
 
     def get_random_spawn_location(self):
-        return random.choice(list(self.generate_potential_spawn_locations())).location
+        return random.choice(list(self.potential_spawn_locations())).location
 
     # TODO: cope with negative coords (here and possibly in other places)
     def can_move_to(self, target_location):