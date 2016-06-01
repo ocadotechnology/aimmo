31fd1bb8962883ad1aa9eb8cbdd3cf19f480a9db
Mike Bryant <m@ocado.com>
2016-06-01 19:15:25 +0100

    Remove obsolete Avatar model

---
+++ players/migrations/0002_auto_20160601_1914.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('players', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='avatar',
+            name='player',
+        ),
+        migrations.DeleteModel(
+            name='Avatar',
+        ),
+    ]
--- players/models.py
+++ players/models.py
@@ -5,8 +5,3 @@ from django.db import models
 class Player(models.Model):
     user = models.OneToOneField(User)
     code = models.TextField()
-
-
-# TODO: switch code back  to Avatar from Player to support players undoing their code changes
-class Avatar(models.Model):
-    player = models.ForeignKey(User)
