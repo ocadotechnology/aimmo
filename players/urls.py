0532ec00b20de6dded8d371a79cae7d9ccc311fb
Spycho <chrisbrett665@gmail.com>
2016-06-02 19:55:06 +0100

    removed restriction from game data api

--- players/urls.py
+++ players/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^statistics/$', staff_member_required(TemplateView.as_view(template_name='players/statistics.html')), name='statistics'),
 
     url(r'^api/code/$', staff_member_required(views.code), name='code'),
-    url(r'^api/games/$', staff_member_required(views.games), name='games'),
+    url(r'^api/games/$', views.games, name='games'),
 
     url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse'),  # TODO: Pull request to make django_js_reverse.urls
 ]
