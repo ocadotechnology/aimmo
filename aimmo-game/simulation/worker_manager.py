d0789cb014a11ebe10f0b5693ec4a5eb43ede979
Wael Al Jishi <jishi9@gmail.com>
2016-06-15 20:28:34 +0100

    WIP: worker manager - works but maybe we want to do this differently

--- aimmo-game/simulation/worker_manager.py
+++ aimmo-game/simulation/worker_manager.py
@@ -9,13 +9,60 @@ import requests
 LOGGER = logging.getLogger(__name__)
 
 
+class _WorkerManagerData(object):
+    """
+    This class is thread safe
+    """
+
+    def __init__(self, game_state, user_codes):
+        self._game_state = game_state
+        self._user_codes = user_codes
+        self._lock = threading.Lock()
+
+    def _remove_avatar(self, user_id):
+        assert self._lock.locked
+        self._game_state.remove_avatar(user_id)
+        del self._user_codes[user_id]
+
+    def remove_user_if_code_is_different(self, user):
+        with self._lock:
+            existing_code = self._user_codes.get(user['id'], None)
+            if existing_code != user['code']:
+                # Remove avatar from the game, so it stops being called for turns
+                if existing_code is not None:
+                    self._remove_avatar(user['id'])
+                return True
+            else:
+                return False
+
+    def add_avatar(self, user, worker_url):
+        with self._lock:
+            # Add avatar back into game
+            self._game_state.add_avatar(
+                user_id=user['id'], worker_url="%s/turn/" % worker_url)
+            self._user_codes[user['id']] = user['code']
+
+    def remove_unknown_avatars(self, known_user_ids):
+        with self._lock:
+            unknown_user_ids = set(self._user_codes) - frozenset(known_user_ids)
+            for u in unknown_user_ids:
+                self._remove_avatar(u)
+            return unknown_user_ids
+
+
 class WorkerManager(threading.Thread):
+    """
+    Methods of this class must be thread safe unless explicitly stated.
+    """
     daemon = True
 
     def __init__(self, game_state, users_url):
-        self.game_state = game_state
+        """
+
+        :param thread_pool:
+        """
+        self._data = _WorkerManagerData(game_state, {})
         self.users_url = users_url
-        self.user_codes = {}
         super(WorkerManager, self).__init__()
 
     def get_persistent_state(self, player_id):
@@ -33,6 +80,36 @@ class WorkerManager(threading.Thread):
 
         raise NotImplemented
 
+    # TODO handle failure
+    def spawn_and_init(self, user):
+        # Get persistent state from worker
+        persistent_state = self.get_persistent_state(user['id'])
+
+        # Kill worker
+        self.remove_worker(user['id'])
+
+        # Spawn worker
+        worker_url = self.create_worker(user['id'])
+
+        # Initialise worker
+        requests.post("%s/initialise/" % worker_url, json={
+            'code': user['code'],
+            'options': {},
+            'persistent_state': persistent_state,
+        })
+
+        # Add avatar back into game
+        self._data.add_avatar(user, worker_url)
+
+    # multiprocessing.pool.imap does not work with eventlet.monkey_patch
+    def _parallel_map(self, func, iterable_args):
+        jobs = [threading.Thread(target=func, args=[arg]) for arg in iterable_args]
+        for j in jobs:
+            j.start()
+        for j in jobs:
+            j.join()
+
+
     def run(self):
         while True:
             try:
@@ -41,32 +118,20 @@ class WorkerManager(threading.Thread):
                 LOGGER.error("Obtaining game data failed: %s", err)
             else:
                 game = game_data['main']
+
+                # Remove users with different code
+                users_to_add = []
                 for user in game['users']:
-                    if self.user_codes.get(user['id'], None) != user['code']:
-                        # Remove avatar from the game, so it stops being called
-                        # for turns
-                        self.game_state.remove_avatar(user['id'])
-                        # Get persistent state from worker
-                        persistent_state = self.get_persistent_state(user['id'])
-                        # Kill worker
-                        self.remove_worker(user['id'])
-                        # Spawn worker
-                        worker_url = self.create_worker(user['id'])
-                        # Initialise worker
-                        requests.post("%s/initialise/" % worker_url, json={
-                            'code': user['code'],
-                            'options': {},
-                            'persistent_state': persistent_state,
-                        })
-                        # Add avatar back into game
-                        self.game_state.add_avatar(
-                            user_id=user['id'], worker_url="%s/turn/" % worker_url)
-                        self.user_codes[user['id']] = user['code']
-                removed_user_ids = set(self.user_codes) - set(user['id'] for user in game['users'])
-                for removed_user in removed_user_ids:
-                    self.game_state.remove_avatar(removed_user)
-                    self.remove_worker(removed_user)
-                    del self.user_codes[removed_user]
+                    if self._data.remove_user_if_code_is_different(user):
+                        users_to_add.append(user)
+
+                # Add removed users
+                self._parallel_map(self.spawn_and_init, users_to_add)
+
+                # Delete extra users
+                known_avatars = set(user['id'] for user in game['users'])
+                removed_user_ids = self._data.remove_unknown_avatars(known_avatars)
+                self._parallel_map(self.remove_worker, removed_user_ids)
 
             time.sleep(10)
 
