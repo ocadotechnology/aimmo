0352d990b0f8cf8b761629368fbead306f8b6df7
Mike Bryant <mike@mikebryant.me.uk>
2016-06-16 00:40:15 +0100

    Don't delete the Ingress or we lose the static IP

--- aimmo-game-creator/update.sh
+++ aimmo-game-creator/update.sh
@@ -4,7 +4,6 @@ kubectl delete rc aimmo-game-creator || true
 kubectl delete rc -l app=aimmo-game
 kubectl delete rc -l app=aimmo-game-worker
 kubectl delete service -l app=aimmo-game
-kubectl delete ingress game || true
 kubectl create -f rc-aimmo-game-creator.yaml
 sleep 5
 kubectl get rc
