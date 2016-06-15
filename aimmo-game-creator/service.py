5ffb5cc16becd3a6954e51c4f3f1ea12f1f091df
Mike Bryant <mike@mikebryant.me.uk>
2016-06-15 20:37:11 +0100

    type: NodePort required for gce service load balancing

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -86,6 +86,7 @@ def create_game_service(api, name, _config):
                         'targetPort': 5000,
                     },
                 ],
+                'type': 'NodePort',
             },
         },
     )
