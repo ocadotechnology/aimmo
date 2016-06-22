fac15e545bf19369c2bb6734a5500dfe3537afb5
Mike Bryant <mike@mikebryant.me.uk>
2016-06-22 18:51:38 +0100

    Fix typo

--- aimmo-game-creator/update.sh
+++ aimmo-game-creator/update.sh
@@ -2,7 +2,7 @@
 set -e
 kubectl delete rc aimmo-game-creator || true
 kubectl delete rc -l app=aimmo-game
-kubectl delete rc -l app=aimmo-game-worker
+kubectl delete pod -l app=aimmo-game-worker
 kubectl delete service -l app=aimmo-game
 kubectl create -f rc-aimmo-game-creator.yaml
 sleep 5
