b5fc7b704c8337d583c1d767aa197446faf820d1
Joshua Blake <joshua.blake@ocado.com>
2016-08-11 15:41:59 +0100

    Pass auth token to creator container and use it when fetching API

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from worker_manager import WORKER_MANAGERS
+import errno
 import logging
 import os
 
@@ -7,8 +8,15 @@ import os
 def main():
     logging.basicConfig(level=logging.DEBUG)
     WorkerManagerClass = WORKER_MANAGERS[os.environ.get('WORKER_MANAGER', 'local')]
-    worker_manager = WorkerManagerClass(os.environ.get('GAME_API_URL',
-                                        'http://localhost:8000/players/api/games/'))
+    url = os.environ.get('GAMES_API_URL', 'http://localhost:8000/players/api/games/')
+    try:
+        with open('/etc/secret/auth') as auth_file:
+            auth_token = auth_file.read()
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        auth_token = 'insecure-creator-auth-token'
+    worker_manager = WorkerManagerClass(url+'?auth_token='+auth_token)
     worker_manager.run()
 
 if __name__ == '__main__':
