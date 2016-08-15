2ad512e6982644554f36670297bb7ad6719519ff
Joshua Blake <joshua.blake@ocado.com>
2016-08-15 10:14:46 +0100

    Use env variable secrets rather than a file. Fixes #115.

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from worker_manager import WORKER_MANAGERS
-import errno
 import logging
 import os
 
@@ -9,13 +8,7 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     WorkerManagerClass = WORKER_MANAGERS[os.environ.get('WORKER_MANAGER', 'local')]
     url = os.environ.get('GAMES_API_URL', 'http://localhost:8000/players/api/games/')
-    try:
-        with open('/etc/secret/auth') as auth_file:
-            auth_token = auth_file.read()
-    except IOError as e:
-        if e.errno != errno.ENOENT:
-            raise
-        auth_token = 'insecure-creator-auth-token'
+    auth_token = os.environ.get('AUTH_TOKEN', 'insecure-creator-auth-token')
     worker_manager = WorkerManagerClass(url+'?auth_token='+auth_token)
     worker_manager.run()
 
