a455bfe353bef40efb1249b2c1d5d49aebf7ef5f
Joshua Blake <joshua.blake@ocado.com>
2016-08-18 16:40:28 +0100

    Refactor AvatarWrapper.decide_action()

--- aimmo-game/simulation/avatar/avatar_wrapper.py
+++ aimmo-game/simulation/avatar/avatar_wrapper.py
@@ -31,28 +31,32 @@ class AvatarWrapper(object):
     def is_moving(self):
         return isinstance(self.action, MoveAction)
 
+    def _fetch_action(self, state_view):
+        return requests.post(self.worker_url, json=state_view).json()
+
+    def _construct_action(self, data):
+        action_data = data['action']
+        action_type = action_data['action_type']
+        action_args = action_data.get('options', {})
+        action_args['avatar'] = self
+        return ACTIONS[action_type](**action_args)
+
     def decide_action(self, state_view):
         try:
-            try:
-                data = requests.post(self.worker_url, json=state_view).json()
-            except ValueError as err:
-                LOGGER.info('Failed to get turn result: %s', err)
-            except requests.exceptions.ConnectionError:
-                LOGGER.info('Could not connect to worker, probably not ready yet')
-            else:
-                try:
-                    action_data = data['action']
-                    action_type = action_data['action_type']
-                    action_args = action_data.get('options', {})
-                    action_args['avatar'] = self
-                    action = ACTIONS[action_type](**action_args)
-                except (KeyError, ValueError) as err:
-                    LOGGER.info('Bad action data supplied: %s', err)
-                else:
-                    self._action = action
-                    return True
+            data = self._fetch_action(state_view)
+            action = self._construct_action(data)
+
+        except (KeyError, ValueError) as err:
+            LOGGER.info('Bad action data supplied: %s', err)
+        except requests.exceptions.ConnectionError:
+            LOGGER.info('Could not connect to worker, probably not ready yet')
         except Exception:
             LOGGER.exception("Unknown error while fetching turn data")
+
+        else:
+            self._action = action
+            return True
+
         self._action = WaitAction(self)
         return False
 
