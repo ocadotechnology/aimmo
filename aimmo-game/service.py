620008c3dd4c43ab83f0a09d5ecde29caa6f9674
Joshua Blake <joshua.blake@ocado.com>
2016-09-08 15:36:09 +0100

    Fix users_url

--- aimmo-game/service.py
+++ aimmo-game/service.py
@@ -124,7 +124,7 @@ def run_game(port):
     game_state = generator.get_game_state(player_manager)
     turn_manager = ConcurrentTurnManager(game_state=game_state, end_turn_callback=send_world_update, completion_url=api_url+'complete/?auth_token='+os.environ['auth_token'])
     WorkerManagerClass = WORKER_MANAGERS[os.environ.get('WORKER_MANAGER', 'local')]
-    worker_manager = WorkerManagerClass(game_state=game_state, users_url=api_url+'/?auth_token='+os.environ['auth_token'], port=port)
+    worker_manager = WorkerManagerClass(game_state=game_state, users_url=api_url+'?auth_token='+os.environ['auth_token'], port=port)
     worker_manager.start()
     turn_manager.start()
 
