5c26ac5b072e41d42dbcbb8b2e2a1b3c617956a6
Chris Mukherjee <c.mukherjee@ocado.com>
2016-11-17 09:26:08 +0000

    removed breakpoints and annotations

--- players/static/js/program.js
+++ players/static/js/program.js
@@ -15,33 +15,6 @@ $( document ).ready(function() {
         enableSnippets: true,
         enableLiveAutocompletion: true
     });
-    editor.renderer.setShowGutter(true);
-    editor.on("guttermousedown", function(e){
-        var target = e.domEvent.target;
-
-        if (target.className.indexOf("ace_gutter-cell") == -1){
-            return;
-        }
-        if (!editor.isFocused()){
-            return;
-        }
-        if (e.clientX > 25 + target.getBoundingClientRect().left){
-            return;
-        }
-        var row = e.getDocumentPosition().row;
-        var breakpointsArray = e.editor.session.getBreakpoints();
-        if(!(row in breakpointsArray)){
-            e.editor.session.setBreakpoint(row);
-        }else{
-            e.editor.session.clearBreakpoint(row);
-        }
-        e.stop();
-    });
-    editor.session.on("changeBreakpoint", function(e){
-        // captures set and clear breakpoint events
-    });
-    var annotations = aceEditor.getSession().getAnnotations();
-    aceEditor.getSession().setAnnotations(annotations);
 
     var showAlert = function(alertString, alertType) {
         if (alertType === DANGER_CLASS || alertType === SUCCESS_CLASS) {
