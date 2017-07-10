140b455d20a86a057ebc533fe3e682e920bfe8e9
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-10 13:45:44 +0100

    Established connection using SocketIO plugin.

---
+++ aimmo-unity/Assets/WorldControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySocketIO;
+using UnitySocketIO.Events;
+
+public class WorldControls : MonoBehaviour 
+{
+	public SocketIOController io;
+
+	// Use this for initialization
+	void Start() 
+	{
+		io.On("connect", (SocketIOEvent e) => {
+			Debug.Log("SocketIO Connected");
+		});
+
+		io.Connect();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+}
