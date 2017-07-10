5724af11c2bc84b1fa1bff93a55296c3d3ea4cd2
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-10 13:40:31 +0100

    Finished implementing the UnityWorldState. Now the world state is exposed to the front-end correctly.

--- aimmo-game/connection/world_state.py
+++ aimmo-game/connection/world_state.py
@@ -32,28 +32,59 @@ class UnityWorldState(WorldState):
     def __init__(self, game_state):
         self.game_state = game_state
 
-    def __player_dict(avatar):
-        # maybe need to add the other charachteristics?
-        return {
-            'id': avatar.player_id,
-            'x': avatar.location.x,
-            'y': avatar.location.y,
-        }
-
     def get_update(self):
+        def player_dict(avatar):
+            # maybe need to add the other charachteristics?
+            return {
+                'id': avatar.player_id,
+                'x': avatar.location.x,
+                'y': avatar.location.y,
+            }
+
         with self.game_state as game_state:
             world = game_state.world_map
             player_data = {p.player_id: player_dict(p) for p in game_state.avatar_manager.avatars}
 
+            # TODO: need to make a more clear layout of resources: new_players, deleted_players, etc.
             pickups = get_pickups_list(world)
-            return  {
-                'players': player_data
+            ans_dict = {
+                'players': player_data,
+                'new_players': {},
+                'deleted_players': {}
                 # 'pickups': pickups # pickups not supported yet
             }
+            print ans_dict
+            return ans_dict
 
     def get_init(self):
-        return {}
+        with self.game_state as game_state:
+            world = game_state.world_map
 
+            # TODO: warn -- we might want new_players, deleted_players in initial dict as well
+            curr_dict = defaultdict(dict)
+            curr_dict["players"] = self.get_update()["players"]
+
+            map_dimensions = {
+                'minX': world.min_x(),
+                'minY': world.min_y(),
+                'maxX': world.max_x(),
+                'maxY': world.max_y()
+            }
+            curr_dict.update(map_dimensions)
+
+            # for the moment the front-end supports only obstacles
+            # we use a hash of the location as an ID for the cells at the moment
+            obstacles = []
+            for cell in world.all_cells():
+                if not cell.habitable:
+                    obstacles.append({
+                        'id' : hash(cell),
+                        'x' : cell.location.x,
+                        'y' : cell.location.y
+                    })
+            curr_dict['objects'] = obstacles
+
+            return curr_dict
 
 class BrowserWorldState(WorldState):
     def __init__(self, game_state):
