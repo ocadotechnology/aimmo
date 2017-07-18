7e47655f0dba8d01783c8898e646dc22ed7a8437
ad5915 <dan.alex97@yahoo.com>
2017-07-18 20:12:38 +0100

    Fixed setup.py so that the CI build works. Separated kubernates and non-kubernates tests.

--- integration-tests/setup.py
+++ integration-tests/setup.py
@@ -1,4 +1,16 @@
 from setuptools import find_packages, setup
+import unittest
+
+def custom_test_suite():
+    test_loader = unittest.TestLoader()
+
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    ktest_suite = test_loader.discover('tests', pattern='ktest_*.py')
+
+    # kubernates tests are not yet fully supported
+    all_tests = unittest.TestSuite([test_suite])
+
+    return all_tests
 
 setup(
     name='integration-tests',
@@ -9,11 +21,6 @@ setup(
     tests_require=[
         'httmock',
     ],
+    test_suite="setup.custom_test_suite",
     zip_safe=False,
 )
-
-import os
-import subprocess
-
-p = subprocess.Popen(['python', "integration_test.py"], cwd="./tests")
-p.communicate()
