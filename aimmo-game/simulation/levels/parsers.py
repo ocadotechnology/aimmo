4aa770a00416058acef9ea34b88159c298303493
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-19 12:19:31 +0100

    Added a map parser. Now the map is formatted in a matrix that should be transformed according to a model.

--- aimmo-game/simulation/levels/parsers.py
+++ aimmo-game/simulation/levels/parsers.py
@@ -11,11 +11,24 @@ class Parser():
             model = json.load(data_file)
         return model
 
+    def parse_map(self, map_name):
+        with open(self.__MAPS_FOLDER + "/" + map_name) as content_file:
+            content = content_file.read()
+        lines = content.split('\n')
+        cells = [list(filter(lambda x: x != '', line.split(" ")))
+            for line in lines]
+        cells = list(filter(lambda x: x != [], cells))
+        return cells
+
+
 def main():
     parser = Parser()
 
     model = parser.parse_model("objects.json")
     pprint(model)
+
+    map = parser.parse_map("level1.txt")
+    pprint(map)
 
 if __name__ == '__main__':
     main()
