243ae08164e44b78ab1354747b99c038e577c5a3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-20 16:09:19 +0100

    Avatar view works, objects exist only if they are around you. Need to fix the out-of-bounds issue in the world map.

--- aimmo-game/simulation/avatar/avatar_view.py
+++ aimmo-game/simulation/avatar/avatar_view.py
@@ -18,7 +18,7 @@ class AvatarView():
         self.is_empty = True
 
     # Returns all the cells in the rectangle defined by two corners.
-    def cells_in_rectangle(sefl, top_left, bottom_right, world_map):
+    def cells_in_rectangle(self, top_left, bottom_right, world_map):
         cells = []
         for x in range(top_left.x, bottom_right.x):
             for y in range(bottom_right.y, top_left.y):
@@ -26,17 +26,10 @@ class AvatarView():
         return cells
 
     # Reveals all the cells in the view.
-    def reveal_all_cells(self):
-        self.cells_to_reveal = self.cells_in_rectangle(self.NW_horizon, self.SE_horizon)
+    def reveal_all_cells(self, world_map):
+        self.cells_to_reveal = self.cells_in_rectangle(self.NW_horizon, self.SE_horizon, world_map)
 
     def move(self, move_direction, world_map):
-        # Shift the four view corners (horizons).
-        def move_horizons(direction):
-            self.NE_horizon += direction
-            self.NW_horizon += direction
-            self.SE_horizon += direction
-            self.SW_horizon += direction
-
         # Update cells to clear and to reveal depending on the move direction.
         if move_direction == direction.EAST:
             self.cells_to_clear = self.cells_in_rectangle(self.NW_horizon, self.SW_horizon + direction.EAST, world_map)
@@ -53,4 +46,10 @@ class AvatarView():
         else:
             raise ValueError
 
-        move_horizons(move_direction)
+        #print("------------ (%d, %d) - (%d, %d) -----------" % (self.NW_horizon.x, self.NW_horizon.y, self.SE_horizon.x, self.SE_horizon.y))
+
+        # Shift the four view corners (horizons).
+        self.NE_horizon += move_direction
+        self.NW_horizon += move_direction
+        self.SE_horizon += move_direction
+        self.SW_horizon += move_direction
