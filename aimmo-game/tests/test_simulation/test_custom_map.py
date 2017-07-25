a60e3fdfe8da4ce0a4806c597a33fbb6b89eb5e2
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-25 16:22:16 +0100

    Implemented a mockery for level generation testing. The map dimension size and coordinate translation has to be fixed.

--- aimmo-game/tests/test_simulation/test_custom_map.py
+++ aimmo-game/tests/test_simulation/test_custom_map.py
@@ -13,7 +13,11 @@ from simulation.custom_map import ScoreCellDecoder
 from simulation.custom_map import ObstacleDecoder
 from simulation.custom_map import PickupDecoder
 
-class TestGenerators(unittest.TestCase):
+from simulation.custom_map import JsonLevelGenerator
+from simulation.levels.levels import RawLevelGenerator
+from .levels.test_parsers import MockParser
+
+class TestDecoders(unittest.TestCase):
     def setUp(self):
         self.map = EmptyMapGenerator(2, 2, {}).get_map()
 
@@ -81,3 +85,26 @@ class TestGenerators(unittest.TestCase):
         self.assertTrue(isinstance(self.map.get_cell(Location(0, 1)).pickup, HealthPickup))
         self.assertTrue(isinstance(self.map.get_cell(Location(1, 0)).pickup, DamagePickup))
         self.assertTrue(isinstance(self.map.get_cell(Location(1, 1)).pickup, InvulnerabilityPickup))
+
+def get_mock_level(map, parsers):
+    class MockLevel(JsonLevelGenerator):
+        def get_map(self):
+            self._register_json(
+                RawLevelGenerator()
+                    .by_parser(MockParser())
+                    .by_map(map)
+                    .by_models(parsers)
+                    .generate_json())
+            self._register_decoders()
+            self._json_decode_map()
+
+            return self.world_map
+    return MockLevel({})
+
+class TestJsonLevelGenerator(unittest.TestCase):
+    def test_json_simple_map(self):
+        mock_level = get_mock_level("map_simple.txt", ["real_model.json"])
+        mock_map = mock_level.get_map()
+
+        # for cell in mock_map.all_cells():
+        #     print("CELL: (" + str(cell.location.x) + " " + str(cell.location.y) + ")")
