ab6f2a2a86a33f36f4a4e71e4eedf2b5ca0c3ced
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-25 15:52:22 +0100

    Written tests for the rest of the decoders.

--- aimmo-game/tests/test_simulation/test_custom_map.py
+++ aimmo-game/tests/test_simulation/test_custom_map.py
@@ -4,6 +4,9 @@ import random
 import unittest
 
 from simulation.location import Location
+from simulation.pickups import HealthPickup
+from simulation.pickups import DamagePickup
+from simulation.pickups import InvulnerabilityPickup
 
 from simulation.world_map import WorldMap
 from simulation.custom_map import ScoreCellDecoder
@@ -34,3 +37,47 @@ class TestGenerators(unittest.TestCase):
         self.assertTrue(self.map.get_cell(Location(0, 1)).habitable)
         self.assertTrue(self.map.get_cell(Location(1, 0)).habitable)
         self.assertFalse(self.map.get_cell(Location(1, 1)).habitable)
+
+    def test_score_cell_decoder(self):
+        ScoreCellDecoder("0").decode({
+            "x" : "0",
+            "y" : "0"
+        }, self.map)
+
+        self.assertTrue(self.map.get_cell(Location(0, 0)).generates_score)
+        self.assertFalse(self.map.get_cell(Location(0, 1)).generates_score)
+        self.assertFalse(self.map.get_cell(Location(1, 0)).generates_score)
+        self.assertFalse(self.map.get_cell(Location(1, 1)).generates_score)
+
+        ScoreCellDecoder("0").decode({
+            "x" : "1",
+            "y" : "1"
+        }, self.map)
+
+        self.assertTrue(self.map.get_cell(Location(0, 0)).generates_score)
+        self.assertFalse(self.map.get_cell(Location(0, 1)).generates_score)
+        self.assertFalse(self.map.get_cell(Location(1, 0)).generates_score)
+        self.assertTrue(self.map.get_cell(Location(1, 1)).generates_score)
+
+    def test_pickup_decoder(self):
+        PickupDecoder("0").decode({
+            "x" : "0",
+            "y" : "1",
+            "type" : "health",
+            "health_restored" : "5"
+        }, self.map)
+        PickupDecoder("0").decode({
+            "x" : "1",
+            "y" : "0",
+            "type" : "damage"
+        }, self.map)
+        PickupDecoder("0").decode({
+            "x" : "1",
+            "y" : "1",
+            "type" : "invulnerability"
+        }, self.map)
+
+        self.assertTrue(self.map.get_cell(Location(0, 0)).pickup is None)
+        self.assertTrue(isinstance(self.map.get_cell(Location(0, 1)).pickup, HealthPickup))
+        self.assertTrue(isinstance(self.map.get_cell(Location(1, 0)).pickup, DamagePickup))
+        self.assertTrue(isinstance(self.map.get_cell(Location(1, 1)).pickup, InvulnerabilityPickup))
