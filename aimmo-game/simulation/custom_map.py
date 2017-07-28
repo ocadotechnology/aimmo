307f57a1d2e254ab3ef66160771dd8fadc910679
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-28 15:51:04 +0100

    Moved decoders to a different folder so that all the level related classes will be placed in only one file.

--- aimmo-game/simulation/custom_map.py
+++ aimmo-game/simulation/custom_map.py
@@ -3,19 +3,15 @@ import math
 
 from simulation.levels.levels import LEVELS
 from simulation.levels.completion_checks import COMPLETION_CHECKS
+from simulation.levels.decoders import *
 
 from pprint import pprint
 
 from simulation.location import Location
 from simulation.game_state import GameState
 from simulation.world_map import WorldMap
-from simulation.world_map import WorldMapStaticSpawnDecorator
 from simulation.world_map import DEFAULT_LEVEL_SETTINGS
 from simulation.world_map import Cell
-
-from simulation.pickups import HealthPickup
-from simulation.pickups import InvulnerabilityPickup
-from simulation.pickups import DamagePickup
 
 import sys
 current_module = sys.modules[__name__]
@@ -105,56 +101,6 @@ class BaseLevelGenerator(BaseGenerator):
     def __init__(self, *args, **kwargs):
         super(BaseLevelGenerator, self).__init__(*args, **kwargs)
         self.settings.update(DEFAULT_LEVEL_SETTINGS)
-
-################################################################################
-
-class Decoder():
-    """
-        See @JsonLevelGenerator and @Levels first.
-
-        A Decorer is a class that receives a Json formatted as in levels/models
-        and decodes it, altering the state of the world_map.
-
-        Decoders are used to translate a Level from the JSON format to the internal
-        state of a map(i.e. a WorldMap).
-
-        An example of a JSon format is:
-          {
-            "code": "1",
-            "id" : "5",
-            "x" : "3",
-            "y" : "3"
-          }
-    """
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, code):
-        self.code = code
-
-    @abc.abstractmethod
-    def decode(self, json, world_map):
-        pass
-
-class ScoreCellDecoder(Decoder):
-    def decode(self, json, world_map):
-        x, y = int(json["x"]), int(json["y"])
-        world_map = WorldMapStaticSpawnDecorator(world_map, Location(x, y))
-        world_map.get_cell(Location(x, y)).generates_score = True
-
-class ObstacleDecoder(Decoder):
-    def decode(self, json, world_map):
-        x, y = int(json["x"]), int(json["y"])
-        world_map.get_cell(Location(x, y)).habitable = False
-
-class PickupDecoder(Decoder):
-    def decode(self, json, world_map):
-        x, y = int(json["x"]), int(json["y"])
-        if json["type"] == "invulnerability":
-            world_map.get_cell(Location(x, y)).pickup = InvulnerabilityPickup(world_map.get_cell(Location(x, y)))
-        if json["type"] == "health":
-            world_map.get_cell(Location(x, y)).pickup = HealthPickup(world_map.get_cell(Location(x, y)), int(json["health_restored"]))
-        if json["type"] == "damage":
-            world_map.get_cell(Location(x, y)).pickup = DamagePickup(world_map.get_cell(Location(x, y)))
 
 ################################################################################
 
