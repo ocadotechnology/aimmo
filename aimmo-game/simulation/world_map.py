7f9dbe63d04e0ab8209c1708e09b0a0c793ad4d5
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-31 11:45:48 +0100

    Moved cell in separate folder. We shall replace the habitable and score_location fields with a new common class.

--- aimmo-game/simulation/world_map.py
+++ aimmo-game/simulation/world_map.py
@@ -20,62 +20,11 @@ DEFAULT_LEVEL_SETTINGS = {
     'PARTIAL_FOG_OF_WAR_DISTANCE': 1000,
 }
 
-class Cell(object):
-    """
-    Any position on the world grid.
-    """
+from simulation.action import MoveAction
+from simulation.location import Location
+from simulation.cell import Cell
 
-    def __init__(self, location, habitable=True, generates_score=False, partially_fogged=False):
-        self.location = location
-        self.habitable = habitable
-        self.generates_score = generates_score
-        self.avatar = None
-        self.pickup = None
-        self.partially_fogged = partially_fogged
-        self.actions = []
-
-        # Used to update the map features in the current view of the user (score points on pickups).
-        self.remove_from_scene = None
-        self.add_to_scene = None
-
-    def __repr__(self):
-        return 'Cell({} h={} s={} a={} p={} f{})'.format(
-            self.location, self.habitable, self.generates_score, self.avatar, self.pickup, self.partially_fogged)
-
-    def __eq__(self, other):
-        return self.location == other.location
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __hash__(self):
-        return hash(self.location)
-
-    @property
-    def moves(self):
-        return [move for move in self.actions if isinstance(move, MoveAction)]
-
-    @property
-    def is_occupied(self):
-        return self.avatar is not None
-
-    def serialise(self):
-        if self.partially_fogged:
-            return {
-                'generates_score': self.generates_score,
-                'location': self.location.serialise(),
-                'partially_fogged': self.partially_fogged
-            }
-        else:
-            return {
-                'avatar': self.avatar.serialise() if self.avatar else None,
-                'generates_score': self.generates_score,
-                'habitable': self.habitable,
-                'location': self.location.serialise(),
-                'pickup': self.pickup.serialise() if self.pickup else None,
-                'partially_fogged': self.partially_fogged
-            }
-
+LOGGER = getLogger(__name__)
 
 class WorldMap(object):
     """
