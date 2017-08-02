a28c2fb5194c0ef8121645b87b9ca8580339e667
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-08-02 19:36:43 +0100

    Hardcoded sprite JSON in custom maps and incremented check complete number.

--- aimmo-game/simulation/levels/completion_checks.py
+++ aimmo-game/simulation/levels/completion_checks.py
@@ -4,7 +4,7 @@ def check_complete(self, game_state):
     except KeyError:
         return False
 
-    return main_avatar.score > 24
+    return main_avatar.score > 24023482
 
 COMPLETION_CHECKS = {
     "level1" : check_complete,
