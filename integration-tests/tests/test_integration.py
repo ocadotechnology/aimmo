0fc6dd80e66f545506e2060d74486c0753edde08
Olaf Szmidt <szmidtolaf@gmail.com>
2017-11-24 11:20:24 +0000

    Omit output from the run script.

--- integration-tests/tests/test_integration.py
+++ integration-tests/tests/test_integration.py
@@ -8,40 +8,41 @@ from connection_set_up import (create_session, send_get_request, send_post_reque
                                obtain_csrftoken, delete_old_database)
 
 
-# """
-# A test that will run on a clean & empty database, create all migrations, new
-# browser session and passes a CSRF token with the POST input request.
-#
-# Server gets killed at the end of the test.
-# :return:
-# """
-url = 'http://localhost:8000/players/accounts/login/'
+class TestIntegration(TestCase):
+    def test_default(self):
+        """
+        A test that will run on a clean & empty database, create all migrations, new
+        browser session and passes a CSRF token with the POST input request.
+        
+        Server gets killed at the end of the test.
+        """
+        url = 'http://localhost:8000/players/accounts/login/'
 
-delete_old_database()
+        delete_old_database()
 
-subprocess.Popen(["python",  "../../run.py"])
-time.sleep(15)
+        subprocess.Popen(["python",  "../run.py"], stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+        time.sleep(15)
 
-print("Creating session...")
-session = create_session()
+        print("Creating session...")
+        session = create_session()
 
-print ("Starting GET request loop...")
-get_response = send_get_request(session, url)
+        print ("Starting GET request loop...")
+        get_response = send_get_request(session, url)
 
-print("Obtaining CSRF Token...")
-csrftoken = obtain_csrftoken(session)
+        print("Obtaining CSRF Token...")
+        csrftoken = obtain_csrftoken(session)
 
-login_info = {
-    'username': 'admin',
-    'password': 'admin',
-    'csrfmiddlewaretoken': csrftoken,
-}
+        login_info = {
+            'username': 'admin',
+            'password': 'admin',
+            'csrfmiddlewaretoken': csrftoken,
+        }
 
-print("Sending post response...")
+        print("Sending post response...")
 
-send_post_request(session, url, login_info)
+        send_post_request(session, url, login_info)
 
-
-os.killpg(0, signal.SIGTERM)
-time.sleep(0.9)
-os.killpg(0, signal.SIGKILL)
+        os.killpg(0, signal.SIGTERM)
+        time.sleep(0.9)
+        os.killpg(0, signal.SIGKILL)
