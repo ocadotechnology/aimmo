ab366c2ca25cb907036c5890f3872db05367478b
Olaf Szmidt <szmidtolaf@gmail.com>
2018-02-22 16:30:52 +0000

    Add a snapshot sample test

--- game_frontend/src/components/Movie/index.test.js
+++ game_frontend/src/components/Movie/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 import Movie from 'components/Movie'
+import renderer from 'react-test-renderer'
 
 // The following 3 lines should be moved to a setup file
 import { configure } from 'enzyme';
@@ -19,25 +20,24 @@ describe('<Movie />', () => {
         const wrapper = shallow(<Movie movie={singleMovie} />);
         expect(wrapper.text()).toContain('Test Title')
     })
-})
 
-describe('<Movie />', () => {
     it('should contain the string Test Director', () => {
         const wrapper = shallow(<Movie movie={singleMovie} />);
         expect(wrapper.text()).toContain('Test Director')
     })
-})
 
-describe('<Movie />', () => {
     it('should contain the string Test Producer', () => {
         const wrapper = shallow(<Movie movie={singleMovie} />);
         expect(wrapper.text()).toContain('Test Producer')
     })
-})
 
-describe('<Movie />', () => {
     it('should contain the string 20/03/2018', () => {
         const wrapper = shallow(<Movie movie={singleMovie} />);
         expect(wrapper.text()).toContain('20/03/2018')
+    })
+
+    it('renders correctly', () => {
+        const tree = renderer.create(<Movie movie={singleMovie} />).toJSON();
+        expect(tree).toMatchSnapshot();
     })
 })
