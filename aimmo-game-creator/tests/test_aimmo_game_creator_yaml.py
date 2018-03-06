2e64514ff00d9b4e95e95dc64207fad927b2c17d
Olaf Szmidt <szmidtolaf@gmail.com>
2018-03-06 16:55:22 +0000

    Add test file with a simple test for replace me string

---
+++ aimmo-game-creator/tests/test_aimmo_game_creator_yaml.py
@@ -0,0 +1,20 @@
+import logging
+from unittest import TestCase
+
+import yaml
+
+logging.basicConfig(level=logging.WARNING)
+
+
+class TestAimmoGameCreatorYaml(TestCase):
+
+    def setUp(self):
+        with open("rc-aimmo-game-creator.yaml", 'r') as stream:
+            try:
+                self.yaml_dict = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                logging.debug(exc)
+
+    def test_REPLACE_ME_string_exists(self):
+        self.assertEqual(self.yaml_dict['spec']['template']['spec']['containers'][0]['env'][1]['value'],
+                         "REPLACE_ME/api/games")
