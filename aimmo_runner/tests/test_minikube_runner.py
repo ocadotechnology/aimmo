6d9525544d2f297717ad5e54d09899d7e9d6d91f
Olaf Szmidt <szmidtolaf@gmail.com>
2018-03-07 11:25:29 +0000

    Write minikube tests

---
+++ aimmo_runner/tests/test_minikube_runner.py
@@ -0,0 +1,26 @@
+import logging
+import mock
+from unittest import TestCase
+from aimmo_runner import minikube
+
+logging.basicConfig(level=logging.WARNING)
+
+
+class TestMinikubeRunner(TestCase):
+
+    @mock.patch('aimmo_runner.minikube.get_ip', return_value='127.0.0.1')
+    def test_get_ip(self, mocked_get_ip_func):
+        """
+        In minikube mode we want this to return localhost for other utilities to work.
+        """
+        self.assertEqual(minikube.get_ip(), '127.0.0.1')
+
+    @mock.patch('aimmo_runner.minikube.get_ip', return_value='127.0.0.1')
+    def test_game_creator_function_creates_correct_game_url(self, mocked_get_ip_func):
+        """
+        Checks if the game creator yaml is created with the correct game API URL. Relies on get_ip()
+        which we expect to fall back to localhost always.
+        """
+        created_yaml = minikube.create_creator_yaml()
+        self.assertEqual(created_yaml['spec']['template']['spec']['containers'][0]['env'][1]['value'],
+                         'http://127.0.0.1:8000/players/api/games')
