c024e94cc0f4202757712df4a3c6471067af6193
Niket Shah <masterniket@gmail.com>
2018-04-04 15:58:51 +0100

    Use shallow rendering for the IDE component

--- game_frontend/src/components/IDE/index.test.js
+++ game_frontend/src/components/IDE/index.test.js
@@ -1,11 +1,12 @@
 /* eslint-env jest */
 import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import IDE from 'components/IDE'
-import renderer from 'react-test-renderer'
 
 describe('<IDE />', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<IDE></IDE>).toJSON()
-    expect(tree).toMatchSnapshot()
+    const tree = shallow(<IDE />)
+    expect(toJson(tree)).toMatchSnapshot()
   })
 })
