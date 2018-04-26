5cd21342c99844e0bfeb9a5a7f3c503e1dd8687b
OlafSzmidt <szmidtolaf@gmail.com>
2018-04-26 10:15:34 +0100

    Add front-end component for AceEditor

--- game_frontend/src/components/IDEEditor/index.js
+++ game_frontend/src/components/IDEEditor/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import AceEditor from 'react-ace';
 
 const IDEEditorLayout = styled.div`
   background-color: #2F4F4F
@@ -11,7 +12,24 @@ export default class IDEEditor extends Component {
   render () {
     return (
       <IDEEditorLayout>
-        <p>{this.props.code}</p>
+          <AceEditor
+              mode="javascript"
+              theme="monokai"
+              name="blah2"
+              onLoad={this.onLoad}
+              onChange={this.onChange}
+              fontSize={14}
+              showPrintMargin={true}
+              showGutter={true}
+              highlightActiveLine={true}
+              value={`TODO`}
+              setOptions={{
+                  enableBasicAutocompletion: true,
+                  enableLiveAutocompletion: true,
+                  enableSnippets: false,
+                  showLineNumbers: true,
+                  tabSize: 2,
+              }}/>
       </IDEEditorLayout>
     )
   }
