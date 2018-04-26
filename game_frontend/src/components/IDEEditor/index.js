a981ed74e0343e8f52b388267153f9122e02bacb
Ria Jha <ria.jha@ocado.com>
2018-04-26 11:26:21 +0100

    Modified theme of code editor

--- game_frontend/src/components/IDEEditor/index.js
+++ game_frontend/src/components/IDEEditor/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import AceEditor from 'react-ace';
+import AceEditor from 'react-ace'
+import 'brace/theme/idle_fingers'
 
 const IDEEditorLayout = styled.div`
   background-color: #2F4F4F
@@ -12,29 +12,27 @@ export default class IDEEditor extends Component {
   render () {
     return (
       <IDEEditorLayout>
-          <AceEditor
-              mode="javascript"
-              theme="monokai"
-              name="blah2"
-              onLoad={this.onLoad}
-              onChange={this.onChange}
-              fontSize={14}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              value={`TODO`}
-              setOptions={{
-                  enableBasicAutocompletion: true,
-                  enableLiveAutocompletion: true,
-                  enableSnippets: false,
-                  showLineNumbers: true,
-                  tabSize: 2,
-              }}/>
+        <AceEditor
+          mode='javascript'
+          theme='idle_fingers'
+          name='blah2'
+          onLoad={this.onLoad}
+          onChange={this.onChange}
+          fontSize={14}
+          showPrintMargin
+          showGutter
+          highlightActiveLine
+          value={`TODO`}
+          width='100%'
+          height='100%'
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2
+          }} />
       </IDEEditorLayout>
     )
   }
-}
-
-IDEEditor.propTypes = {
-  code: PropTypes.string
 }
