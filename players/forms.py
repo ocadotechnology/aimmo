9f2d6c4c5e3c943815b0374118a7048bef4ab2f0
Olaf Szmidt <szmidtolaf@gmail.com>
2018-05-15 11:52:44 +0100

    Change default settings. Solves #567 (#578)
    * Change default settings. Solves #567
    
    * Add a blank file that was removed.

--- players/forms.py
+++ players/forms.py
@@ -6,4 +6,9 @@ from players.models import Game
 class AddGameForm(ModelForm):
     class Meta:
         model = Game
-        exclude = ['Main', 'owner', 'auth_token', 'completed', 'main_user', 'static_data', 'can_play']
+        exclude = ['Main', 'owner', 'auth_token', 'completed', 'main_user', 'static_data', 'can_play',
+                   'public', 'generator', 'target_num_cells_per_avatar',
+                   'target_num_score_locations_per_avatar', 'score_despawn_chance',
+                   'target_num_pickups_per_avatar', 'pickup_spawn_chance', 'obstacle_ratio',
+                   'start_height', 'start_width'
+                   ]
