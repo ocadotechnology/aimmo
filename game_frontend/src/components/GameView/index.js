46ddd15854ba2aff46f455fbb761bc6a64a29a31
Olaf Szmidt <szmidtolaf@gmail.com>
2018-05-24 17:13:24 +0100

    Introduce new unity module to the component

--- game_frontend/src/components/GameView/index.js
+++ game_frontend/src/components/GameView/index.js
@@ -1,8 +1,22 @@
 import styled from 'styled-components'
+import React, { Component } from 'react'
+import Unity from "react-unity-webgl"
 
-const GameView = styled.div`
+const GameViewLayout = styled.div`
   background-color: purple
   grid-area: game-view
 `
 
+export class GameView extends Component {
+  render() {
+    return (
+      <GameViewLayout>
+        <Unity
+          src="TODO"
+          loader="TODO"
+        />
+      </GameViewLayout>
+    )
+  }
+}
 export default GameView
