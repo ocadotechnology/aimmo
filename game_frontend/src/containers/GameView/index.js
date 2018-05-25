c00f05fa78db0138596757a98982246b77901d67
Olaf Szmidt <szmidtolaf@gmail.com>
2018-05-25 10:24:24 +0100

    Add some extra functionality local to the file for now

--- game_frontend/src/containers/GameView/index.js
+++ game_frontend/src/containers/GameView/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import React, { Component } from 'react'
-import Unity, { RegisterExternalListener } from "react-unity-webgl"
+import Unity, { RegisterExternalListener, UnityEvent } from "react-unity-webgl"
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
@@ -13,11 +13,24 @@ export class GameView extends Component {
   constructor() {
     // It's recommended to register the callback before loading Unity.
     super()
+    console.log("constructor")
     RegisterExternalListener("SendAllConnect", this.sendAllConnect.bind(this))
+    // TODO: probably move below to epics or something
+    this.setGameURL = new UnityEvent("World Controller", "SetGameURL")
+    this.setGamePort = new UnityEvent("World Controller", "SetGamePort")
+    this.setGamePath = new UnityEvent("World Controller", "SetGamePath")
+    this.setSSL = new UnityEvent("World Controller", "SetSSL")
+    this.establishConnection = new UnityEvent("World Controller", "EstablishConnection")
   }
 
   sendAllConnect() {
     console.log("sendAllConnect hit")
+    this.setGameURL("test")
+    this.setGamePort(0)
+    this.setGamePath("/yo")
+    this.setSSL("True")
+    this.establishConnection()
+    console.log("finished")
   }
   
   render() {
@@ -31,4 +44,9 @@ export class GameView extends Component {
     )
   }
 }
+
+const mapStateToProps = () => ({})
+
+const mapDispatchToProps = {}
+
 export default connect(mapStateToProps, mapDispatchToProps)(GameView)
