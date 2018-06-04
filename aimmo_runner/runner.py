739dbe2001d3066ff0e6cd7c236eb22113aa6c1c
Ria Jha <ria.jha@ocado.com>
2018-06-04 15:43:49 +0100

    Testing again

--- aimmo_runner/runner.py
+++ aimmo_runner/runner.py
@@ -39,8 +39,7 @@ def run(use_minikube, server_wait=True, capture_output=False, test_env=False):
 
     run_command(['pip', 'install', '-e', ROOT_DIR_LOCATION], capture_output=capture_output)
     run_command(['pip', 'install', 'codeforlife-portal'], capture_output=capture_output)
-    if not test_env:
-        run_command(['python', _MANAGE_PY, 'migrate', '--noinput'], capture_output=capture_output)
+    run_command(['python', _MANAGE_PY, 'migrate', '--noinput'], capture_output=capture_output)
     run_command(['python', _MANAGE_PY, 'collectstatic', '--noinput'], capture_output=capture_output)
 
     create_superuser_if_missing(username='admin', password='admin')
