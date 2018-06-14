7500f1a5cfe34209aba64f7c8aeee3dd39b081b7
Olaf Szmidt <szmidtolaf@gmail.com>
2018-06-14 16:02:27 +0100

    Add reducer test for all current cases

---
+++ game_frontend/src/redux/features/Game/reducers.test.js
@@ -0,0 +1,20 @@
+/* eslint-env jest */
+import actions from './actions'
+import gameReducer from './reducers';
+
+describe('gameReducer', () => {
+  it('should return the initial state', () => {
+    expect(gameReducer(undefined, {})).toEqual({})
+  })
+
+  it('should handle GET_CONNECTION_PARAMS_SUCCESS', () => {
+    const expectedState = {
+      connectionParams: {
+          id: 1
+      },
+      initialState: "someValue"
+    }
+    const action = actions.getConnectionParamsSuccess({ id: 1 })
+    expect(gameReducer({initialState: "someValue"}, action)).toEqual(expectedState)
+  })
+})
