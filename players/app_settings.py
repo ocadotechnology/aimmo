a5e4136eeea95c217c9b61290d4eecd89e25883f
Ria Jha <ria.jha@ocado.com>
2018-06-18 16:39:26 +0100

    Added comments for both functions in app_settings that import things from portal

--- players/app_settings.py
+++ players/app_settings.py
@@ -12,6 +12,12 @@ USERS_FOR_NEW_AIMMO_GAME = getattr(settings, 'USERS_FOR_NEW_AIMMO_GAME', None)
 
 
 def get_aimmo_preview_user_decorator():
+    """
+    This function is used to import a decorator from portal, which
+    checks whether the logged in user is a preview user.
+
+    :return: A function decorator
+    """
     if PREVIEW_USER_AIMMO_DECORATOR:
         func = import_string(PREVIEW_USER_AIMMO_DECORATOR)
         return func
@@ -19,6 +25,12 @@ def get_aimmo_preview_user_decorator():
 
 
 def get_users_for_new_game(request):
+    """
+    Imports and calls a function defined in portal, that decides
+    which users should be added to the newly created game.
+    :param request:
+    :return: List of User objects
+    """
     if USERS_FOR_NEW_AIMMO_GAME:
         func = import_string(USERS_FOR_NEW_AIMMO_GAME)
         return func(request)
