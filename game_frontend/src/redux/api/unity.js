9215351635faa17e8e9ae89be04d2fceec707019
Olaf Szmidt <szmidtolaf@gmail.com>
2018-06-20 17:02:44 +0100

    Fix indentation and other eslint issues in unity

--- game_frontend/src/redux/api/unity.js
+++ game_frontend/src/redux/api/unity.js
@@ -1,33 +1,32 @@
-import { UnityEvent } from "react-unity-webgl";
+import { UnityEvent } from 'react-unity-webgl';
 import { Observable } from 'rxjs'
 import { map, mergeMap, catchError, tap } from 'rxjs/operators'
 import { ofType } from 'redux-observable'
 import types from '../features/Game/types'
 import actions from '../features/Game/types'
 
-const sendUnityEvent = action$ => 
-    action$.mergeMap(action =>
-        Observable.of(action).pipe(
-            emitToUnity,
-            map(event => action.payload.successAction),
-            catchError(error => Observable.of(action.payload.failAction))
-        )
+const sendUnityEvent = action$ =>
+  action$.mergeMap(action =>
+    Observable.of(action).pipe(
+      emitToUnity,
+      map(event => action.payload.successAction),
+      catchError(error => Observable.of(action.payload.failAction))
     )
+  )
 
+const emitToUnity = action$ =>
+  action$.map(
+    action => {
+      let unityEvent = new UnityEvent('World Controller', action.payload.unityEvent)
 
-const emitToUnity = action$ => 
-    action$.map(
-        action => {
-            let unityEvent = new UnityEvent("World Controller", action.payload.unityEvent)
+      if (unityEvent.canEmit()) {
+        unityEvent.emit(action.payload.unityData)
+      } else {
+        throw 'Cannot emit the function!'
+      }
+    }
+  )
 
-            if(unityEvent.canEmit()) {
-                unityEvent.emit(action.payload.unityData)
-            } else {
-                throw "Cannot emit the function!" 
-            }
-        }
-    )
-
-export default { 
-    sendUnityEvent
+export default {
+  sendUnityEvent
 }
