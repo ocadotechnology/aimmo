d4ccf15bda77c3f45ab6b1e66c6909dae530a9d5
Olaf Szmidt <szmidtolaf@gmail.com>
2018-06-22 10:30:56 +0100

    Change the djangoBundler check environment before setting vars

--- game_frontend/djangoBundler.js
+++ game_frontend/djangoBundler.js
@@ -11,9 +11,9 @@ const options = {
   outFile: 'index.html',
   publicUrl: './',
   watch: process.env.NODE_ENV !== 'production',
-  minify: false,
+  minify: process.env.NODE_ENV === 'production',
   target: 'browser',
-  cache: false
+  cache: process.env.NODE_ENV === 'production'
 }
 
 const templateFolder = Path.resolve(Path.join(__dirname, '../players/templates/players'))
@@ -24,7 +24,7 @@ bundler.on('bundled', (bundle) => {
   let entryPointHTML = shell.cat(bundle.name).stdout
   entryPointHTML = '{% load static %}\n' + entryPointHTML
   entryPointHTML = entryPointHTML.replace(/(<script src=")(.*\.js)("><\/script>)/g, '$1{% static "react/$2" %}$3')
-  entryPointHTML += "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.4/socket.io.min.js\"></script>"
+  entryPointHTML += '<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.4/socket.io.min.js"></script>'
   fs.writeFile(`${templateFolder}/game_ide.html`, entryPointHTML, (error) => {
     if (error) { return console.log(error) }
     console.log('game_ide.html django template generated sucessfully')
