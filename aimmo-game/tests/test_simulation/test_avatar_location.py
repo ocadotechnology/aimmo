ba0352d521faec8d0d2e21473c869c068cc2d961
Niket Shah <masterniket@gmail.com>
2018-07-10 18:22:47 +0100

    Add messy test that tests avatar issue

--- aimmo-game/tests/test_simulation/test_avatar_location.py
+++ aimmo-game/tests/test_simulation/test_avatar_location.py
@@ -1,30 +1,30 @@
 import unittest
 import random
 from simulation.communicator import Communicator
-# from simulation.worker_manager import LocalWorkerManager
-# from simulation.turn_manager import ConcurrentTurnManager
-# from simulation.map_generator import Main
-# from simulation.avatar.avatar_manager import AvatarManager
-# from .test_worker_manager import ConcreteWorkerManager
+from simulation.worker_manager import WorkerManager
+from simulation.turn_manager import ConcurrentTurnManager
+from simulation.map_generator import Main
+from simulation.avatar.avatar_manager import AvatarManager
+from .concrete_worker_manager import ConcreteWorkerManager
 
 class MockCommunicator(Communicator):
     def __init__(self):
-        pass
-
-    def get_game_metadata(self):
-        data = {
+        self.data = {
             "main": {
                 "parameters": [],
                 "main_avatar": 1,
                 "users": [
                     {
                         "id": 1,
-                        "code": "class Avatar..."
+                        "code": "class Avatar:"
                     }
                 ]
             }
         }
-        return data
+        pass
+
+    def get_game_metadata(self):
+        return self.data
 
     def mark_game_complete(self, data=None):
         return {}
@@ -33,17 +33,22 @@ class MockCommunicator(Communicator):
 class TestAvatarLocation(unittest.TestCase):
 
     def test_wait_action(self):
-        pass
-        # settings = {'START_WIDTH': 3, 'START_HEIGHT': 3, 'OBSTACLE_RATIO': 0}
-        # map_generator = Main(settings)
-        # player_manager = AvatarManager()
-        # communicator = MockCommunicator()
-        # game_state = map_generator.get_game_state(player_manager)
-        # worker_manager = ConcreteWorkerManager(game_state=game_state,communicator=communicator)
-        # turn_manager = ConcurrentTurnManager(game_state=game_state, end_turn_callback = lambda: None,communicator=communicator)
-        # random.seed(0)
-        # worker_manager.update()
-        # turn_manager._run_single_turn()
+        settings = {'START_WIDTH': 3, 'START_HEIGHT': 3, 'OBSTACLE_RATIO': 0}
+        map_generator = Main(settings)
+        player_manager = AvatarManager()
+        mock_communicator = MockCommunicator()
+        game_state = map_generator.get_game_state(player_manager)
+        worker_manager = ConcreteWorkerManager(game_state=game_state, communicator=mock_communicator)
+        turn_manager = ConcurrentTurnManager(game_state=game_state, end_turn_callback = lambda: None, communicator=mock_communicator)
+        random.seed(0)
+        worker_manager.update()
+        turn_manager._run_single_turn()
+        state_before_code_change = game_state.get_state_for(game_state.avatar_manager.active_avatars[0])
+        mock_communicator.data["main"]["users"][0]["code"] = "class Avatar: different code"
+        worker_manager.update()
+        turn_manager._run_single_turn()
+        state_after_code_change = game_state.get_state_for(game_state.avatar_manager.active_avatars[0])
+        self.assertEqual(state_before_code_change, state_after_code_change)
 
 
 
--- aimmo-game/tests/test_simulation/test_worker_manager.py
+++ aimmo-game/tests/test_simulation/test_worker_manager.py
@@ -9,29 +9,7 @@ from simulation.avatar.avatar_manager import AvatarManager
 from simulation.game_state import GameState
 from simulation.worker_manager import WorkerManager
 from .maps import InfiniteMap
-
-
-class ConcreteWorkerManager(WorkerManager):
-    def __init__(self, *args, **kwargs):
-        self.final_workers = set()
-        self.clear()
-        super(ConcreteWorkerManager, self).__init__(*args, **kwargs)
-
-    def clear(self):
-        self.removed_workers = []
-        self.added_workers = []
-
-    def create_worker(self, player_id):
-        self.added_workers.append(player_id)
-        self.final_workers.add(player_id)
-
-    def remove_worker(self, player_id):
-        self.removed_workers.append(player_id)
-        try:
-            self.final_workers.remove(player_id)
-        except KeyError:
-            pass
-
+from .concrete_worker_manager import ConcreteWorkerManager
 
 class RequestMock(object):
     def __init__(self, num_users):
