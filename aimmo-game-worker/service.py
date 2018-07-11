5657ead07a21cce00e9b9ddbc89ed16cab417a7b
Olaf Szmidt <szmidtolaf@gmail.com>
2018-07-11 14:27:05 +0100

    Refactor options out as they're not used and catch errors

--- aimmo-game-worker/service.py
+++ aimmo-game-worker/service.py
@@ -7,6 +7,7 @@ import flask
 
 from simulation.avatar_state import AvatarState
 from simulation.world_map import WorldMap
+from simulation.action import WaitAction
 
 app = flask.Flask(__name__)
 LOGGER = logging.getLogger(__name__)
@@ -19,26 +20,29 @@ def process_turn():
     LOGGER.info('Calculating action')
     data = flask.request.get_json()
 
-    world_map = WorldMap(**data['world_map'])
-    avatar_state = AvatarState(**data['avatar_state'])
+    try:
+        from avatar import Avatar
+        global worker_avatar
+        worker_avatar = Avatar()
 
-    action = worker_avatar.handle_turn(avatar_state, world_map)
+        world_map = WorldMap(**data['world_map'])
+        avatar_state = AvatarState(**data['avatar_state'])
+
+        action = worker_avatar.handle_turn(avatar_state, world_map)
+    except Exception as e:
+        LOGGER.info("Do stuff later with the errored code.")
+        LOGGER.info(e)
+        action = WaitAction()
 
     return flask.jsonify(action=action.serialise())
 
 
-def run(host, port, directory):
+def run(host, port):
     logging.basicConfig(level=logging.DEBUG)
-
-    with open('{}/options.json'.format(directory)) as option_file:
-        options = json.load(option_file)
-    from avatar import Avatar
-    global worker_avatar
-    worker_avatar = Avatar(**options)
 
     app.config['DEBUG'] = False
     app.run(host, port)
 
 
 if __name__ == '__main__':
-    run(host=sys.argv[1], port=int(sys.argv[2]), directory=sys.argv[3])
+    run(host=sys.argv[1], port=int(sys.argv[2]))
