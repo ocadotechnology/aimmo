686fc3f7189346e142a094ffe6c4673a5002535c
Niall Egan <negan1998@gmail.com>
2018-07-18 20:01:48 +0100

    Send logs from worker (#677)
    * Send logs from worker
    
    minor change
    
    * Updated test
    
    * Removed logger

--- aimmo-game-worker/service.py
+++ aimmo-game-worker/service.py
@@ -21,9 +21,9 @@ def process_turn():
     world_map = WorldMap(**data['world_map'])
     avatar_state = AvatarState(**data['avatar_state'])
 
-    action = avatar_runner.process_avatar_turn(world_map, avatar_state)
+    action, logs = avatar_runner.process_avatar_turn(world_map, avatar_state)
 
-    return flask.jsonify(action=action.serialise())
+    return flask.jsonify(action=action.serialise(), logs=logs)
 
 
 def run(host, port):
