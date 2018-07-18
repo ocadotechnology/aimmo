c6e98608f70d01c6c9bda96ade79927778adf805
Niall Egan <negan1998@gmail.com>
2018-07-18 16:25:22 +0100

    Send logs from worker
    minor change

--- aimmo-game-worker/avatar_runner.py
+++ aimmo-game-worker/avatar_runner.py
@@ -1,6 +1,11 @@
 import logging
 import traceback
+import sys
+
+from six import StringIO
+
 from simulation.action import WaitAction
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,15 +15,26 @@ class AvatarRunner(object):
         self.avatar = avatar
 
     def process_avatar_turn(self, world_map, avatar_state):
+        output_log = StringIO()
+
         try:
+            sys.stdout = output_log
+            sys.stderr = output_log
             if self.avatar is None:
                 from avatar import Avatar
                 self.avatar = Avatar()
-            return self.avatar.handle_turn(world_map, avatar_state)
+
+            action = self.avatar.handle_turn(world_map, avatar_state)
 
         except Exception as e:
+            traceback.print_exc()
             LOGGER.error("Code failed to run")
-            LOGGER.error(traceback.print_exc())
+            LOGGER.error(e)
             action = WaitAction()
 
-        return action
+        finally:
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
+
+        logs = output_log.getvalue()
+        return action, logs
