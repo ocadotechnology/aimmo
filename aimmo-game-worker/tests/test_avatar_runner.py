20783568ccb91237a938e1ae576d6bc856bcb747
Olaf Szmidt <szmidtolaf@gmail.com>
2018-07-18 13:33:24 +0100

    Fix #673

--- aimmo-game-worker/tests/test_avatar_runner.py
+++ aimmo-game-worker/tests/test_avatar_runner.py
@@ -7,10 +7,10 @@ class TestAvatarRunner(TestCase):
 
     def test_runner_does_not_crash_on_code_errors(self):
         class Avatar(object):
-            def handle_turn(self, world_map, avatar_state):
+            def process_avatar_turn(self, world_map, avatar_state):
                 assert False
                 return None
 
         runner = AvatarRunner(Avatar())
-        action = runner.handle_turn({}, {})
+        action = runner.process_avatar_turn({}, {})
         self.assertIsInstance(action, WaitAction)
