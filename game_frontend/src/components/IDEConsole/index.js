bf4d225f7651bd350de846909eb9ff421b60009f
Niket Shah <masterniket@gmail.com>
2018-07-30 10:50:02 +0100

    make initial console presentational component

--- game_frontend/src/components/IDEConsole/index.js
+++ game_frontend/src/components/IDEConsole/index.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components'
+import React, { Component } from 'react'
 
-export const IDEConsole = styled.div`
-  background-color: ${props => props.theme.palette.background.default}
-  grid-area: ide-console
+export const IDEConsoleLayout = styled.div`
+  grid-area: ide-console;
+  color: ${props => props.theme.palette.text.primary};
+  background-color: ${props => props.theme.palette.background.default};
+  font-family: ${props => props.theme.additionalVariables.typography.code.fontFamily};
+  overflow: auto;
 `
 
-export default IDEConsole
+export default class IDEConsole extends Component {
+  render () {
+    return (
+      <IDEConsoleLayout>
+        <p>{ this.props.logs }</p>
+      </IDEConsoleLayout>
+    )
+  }
+}
--- game_frontend/src/components/IDEConsole/index.test.js
+++ game_frontend/src/components/IDEConsole/index.test.js
@@ -4,7 +4,12 @@ import IDEConsole from 'components/IDEConsole'
 import createShallowWithTheme from 'testHelpers/createShallow'
 
 describe('<IDEConsole />', () => {
-  it('renders correctly', () => {
+  it('renders correctly with logs', () => {
+    const tree = createShallowWithTheme(<IDEConsole logs='test logs' />)
+    expect(tree).toMatchSnapshot()
+  })
+  
+  it('renders correctly without logs', () => {
     const tree = createShallowWithTheme(<IDEConsole />)
     expect(tree).toMatchSnapshot()
   })
