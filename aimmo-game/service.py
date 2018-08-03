f8d4e18358050ad37c3e7163798c27eb7330cd65
Olaf Szmidt <szmidtolaf@gmail.com>
2018-08-03 11:44:18 +0100

    Leaking data structures between tests (#707)
    * Introduce state_provider injection
    
    * Refactor game state provider into a new file
    
    * Mis-spelt import causing tests to fail
    
    * Missed out import
    
    * Possible path change for import
    
    * Refactor globals into providers instances instead
    
    * New getter for logs
    
    * More refactoring for tests
    
    * Take care of session id to avatar id mappings
    
    * Remove static typing of Python 2 sheninigans
    
    * Add package-lock.json back
    
    * Rename globals to not be called globals =)
    
    * Remove irrelevant comment as its already documented in issues

--- aimmo-game/service.py
+++ aimmo-game/service.py
@@ -10,8 +10,11 @@ import flask
 import socketio as SocketIO
 
 from flask_cors import CORS
+
 from simulation import map_generator
-from simulation.turn_manager import state_provider, logs_provider, ConcurrentTurnManager
+from simulation.turn_manager import ConcurrentTurnManager
+from simulation.logs_provider import LogsProvider
+from simulation.game_state_provider import GameStateProvider
 from simulation.avatar.avatar_manager import AvatarManager
 from simulation.worker_managers import WORKER_MANAGERS
 from simulation.pickups import pickups_update
@@ -27,8 +30,10 @@ socketio = SocketIO.Server()
 LOGGER = logging.getLogger(__name__)
 
 worker_manager = None
+state_provider = None
+logs_provider = None
+session_id_to_avatar_id_mappings = {}
 
-session_id_to_avatar_id = {}
 USER_WATCHING_GAME = 0
 
 
@@ -57,7 +62,7 @@ def player_dict(avatar):
     }
 
 
-def get_game_state():
+def get_game_state(state_provider=state_provider):
     with state_provider as game_state:
         world_map = game_state.world_map
 
@@ -74,7 +79,8 @@ def get_game_state():
 
 
 @socketio.on('connect')
-def world_update_on_connect(sid, environ):
+def world_update_on_connect(sid, environ,
+                            session_id_to_avatar_id=session_id_to_avatar_id_mappings):
     socket_data = get_game_state()
     socket_data['logs'] = ''
     session_id_to_avatar_id[sid] = None
@@ -96,11 +102,11 @@ def world_update_on_connect(sid, environ):
     )
 
 
-def send_world_update():
+def send_world_update(session_id_to_avatar_id=session_id_to_avatar_id_mappings):
     socket_data = get_game_state()
 
     for sid, avatar_id in session_id_to_avatar_id.iteritems():
-        avatar_logs = logs_provider.get(avatar_id, '')
+        avatar_logs = logs_provider.get_user_logs(avatar_id)
         socket_data['logs'] = avatar_logs
 
         socketio.emit(
@@ -111,7 +117,8 @@ def send_world_update():
 
 
 @socketio.on('disconnect')
-def remove_session_id_from_mappings(sid):
+def remove_session_id_from_mappings(sid,
+                                    session_id_to_avatar_id=session_id_to_avatar_id_mappings):
     LOGGER.info("Socket disconnected for session id:{}. ".format(sid))
     try:
         del session_id_to_avatar_id[sid]
@@ -135,18 +142,22 @@ def player_data(player_id):
 
 
 def run_game(port):
-    global worker_manager
+    global worker_manager, state_provider, logs_provider
 
     print("Running game...")
     settings = pickle.loads(os.environ['settings'])
     api_url = os.environ.get('GAME_API_URL', 'http://localhost:8000/aimmo/api/games/')
     generator = getattr(map_generator, settings['GENERATOR'])(settings)
     player_manager = AvatarManager()
+    logs_provider = LogsProvider()
+    state_provider = GameStateProvider()
     communicator = Communicator(api_url=api_url, completion_url=api_url+'complete/')
     game_state = generator.get_game_state(player_manager)
     turn_manager = ConcurrentTurnManager(game_state=game_state,
                                          end_turn_callback=send_world_update,
-                                         communicator=communicator)
+                                         communicator=communicator,
+                                         state_provider=state_provider,
+                                         logs_provider=logs_provider)
     WorkerManagerClass = WORKER_MANAGERS[os.environ.get('WORKER_MANAGER', 'local')]
     worker_manager = WorkerManagerClass(game_state=game_state,
                                         communicator=communicator,
