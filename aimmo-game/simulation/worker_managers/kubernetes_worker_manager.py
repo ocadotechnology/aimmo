da59fffd322bcb6452ed80dbea275497dd21e135
Niall Egan <negan1998@gmail.com>
2018-08-07 11:16:07 +0100

    Working on configuration

--- aimmo-game/simulation/worker_managers/kubernetes_worker_manager.py
+++ aimmo-game/simulation/worker_managers/kubernetes_worker_manager.py
@@ -3,7 +3,8 @@ import os
 import time
 
 from pykube import HTTPClient, KubeConfig, Pod
-from kubernetes import client
+import kubernetes.client
+import kubernetes.config
 
 from .worker_manager import WorkerManager
 
@@ -16,38 +17,39 @@ class KubernetesWorkerManager(WorkerManager):
 
     def __init__(self, *args, **kwargs):
         #self.api = HTTPClient(KubeConfig.from_service_account())
-        self.api = client.CoreV1Api()
+        kubernetes.client.Configuration()
+        self.api = kubernetes.client.CoreV1Api()
         self.game_id = os.environ['GAME_ID']
         self.game_url = os.environ['GAME_URL']
         super(KubernetesWorkerManager, self).__init__(*args, **kwargs)
 
     def make_pod(self, player_id):
-        container = client.V1Container(
-                            env=[client.V1EnvVar(
+        container = kubernetes.client.V1Container(
+                            env=[kubernetes.client.V1EnvVar(
                                          name='DATA_URL',
                                          value='%s/player/%d' % (self.game_url, player_id))],
                             name='aimmo-game-worker',
                             image='ocadotechnology/aimmo-game-worker:%s' % os.environ.get('IMAGE_SUFFIX', 'latest'),
-                            ports=[client.V1ContainerPort(
+                            ports=[kubernetes.client.V1ContainerPort(
                                             containerPort=5000,
                                             protocol='TCP')],
-                            resources=client.V1ResourceRequirements(
+                            resources=kubernetes.client.V1ResourceRequirements(
                                     limits={'cpu': '10m', 'memory': '64Mi'},
                                     requests={'cpu': '7m', 'memory': '32Mi'}),
-                            securityContext=client.V1SecurityContext(
-                                    capabilities=client.V1Capabilities(
+                            securityContext=kubernetes.client.V1SecurityContext(
+                                    capabilities=kubernetes.client.V1Capabilities(
                                         drop=['all'],
                                         add=['NET_BIND_SERVICE'])))
-        pod_spec = client.V1PodSpec(containers=[container])
+        pod_spec = kubernetes.client.V1PodSpec(containers=[container])
 
-        metadata = client.V1ObjectMeta(
+        metadata = kubernetes.client.V1ObjectMeta(
                         labels={
                             'app': 'aimmo-game-worker',
                             'game': self.game_id,
                             'player': str(player_id)},
                         generate_name="aimmo-%s-worker-%s-" % (self.game_id, player_id))
 
-        return client.V1Pod(metadata=metadata, spec=pod_spec)
+        return kubernetes.client.V1Pod(metadata=metadata, spec=pod_spec)
 
     def create_worker(self, player_id):
         pod = self.api.create_namespace_pod(namespace=K8S_NAMESPACE, body=self.make_pod(player_id))
@@ -65,12 +67,11 @@ class KubernetesWorkerManager(WorkerManager):
 
     def remove_worker(self, player_id):
         pods = self.api.list_namespaced_pod(namespace=K8S_NAMESPACE,
-                                            selector='''
+                                            label_selector='''
                                                     'app': 'aimmo-game-worker',
                                                     'game': self.game_id,
-                                                    'player': {}'''.format(
-                                                    str(player_id)))
+                                                    'player': {}'''.format(player_id))
 
         for pod in pods:
             LOGGER.debug('Removing pod %s', pod.spec)
-            self.api.delete_namespaced_pod(pod.spec.name, K8S_NAMESPACE, client.V1DeleteOptions())
+            self.api.delete_namespaced_pod(pod.spec.name, K8S_NAMESPACE, kubernetes.client.V1DeleteOptions())
