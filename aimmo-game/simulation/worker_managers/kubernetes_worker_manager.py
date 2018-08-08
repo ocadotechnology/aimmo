ffb3c2d80516c14c3e7ffd9c77d802ac1fee0174
Niall Egan <negan1998@gmail.com>
2018-08-08 10:55:40 +0100

    error corrections

--- aimmo-game/simulation/worker_managers/kubernetes_worker_manager.py
+++ aimmo-game/simulation/worker_managers/kubernetes_worker_manager.py
@@ -17,7 +17,7 @@ class KubernetesWorkerManager(WorkerManager):
 
     def __init__(self, *args, **kwargs):
         #self.api = HTTPClient(KubeConfig.from_service_account())
-        kubernetes.client.Configuration()
+        kubernetes.config.load_incluster_config()
         self.api = kubernetes.client.CoreV1Api()
         self.game_id = os.environ['GAME_ID']
         self.game_url = os.environ['GAME_URL']
@@ -31,15 +31,15 @@ class KubernetesWorkerManager(WorkerManager):
                             name='aimmo-game-worker',
                             image='ocadotechnology/aimmo-game-worker:%s' % os.environ.get('IMAGE_SUFFIX', 'latest'),
                             ports=[kubernetes.client.V1ContainerPort(
-                                            containerPort=5000,
+                                            container_port=5000,
                                             protocol='TCP')],
                             resources=kubernetes.client.V1ResourceRequirements(
-                                    limits={'cpu': '10m', 'memory': '64Mi'},
-                                    requests={'cpu': '7m', 'memory': '32Mi'}),
-                            securityContext=kubernetes.client.V1SecurityContext(
-                                    capabilities=kubernetes.client.V1Capabilities(
-                                        drop=['all'],
-                                        add=['NET_BIND_SERVICE'])))
+                                            limits={'cpu': '10m', 'memory': '64Mi'},
+                                            requests={'cpu': '7m', 'memory': '32Mi'}),
+                            security_context=kubernetes.client.V1SecurityContext(
+                                                capabilities=kubernetes.client.V1Capabilities(
+                                                    drop=['all'],
+                                                    add=['NET_BIND_SERVICE'])))
         pod_spec = kubernetes.client.V1PodSpec(containers=[container])
 
         metadata = kubernetes.client.V1ObjectMeta(
@@ -52,12 +52,12 @@ class KubernetesWorkerManager(WorkerManager):
         return kubernetes.client.V1Pod(metadata=metadata, spec=pod_spec)
 
     def create_worker(self, player_id):
-        pod = self.api.create_namespace_pod(namespace=K8S_NAMESPACE, body=self.make_pod(player_id))
+        pod = self.api.create_namespaced_pod(namespace=K8S_NAMESPACE, body=self.make_pod(player_id))
 
         iterations = 0
         while pod.status.phase == 'Pending':
             if iterations > 30:
-                raise EnvironmentError('Could not start worker %s, details %s' % (player_id, pod.obj))
+                raise EnvironmentError('Could not start worker %s, details %s' % (player_id, pod))
             LOGGER.debug('Waiting for worker %s', player_id)
             time.sleep(5)
             iterations += 1
@@ -68,10 +68,10 @@ class KubernetesWorkerManager(WorkerManager):
     def remove_worker(self, player_id):
         pods = self.api.list_namespaced_pod(namespace=K8S_NAMESPACE,
                                             label_selector='''
-                                                    'app': 'aimmo-game-worker',
-                                                    'game': self.game_id,
-                                                    'player': {}'''.format(player_id))
+                                                     app=aimmo-game-worker,
+                                                     game=self.game_id,
+                                                     player={}'''.format(player_id))
 
-        for pod in pods:
-            LOGGER.debug('Removing pod %s', pod.spec)
+        for pod in pods.items:
+            LOGGER.error('Removing pod %s', pod.spec)
             self.api.delete_namespaced_pod(pod.spec.name, K8S_NAMESPACE, kubernetes.client.V1DeleteOptions())
