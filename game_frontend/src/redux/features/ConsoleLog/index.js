437ec34e9bbffb9dcda2391597705c70b80f2c78
Niket Shah <masterniket@gmail.com>
2018-08-13 09:17:42 +0100

    Connect console log up to the rootReducer

---
+++ game_frontend/src/redux/features/ConsoleLog/index.js
@@ -0,0 +1,6 @@
+import reducer from './reducers'
+
+export { default as consoleLogTypes } from './types'
+export { default as actions } from './actions'
+
+export default reducer
--- game_frontend/src/redux/features/index.js
+++ game_frontend/src/redux/features/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import { combineEpics } from 'redux-observable'
 import ghibliReducer, { movieEpics } from './GhibliMovies'
 import editorReducer, { editorEpics } from './Editor'
+import consoleLogReducer from './ConsoleLog'
 import gameReducer, { gameEpics } from './Game'
 
 const rootEpic = combineEpics(
@@ -13,7 +14,8 @@ const rootEpic = combineEpics(
 const rootReducer = combineReducers({
   ghibli: ghibliReducer,
   editor: editorReducer,
-  game: gameReducer
+  game: gameReducer,
+  consoleLog: consoleLogReducer
 })
 
 export {
