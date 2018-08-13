6356ebce5bc434bdfef3be115a7f7eb2abbf006b
Niket Shah <masterniket@gmail.com>
2018-08-13 09:10:57 +0100

    Complete tests for Console Log Reducer

--- game_frontend/src/redux/features/ConsoleLog/reducers.test.js
+++ game_frontend/src/redux/features/ConsoleLog/reducers.test.js
@@ -9,14 +9,25 @@ describe('consoleLogReducer', () => {
 
   it('should handle SOCKET_LOG_RECEIVED', () => {
     const expectedStateLog = 'Hello, good morning, I got here in my code'
+    
     const action = actions.socketConsoleLogReceived(expectedStateLog)
-    expect(consoleLogReducer(undefined, action).logs).toHaveLength(1)
-    expect(consoleLogReducer(undefined, action).logs[0].log).toEqual(expectedStateLog)
+    const actualState = consoleLogReducer(undefined, action)
+
+    expect(actualState.logs).toHaveLength(1)
+    expect(actualState.logs[0].log).toEqual(expectedStateLog)
   })
 
   it('should get rid of old logs past MAX_NUMBER_OF_STORED_LOGS', () => {
-    let initialLogs = []
+    let initialLogs = Array(MAX_NUMBER_OF_STORED_LOGS - 1).fill({ timestamp: '1', log: 'Same old logs' })
+    const newLogMessage = 'I\'m a new log!'
+    const ancientLogMessage = 'I\'m a new log!'
+    initialLogs.unshift({ timestamp: '0', log: ancientLogMessage })
     
-    const initialState = { logs}
+    const initialState = { logs: initialLogs }
+    const action = actions.socketConsoleLogReceived(newLogMessage)
+    const actualState = consoleLogReducer(initialState, action)
+
+    expect(actualState.logs).toHaveLength(MAX_NUMBER_OF_STORED_LOGS)
+    expect(actualState.logs[MAX_NUMBER_OF_STORED_LOGS - 1].log).toEqual(newLogMessage)
   })
 })
