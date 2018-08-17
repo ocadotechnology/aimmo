2b11073af15a259f5b5e57ffd3b0b7a08d0ee44e
Ria Jha <ria.jha@ocado.com>
2018-08-17 13:12:50 +0100

    Need to fix css

--- game_frontend/src/components/GameMenu/index.js
+++ game_frontend/src/components/GameMenu/index.js
@@ -1,20 +1,30 @@
 import styled from 'styled-components'
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
-import { CloseIcon } from '../icons'
+import CloseIcon from '../icons/Close.js'
 
 const GameMenuLayout = styled.nav`
   background-color: ${props => props.theme.palette.background.default}
   grid-area: game-menu
 `
 
+const ExitButton = styled(Button)`
+  position: relative;
+  float: right;
+  right: 40px;
+  top: 20px;
+`
+
 export default class GameMenu extends Component {
+  onClick () {
+    const url = window.location.href
+    window.location.href = url.substr(0, url.indexOf('play'))
+  }
   render () {
     return (
       <GameMenuLayout>
-        <Button
-          id='close-game-button'> <CloseIcon /> </Button>
+        <ExitButton
+          onClick={this.onClick.bind(this)}> <CloseIcon /> </ExitButton>
       </GameMenuLayout>
     )
   }
