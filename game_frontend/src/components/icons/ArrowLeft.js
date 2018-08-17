d8b0c5ff18eabe34c4d7117081004f1a9e37de53
Niket Shah <masterniket@gmail.com>
2018-08-17 08:26:13 +0100

    Add icons (#736)
    * Add icons

---
+++ game_frontend/src/components/icons/ArrowLeft.js
@@ -0,0 +1,10 @@
+import SvgIcon from '@material-ui/core/SvgIcon'
+import React from 'react'
+
+const ArrowLeftIcon = props => (
+  <SvgIcon {...props}>
+    <polygon id="path-1" points="14 7 9 12 14 17"></polygon>
+  </SvgIcon>
+)
+
+export default ArrowLeftIcon
---
+++ game_frontend/src/components/icons/ArrowRight.js
@@ -0,0 +1,10 @@
+import SvgIcon from '@material-ui/core/SvgIcon'
+import React from 'react'
+
+const ArrowRightIcon = props => (
+  <SvgIcon {...props}>
+    <polygon id="path-1" points="10 17 15 12 10 7"></polygon>
+  </SvgIcon>
+)
+
+export default ArrowRightIcon
