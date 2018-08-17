344481dccb0d6549aa27fa105ae581f3c1839dd8
Ria Jha <ria.jha@ocado.com>
2018-08-17 09:49:16 +0100

    close icon

--- game_frontend/src/components/GameMenu/index.js
+++ game_frontend/src/components/GameMenu/index.js
@@ -1,8 +1,21 @@
 import styled from 'styled-components'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import { CloseIcon } from '../icons'
 
-const GameMenu = styled.nav`
+const GameMenuLayout = styled.nav`
   background-color: ${props => props.theme.palette.background.default}
   grid-area: game-menu
 `
 
-export default GameMenu
+export default class GameMenu extends Component {
+  render () {
+    return (
+      <GameMenuLayout>
+        <Button
+          id='close-game-button'> <CloseIcon /> </Button>
+      </GameMenuLayout>
+    )
+  }
+}
