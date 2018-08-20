59af2623cdfe22bd148018a66726504d7349fcdc
Niket Shah <masterniket@gmail.com>
2018-08-20 16:22:50 +0100

    Add semicolons

--- game_frontend/src/components/GameMenu/index.js
+++ game_frontend/src/components/GameMenu/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 const GameMenu = styled.nav`
-  background-color: ${props => props.theme.palette.background.default}
-  grid-area: game-menu
+  background-color: ${props => props.theme.palette.background.default};
+  grid-area: game-menu;
 `
 
 export default GameMenu
