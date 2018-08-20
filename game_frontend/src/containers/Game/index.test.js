71ca713abdd2db7185fcfa2ded33537627f852e3
riaJha97 <37871616+riaJha97@users.noreply.github.com>
2018-08-20 16:08:04 +0100

    Add exit button to game screen (#745)
    * Add icons
    
    * close icon
    
    * Need to fix css
    
    * Updated test snapshots with new close button
    
    * Added exit button, linked to aimmo
    
    * Refactored
    
    * Added snapshot tests for exit button and game menu layout
    
    * Removed extra space :)
    
    * Imports
    
    * Updated snapshots

--- game_frontend/src/containers/Game/index.test.js
+++ game_frontend/src/containers/Game/index.test.js
@@ -1,12 +1,15 @@
 /* eslint-env jest */
 import React from 'react'
-import Game from 'components/Game'
+import { Game } from 'containers/Game'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
 describe('<Game />', () => {
   it('renders correctly', () => {
-    const tree = shallow(<Game />)
+    const props = {
+      connectToGame: jest.fn()
+    }
+    const tree = shallow(<Game {...props} />)
     expect(toJson(tree)).toMatchSnapshot()
   })
 })
