352e589d9adfdbb12f8cf313ff041933ebf04708
Ria Jha <ria.jha@ocado.com>
2018-09-04 10:22:44 +0100

    Displaying players' first names in admin

--- aimmo/admin.py
+++ aimmo/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from models import Avatar, Game
 
+class Game_DataAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+    readonly_fields = ['players']
+
+    def players(self, obj):
+        return "\n ".join([u.first_name for u in obj.can_play.all()])
+
+
+
+
 admin.site.register(Avatar)
-admin.site.register(Game)
+admin.site.register(Game, Game_DataAdmin)
