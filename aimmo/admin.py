7a299b0e7b4ba021ee90530f43f3a5ec5940ed80
riaJha97 <37871616+riaJha97@users.noreply.github.com>
2018-09-04 13:43:44 +0100

    Changed admin display of Game (#782)
    * Displaying players' first names in admin
    
    * newline
    
    * Added blank line
    
    * PR comments

--- aimmo/admin.py
+++ aimmo/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from models import Avatar, Game
 
+
+class GameDataAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+    readonly_fields = ['players']
+
+    def players(self, obj):
+        return "\n".join([u.first_name for u in obj.can_play.all()])
+
 admin.site.register(Avatar)
-admin.site.register(Game)
+admin.site.register(Game, GameDataAdmin)
