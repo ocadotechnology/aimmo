58be8bcaf0e329a9959291a2fe4bc949249b0465
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-09-06 15:02:30 +0100

    more test work

--- aimmo-game/tests/test_simulation/worker_managers/test_local_worker_manager.py
+++ aimmo-game/tests/test_simulation/worker_managers/test_local_worker_manager.py
@@ -1,15 +1,19 @@
+import os
+
 from unittest import TestCase
 
 from simulation.worker_managers.local_worker_manager import LocalWorkerManager
 
 class TestLocalWorkerManager(TestCase):
     def test_local_worker_ports_do_not_conflict(self):
-        os.environ['GAME_ID'] = 1
+        os.environ['GAME_ID'] = '1'
         worker_manager1 = LocalWorkerManager()
         local_worker1 = worker_manager1.create_worker(1)
+        http1, host1, port1 = local_worker1.split(':')
 
-        os.environ['GAME_ID'] = 2
+        os.environ['GAME_ID'] = '2'
         worker_manager2 = LocalWorkerManager()
-        local_worker2 = worker_manager1.create_worker(1)
+        local_worker2 = worker_manager2.create_worker(1)
+        ttp2, host2, port2 = local_worker2.split(':')
 
-        assertNotEquals(local_worker1.port, local_worker2.port)
+        self.assertNotEquals(port1, port2)
