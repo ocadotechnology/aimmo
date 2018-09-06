475df3a3b8b6880f9245184699aac612b322b591
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-09-06 15:20:11 +0100

    add additional test and made them more pythonic

--- aimmo-game/tests/test_simulation/worker_managers/test_local_worker_manager.py
+++ aimmo-game/tests/test_simulation/worker_managers/test_local_worker_manager.py
@@ -10,11 +10,21 @@ class TestLocalWorkerManager(TestCase):
         os.environ['GAME_ID'] = '1'
         worker_manager1 = LocalWorkerManager()
         local_worker1 = worker_manager1.create_worker(1)
-        http1, host1, port1 = local_worker1.split(':')
+        _, _, port1 = local_worker1.split(':')
 
         os.environ['GAME_ID'] = '2'
         worker_manager2 = LocalWorkerManager()
         local_worker2 = worker_manager2.create_worker(1)
-        ttp2, host2, port2 = local_worker2.split(':')
+        _, _, port2 = local_worker2.split(':')
+
+        self.assertNotEquals(port1, port2)
+
+    def test_local_workers_in_the_same_game_do_not_have_port_conflicts(self):
+        os.environ['GAME_ID'] = '1'
+        worker_manager = LocalWorkerManager()
+        local_worker1 = worker_manager.create_worker(1)
+        local_worker2 = worker_manager.create_worker(2)
+        _, _, port1 = local_worker1.split(':')
+        _, _, port2 = local_worker2.split(':')
 
         self.assertNotEquals(port1, port2)
