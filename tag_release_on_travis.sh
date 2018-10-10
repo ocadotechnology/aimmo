9694ad14e1aedb8918bb3dbedee05b6a65e0f733
Niket Shah <masterniket@gmail.com>
2018-10-10 14:37:45 +0100

    push git tag before release

--- tag_release_on_travis.sh
+++ tag_release_on_travis.sh
@@ -7,9 +7,10 @@ fi
 
 if [ "$BRANCH" = "versions" ]
 then
-    git tag "$(cat version.txt).b$TRAVIS_BUILD_NUMBER"
-    export TRAVIS_TAG="$(cat version.txt).b$TRAVIS_BUILD_NUMBER"
+    export TAG_NAME="$(cat version.txt).b$TRAVIS_BUILD_NUMBER"
 else
-    git tag "$(cat version.txt)"
-    export TRAVIS_TAG="$(cat version.txt)"
+    export TAG_NAME="$(cat version.txt)"
 fi
+
+git tag "$TAG_NAME"
+git push origin "$TAG_NAME"
