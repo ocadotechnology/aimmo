cb448b92216ccbbdddd07d21de0652bda1454aea
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-10-10 12:57:29 +0100

    More travis build tests

--- aimmo_runner/minikube.py
+++ aimmo_runner/minikube.py
@@ -183,7 +183,7 @@ def create_roles():
     the rights of certain pods.
     """
     role_path = os.path.join(BASE_DIR, 'rbac')
-    run_command(['kubectl get roles'])
+    run_command(['ls'])
     #run_command(['kubectl', 'apply', '-Rf', role_path])
 
 def start():
