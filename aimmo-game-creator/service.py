6cfe832d7b1df9779ccd121a1885d75e174b81f1
Niket Shah <masterniket@gmail.com>
2018-10-23 15:44:49 +0100

    provide default for LOCALHOST_IP

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -8,7 +8,7 @@ from game_manager import GAME_MANAGERS
 def main():
     logging.basicConfig(level=logging.DEBUG)
     game_manager_class = GAME_MANAGERS[os.environ.get('GAME_MANAGER', 'local')]
-    host = os.environ['LOCALHOST_IP'] 
+    host = os.environ.get('LOCALHOST_IP', '')
     game_manager = game_manager_class(os.environ.get('GAME_API_URL',
                                                      "http://{}:8000/aimmo/api/games/".format(host)))
     game_manager.run()
