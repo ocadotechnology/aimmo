1e95eba0c11a9cb2d8631ce538ec3228bea2f90b
Niket Shah <masterniket@gmail.com>
2018-10-30 15:30:08 +0000

    remove docker entrypoint scripts

--- aimmo-game-worker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOST_DOMAIN="host.docker.internal"
-ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
-  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
-  echo -e "$HOST_IP\t$HOST_DOMAIN"
-fi
