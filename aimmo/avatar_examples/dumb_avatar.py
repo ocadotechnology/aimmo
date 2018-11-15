173270c2deab164de1cbb4193a230d75a88135b7
faucomte97 <f.aucomte@hotmail.co.uk>
2018-11-15 11:35:52 +0000

    Removed imports from avatar examples

--- aimmo/avatar_examples/dumb_avatar.py
+++ aimmo/avatar_examples/dumb_avatar.py
@@ -1,8 +1,4 @@
-class Avatar(object):
+class Avatar:
     def handle_turn(self, world_state, avatar_state):
-        from simulation.action import MoveAction
-        import simulation.direction as direction
-        import random
-
         new_dir = random.choice(direction.ALL_DIRECTIONS)
         return MoveAction(new_dir)
