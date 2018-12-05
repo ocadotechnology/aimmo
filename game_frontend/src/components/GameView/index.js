9eaf2607f3d9e8def39756eb159eda7e519d8812
Niket Shah <masterniket@gmail.com>
2018-12-05 10:01:29 +0000

    Upgrade react unity webgl to 7.x (#910)
    * alphabetize npm packages
    
    * upgrade react unity webgl to 7.0.7
    
    * update code to work with react unity webgl 7.x
    
    * update yarn.lock
    
    * write more tests for GameView
    
    * add docker to top Pipfile
    
    * Merge branch 'development' into upgrade_react_unity_webgl
    
    # Conflicts:
    #	aimmo-game/Pipfile.lock
    
    * bump version
    
    * Merge branch 'development' into upgrade_react_unity_webgl
    
    # Conflicts:
    #	version.txt

--- game_frontend/src/components/GameView/index.js
+++ game_frontend/src/components/GameView/index.js
@@ -2,26 +2,55 @@ import styled from 'styled-components'
 import React, { Component } from 'react'
 import Unity from 'react-unity-webgl'
 import PropTypes from 'prop-types'
+import { LinearProgress } from '@material-ui/core'
+import { unityContent } from 'api/unity'
 
 export const GameViewLayout = styled.div`
   grid-area: game-view;
 `
 
-const UNITY_LOADED = 1
-
 export default class GameView extends Component {
-  onProgress = progression => {
-    if (progression === UNITY_LOADED) {
-      this.props.connectToGame()
+  state = {
+    unityContentLoaded: false
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      unityContent
     }
+    unityContent.on('loaded', this.unityContentLoaded)
+  }
+
+  unityContentLoaded = () => {
+    this.setState({
+      unityContentLoaded: true
+    })
+    this.props.connectToGame()
+  }
+
+  renderloadingBar = unityContentLoaded => {
+    if (!unityContentLoaded) {
+      return (
+        <LinearProgress color='secondary' />
+      )
+    }
+  }
+
+  renderUnityView = unityContent => {
+    return (
+      <Unity
+        unityContent={unityContent}
+        height='100%'
+        width='100%' />
+    )
   }
 
   render () {
     return (
       <GameViewLayout>
-        <Unity src='/static/unity/Build/unity.json'
-          loader='/static/unity/Build/UnityLoader.js'
-          onProgress={this.onProgress} />
+        {this.renderloadingBar(this.state.unityContentLoaded)}
+        {this.renderUnityView(this.state.unityContent)}
       </GameViewLayout>
     )
   }
--- game_frontend/src/components/GameView/index.test.js
+++ game_frontend/src/components/GameView/index.test.js
@@ -4,9 +4,30 @@ import GameView, { GameViewLayout } from 'components/GameView'
 import { shallow } from 'enzyme/build/index'
 
 describe('<GameView />', () => {
-  it('matches snapshot', () => {
+  it('shows loading bar whilst unity is loading', () => {
     const component = shallow(<GameView />)
     expect(component).toMatchSnapshot()
+  })
+
+  it('does not show the loading bar when unity has loaded', () => {
+    const connectToGame = jest.fn()
+    const props = {
+      connectToGame
+    }
+    const component = shallow(<GameView {...props} />)
+    component.instance().unityContentLoaded()
+    expect(component).toMatchSnapshot()
+  })
+
+  it('connects to the game only after unity has been loaded', () => {
+    const connectToGame = jest.fn()
+    const props = {
+      connectToGame
+    }
+    const component = shallow(<GameView {...props} />)
+    expect(connectToGame).not.toBeCalled()
+    component.instance().unityContentLoaded()
+    expect(connectToGame).toBeCalled()
   })
 })
 
