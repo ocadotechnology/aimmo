96a8511e8c7bf23c0c3d12b723c2b560c45e75f7
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-12-06 15:33:25 +0000

    minor change

--- aimmo-game/simulation/worker_managers/worker_manager.py
+++ aimmo-game/simulation/worker_managers/worker_manager.py
@@ -28,7 +28,7 @@ class WorkerManager(object):
         """
         def prepare_request_threads():
             return [Thread(target=worker.fetch_data,
-                           args=(player_id_to_game_state[player_id], self.player_id_to_worker[player_id].code))
+                           args=(player_id_to_game_state[player_id],))
                     for (player_id, worker) in self.player_id_to_worker.items()]
 
         def timed_process_for_worker_turn_requests(duration):
