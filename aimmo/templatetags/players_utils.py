6b727914308c33b6a1ab382867d53a5f53c483ea
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2018-12-10 16:38:35 +0000

    Created template tag to find user's playable game (#919)
    * Added template tag for getting a user's playable games.

--- aimmo/templatetags/players_utils.py
+++ aimmo/templatetags/players_utils.py
@@ -1,15 +1,16 @@
 from django import template
 
-from aimmo import app_settings
-
 register = template.Library()
 
 
 @register.inclusion_tag('players/dropdown.html', takes_context=True)
 def game_dropdown_list(context, base_url):
+    return get_user_playable_games(context, base_url)
+
+
+def get_user_playable_games(context, base_url):
     user = context.request.user
     return {
         'base_url': base_url,
-        'open_play_games': user.playable_games.all() if user.is_authenticated() else None,
-        'level_numbers': xrange(1, app_settings.MAX_LEVEL+1),
+        'open_play_games': user.playable_games.all() if user.is_authenticated() else None
     }
