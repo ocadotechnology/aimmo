4de999b4ea20b925e39fbb183130e215ba123512
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-12-19 11:23:31 +0000

    Revert "trying adding section to setup.py to force coverage generation"
    This reverts commit 379ca5df8ad46d2a77886df2c3c3a02bc1944a90.

--- aimmo-game-creator/setup.py
+++ aimmo-game-creator/setup.py
@@ -3,15 +3,8 @@ from setuptools import find_packages, setup
 import sys
 import os
 
-try:
-    with_coverage = os.environ['COVERAGE']
-    print('Generating coverage data.')
-    if with_coverage:
-        import coverage
-        cov = coverage.Coverage()
-        cov.start()
-except:
-    print('Not generating coverage data.')
+
+withcoverage = os.environ.get('WITH_COVERAGE')
 
 setup(
     name='aimmo-game-creator',
@@ -27,11 +20,3 @@ setup(
     test_suite='tests',
     zip_safe=False,
 )
-
-try:
-    with_coverage = os.environ['COVERAGE']
-    if with_coverage:
-        cov.stop()
-        cov.save()
-except:
-    pass
