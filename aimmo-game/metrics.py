e1fcf48a763790506908befcd9275b4deee23d87
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-02-01 08:41:42 +0000

    increase precision of quantiles for histogram

--- aimmo-game/metrics.py
+++ aimmo-game/metrics.py
@@ -7,8 +7,7 @@ import os
 # as decoraters do not function properly with async code.
 
 # Measures the time taken for the game to go through an entire turn (game runner's update method)
-CUSTOM_BUCKET = [0.0, 0.25,0.5,0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5
-                ,2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 4.25, 4.5, 4.75, 5.0]
+CUSTOM_BUCKET = [x/10 for x in range(10,51)] 
 GAME_TURN_PROCESSING_SECONDS = Histogram('function_exec_time', 'Test metric to see if we can time a functions execution',
                         buckets=CUSTOM_BUCKET,
                         labelnames=('game_id'),
