b1c5c07996abb58b1a885a1c97788ca3c7354926
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-02-01 14:31:32 +0000

    fix syntax error++

--- aimmo-game/metrics.py
+++ aimmo-game/metrics.py
@@ -6,10 +6,15 @@ import os
 # you should use the context manager approach instead of decorators. When using the metrics
 # as decoraters do not function properly with async code.
 
+# Do not use a metric more than once, it will probably break them. If you need to measure
+# something in a similar or the same way as an existing metric, create a new one.
+
 # Measures the time taken for the game to go through an entire turn (game runner's update method)
 def GAME_TURN_PROCESSING_SECONDS():
-    CUSTOM_BUCKET = [x/10 for x in range(10,51)] 
-    return Histogram('function_exec_time', 'Test metric to see if we can time a functions execution',
+    """ Used for measuring the time it games for the game to complete a turn. This is stored
+        on a Histogram with values 1 to 5 +infinity going in steps of 0.1. """
+    CUSTOM_BUCKET = [x/10 for x in range(10,51)]
+    GAME_TURN_PROCESSING = Histogram('function_exec_time', 'Test metric to see if we can time a functions execution',
                         buckets=CUSTOM_BUCKET,
-                        labelnames=('game_id'),
-                        labelvalues=(f'{os.environ['GAME_ID']}'))
+                        labelnames=('game_id'))
+    return GAME_TURN_PROCESSING.labels(f'{os.environ['GAME_ID']}')
