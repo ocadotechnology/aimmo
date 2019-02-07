c4345c3a2df0d2782bdad8a75aa7b84efb7a2661
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-02-07 14:42:38 +0000

    moving cmds back into travis, as cmd was not found by travis

--- get_coverage_on_travis.sh
+++
@@ -1,5 +0,0 @@
-# Gets the container ID of the last container (will be the one used for tests on travis) and pulls the
-# coverage files out.
-read CONTAINERID <<< $(docker ps --all -q | head -1)
-sudo docker cp -a $CONTAINERID:.coverage $(pwd)/.coverage
-sudo docker cp -a $CONTAINERID:coverage.xml $(pwd)/coverage.xml
