748bc347d36aff8e521b538ad4ab9ebc8fbbf131
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-02-19 10:51:08 +0000

    minor changes

--- aimmo-game/simulation/game_logic/pickup_appliers.py
+++ aimmo-game/simulation/game_logic/pickup_appliers.py
@@ -4,7 +4,8 @@ LOGGER = getLogger(__name__)
 
 
 class PickupApplier:
-    def apply(self, worldmap):
+    def apply(self, worldmap: object):
+        """ applys pickup affects to any avatar that is now on a pickup cell """
         for cell in worldmap.pickup_cells():
             if cell.avatar is not None:
                 cell.pickup.apply(cell.avatar)
