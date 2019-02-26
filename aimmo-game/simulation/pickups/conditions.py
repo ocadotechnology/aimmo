70c8814aeec289259792f6aff36be38902c685fd
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-02-26 11:55:29 +0000

    pep8

--- aimmo-game/simulation/pickups/conditions.py
+++ aimmo-game/simulation/pickups/conditions.py
@@ -4,12 +4,14 @@ if TYPE_CHECKING:
     from simulation.world_map import WorldMap
     from simulation.cell import Cell
 
+
 class TurnState:
     """
     Data structure used by conditions.
-    
+
     On a given turn, all conditions for a pickup get access to the same TurnState object.
     """
+
     def __init__(self, world_map: 'WorldMap', pickup_cell: 'Cell'):
         self.world_map = world_map
         self.pickup_cell = pickup_cell
--- aimmo-game/tests/functional/test_conditions.py
+++ aimmo-game/tests/functional/test_conditions.py
@@ -17,6 +17,7 @@ def game():
     game.game_state.add_avatar(1, Location(0, 0))
     return game
 
+
 def test_avatar_on_cell(game):
     cell = game.game_state.world_map.get_cell(Location(1, 0))
     condition = avatar_on_cell(TurnState(game.game_state.world_map, cell))
