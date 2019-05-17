3c7fb432f72c2a75cdcea4d6342f7ee9ad947c55
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-05-17 11:42:44 +0100

    trying to figure out why appconfig doesnt work

--- aimmo/apps.py
+++ aimmo/apps.py
@@ -6,6 +6,9 @@ class AimmoConfig(AppConfig):
 
     def ready(self):
         games = self.get_model("Game").objects.all()
+        print("########################################")
+        print(games)
+        print("########################################")
         for game in games:
             game.auth_token = ""
             game.save()
