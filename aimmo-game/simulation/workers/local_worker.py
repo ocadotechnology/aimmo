548af2eda66ec6c6de42af48250dd3dcfed4773c
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-05-20 10:16:21 +0100

    Revert "Revert "Revert "for testing on dev"""
    This reverts commit 816f866e7aacd818378d19a80df8ce2967575a8b.

--- aimmo-game/simulation/workers/local_worker.py
+++ aimmo-game/simulation/workers/local_worker.py
@@ -47,7 +47,7 @@ class LocalWorker(Worker):
         template["environment"]["PORT"] = port
         self.container = self.client.containers.run(
             name="aimmo-{}-worker-{}".format(self.game_id, self.player_id),
-            image="dent50cent/aimmo-game-worker:test",
+            image="ocadotechnology/aimmo-game-worker:test",
             ports={f"{port}/tcp": port},
             **template,
         )
