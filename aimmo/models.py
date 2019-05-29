7744e4d05612af0a90c25bac6a8dc30ccc87419d
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-05-29 14:07:16 +0100

    serialised settings

--- aimmo/models.py
+++ aimmo/models.py
@@ -57,7 +57,6 @@ class Game(models.Model):
     start_height = models.IntegerField(default=31)
     start_width = models.IntegerField(default=31)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=RUNNING)
-    settings = {}
 
     @property
     def is_active(self):
@@ -69,21 +68,7 @@ class Game(models.Model):
     def can_user_play(self, user):
         return self.public or user in self.can_play.all()
 
-    def settings_as_dict(self):
-        return {
-            "TARGET_NUM_CELLS_PER_AVATAR": self.target_num_cells_per_avatar,
-            "TARGET_NUM_SCORE_LOCATIONS_PER_AVATAR": self.target_num_score_locations_per_avatar,
-            "SCORE_DESPAWN_CHANCE": self.score_despawn_chance,
-            "TARGET_NUM_PICKUPS_PER_AVATAR": self.target_num_pickups_per_avatar,
-            "PICKUP_SPAWN_CHANCE": self.pickup_spawn_chance,
-            "OBSTACLE_RATIO": self.obstacle_ratio,
-            "START_HEIGHT": self.start_height,
-            "START_WIDTH": self.start_width,
-            "GENERATOR": self.generator,
-        }
-
     def save(self, *args, **kwargs):
-        self.settings = json.dumps(self.settings_as_dict())
         super(Game, self).full_clean()
         super(Game, self).save(*args, **kwargs)
 
--- aimmo/serializers.py
+++ aimmo/serializers.py
@@ -4,4 +4,12 @@ from rest_framework import serializers
 class GameSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     status = serializers.CharField(max_length=1)
-    settings = serializers.JSONField()
+    generator = serializers.CharField(max_length=20)
+    obstacle_ratio = serializers.FloatField()
+    pickup_spawn_chance = serializers.FloatField()
+    score_despawn_chance = serializers.FloatField()
+    start_height = serializers.IntegerField()
+    start_width = serializers.IntegerField()
+    target_num_cells_per_avatar = serializers.FloatField()
+    target_num_pickups_per_avatar = serializers.FloatField()
+    target_num_score_locations_per_avatar = serializers.FloatField()
