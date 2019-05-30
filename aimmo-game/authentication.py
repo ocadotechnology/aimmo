e6289ce7b4886882389f5ee296498ba07c6abf1e
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-05-30 15:33:09 +0100

    grammar change

--- aimmo-game/authentication.py
+++ aimmo-game/authentication.py
@@ -5,7 +5,7 @@ import requests
 
 
 async def initialize_game_token(communicator):
-    """Gets game token and stores it somewhere accesible."""
+    """Get game token and stores it somewhere accesible."""
     if os.environ["WORKER"] == "kubernetes":
         api = kubernetes.client.CoreV1Api()
         game_id = os.environ.get("GAME_ID")
