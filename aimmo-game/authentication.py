a8435c98601e182d5bdf2136434ab71d65689b6d
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2019-05-31 08:13:06 +0100

    Fix typo (#1047)
    * fix typo
    
    * grammar change
    
    * bump version
    
    * grammar fix++

--- aimmo-game/authentication.py
+++ aimmo-game/authentication.py
@@ -5,12 +5,12 @@ import requests
 
 
 async def initialize_game_token(communicator):
-    """Gets game token and stores it somewhere accesible."""
+    """Get game token and store it somewhere accessible."""
     if os.environ["WORKER"] == "kubernetes":
         api = kubernetes.client.CoreV1Api()
         game_id = os.environ.get("GAME_ID")
 
-        secret = v1.read_namespaced_secret(f"game-{game_id}-token", "default")
+        secret = api.read_namespaced_secret(f"game-{game_id}-token", "default")
         os.environ["TOKEN"] = secret.data["token"]
 
     await communicator.patch_token({"token": os.environ["TOKEN"]})
