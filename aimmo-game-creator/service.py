5cf7a08c5983579f8a6245c5a1432849781d34ec
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-06-07 14:23:01 +0100

    extreme test of code not updating

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -4,23 +4,8 @@ import os
 
 from game_manager import GAME_MANAGERS
 
+logging.basicConfig(level=logging.DEBUG)
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    game_manager_class = GAME_MANAGERS[os.environ.get("GAME_MANAGER", "local")]
-    host = os.environ.get("LOCALHOST_IP", "localhost")
-    game_manager = game_manager_class(
-        os.environ.get("GAME_API_URL", "http://{}:8000/aimmo/api/games/".format(host))
-    )
-    game_manager.run()
+LOGGER = logging.Logger()
 
-
-if __name__ == "__main__":
-    # Create a place to store game tokens for local mode.
-    if not os.path.exists("/tokens"):
-        os.makedirs("/tokens")
-
-    with open("/tokens/local_tokens.json", "w+") as f:
-        f.write("{}")
-
-    main()
+LOGGER.critical("I SHOULD PRINT OUT AND NOTHING ELSE")
