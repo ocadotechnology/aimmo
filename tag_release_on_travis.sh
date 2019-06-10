12622b9a095166b34fab896d46743280762c21f5
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-06-10 13:27:17 +0100

    deploy pypi for dev

--- tag_release_on_travis.sh
+++ tag_release_on_travis.sh
@@ -5,9 +5,11 @@ else
     export BRANCH="$TRAVIS_BRANCH"
 fi
 
-if [ "$BRANCH" = "development" ]
-then
-    git tag "$(cat version.txt).b$TRAVIS_BUILD_NUMBER"
-else
-    git tag "$(cat version.txt)"
-fi
+
+git tag "$(cat version.txt).b$TRAVIS_BUILD_NUMBER.dev$TRAVIS_BUILD_NUMBER"
+# if [ "$BRANCH" = "development" ]
+# then
+#     git tag "$(cat version.txt).b$TRAVIS_BUILD_NUMBER"
+# else
+#     git tag "$(cat version.txt)"
+# fi
