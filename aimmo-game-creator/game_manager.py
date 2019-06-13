1718714236d9dcb7a6e501032ceb63dd23401be8
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-06-13 14:56:20 +0100

    Revert "try moving secret creation to the same file as the rest?"
    This reverts commit 136b820088ad4f8e0ba34f32de97edc14785d45e.

--- aimmo-game-creator/game_manager.py
+++ aimmo-game-creator/game_manager.py
@@ -351,38 +351,13 @@ class KubernetesGameManager(GameManager):
         service = self._make_service(game_id)
         self.api.create_namespaced_service(K8S_NAMESPACE, service)
 
-    def load_template(self, name: str, namespace: str, data: dict):
-        """Loads a template file, fills in the needed data."""
-        with open("kube_templates/game_token.yaml", "r") as f:
-            template = yaml.safe_load(f)
-
-            template["metadata"]["name"] = name
-            template["metadata"]["namespace"] = namespace
-            template["data"]["token"] = data["token"]
-
-            return template
-
-    def create_secret(self, name: str, namespace: str, data: dict):
-        """Creates the k8s object."""
-        template = self.load_template(name, namespace, data)
-
-        metadata = kubernetes.client.V1ObjectMeta(template["metadata"])
-        data = template["data"]
-
-        body = kubernetes.client.V1Secret(**template)
-        try:
-            self.api.create_namespaced_secret(namespace, body)
-        except ApiException:
-            self.api.patch_namespaced_secret(name, namespace, body)
-
     def _create_game_secret(self, game_id):
         name = KubernetesGameManager._create_game_name(game_id) + "-token"
         try:
             secret = self.api.read_namespaced_secret(name, K8S_NAMESPACE)
         except ApiException:
             data = {"token": self._generate_game_token()}
-            # self.secret_creator.create_secret(name, K8S_NAMESPACE, data)
-            self.create_secret(name, K8S_NAMESPACE, data)
+            self.secret_creator.create_secret(name, K8S_NAMESPACE, data)
 
     def _add_path_to_ingress(self, game_id):
         backend = kubernetes.client.V1beta1IngressBackend(
