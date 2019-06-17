e336cf9b08e5201a74cc150e94d3bf25f0c483f9
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-06-17 15:49:27 +0100

    tidy up (IT WORKS! :D)

--- aimmo-game-creator/kube_components.py
+++ aimmo-game-creator/kube_components.py
@@ -15,29 +15,17 @@ class TokenSecretCreator:
         kubernetes.config.load_incluster_config()
         self.api = kubernetes.client.CoreV1Api()
 
-    def load_template(self, name: str, namespace: str, data: dict):
-        """Creates the template dict, fills in the needed data."""
-        template = {}
-
-        template["kind"] = "Secret"
-
-        template["metadata"] = {}
-        template["metadata"]["name"] = name
-        template["metadata"]["namespace"] = namespace
-
-        template["string_data"] = {}
-        template["string_data"]["token"] = data["token"]
-
-        return template
-
     def create_secret(self, name: str, namespace: str, data: dict):
         """Creates the k8s object."""
-        template = self.load_template(name, namespace, data)
+        template = {}
+        template["kind"] = "Secret"
+        template["string_data"] = data
+        template["metadata"] = kubernetes.client.V1ObjectMeta(
+            name=name, namespace=namespace
+        )
 
-        template["metadata"] = kubernetes.client.V1ObjectMeta(**template["metadata"])
-        LOGGER.debug(f"Your template sir: {template}")
         body = kubernetes.client.V1Secret(**template)
-        LOGGER.debug(f"every body, yeah! {body}")
+
         try:
             self.api.create_namespaced_secret(namespace, body)
         except ApiException:
