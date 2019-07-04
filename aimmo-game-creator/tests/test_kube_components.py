3c54cf048bd1c2a9dcd2de5f9455a867499dc8f7
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-07-04 09:47:12 +0100

    added some tests

--- aimmo-game-creator/tests/test_kube_components.py
+++ aimmo-game-creator/tests/test_kube_components.py
@@ -10,7 +10,7 @@ class TestKubeComponents(TestCase):
     def setUp(self):
         secret_creator = TokenSecretCreator
         self.secret = secret_creator.create_secret_object(
-            None, "TEST_NAME", "TEST_NAMESPACE", {"token": "TEST_TOKEN"}
+            None, "game-1-token", "default", {"token": "TEST_TOKEN"}
         )
 
     def test_secret_is_loaded_and_data_is_added_correctly(self):
@@ -19,6 +19,6 @@ class TestKubeComponents(TestCase):
         """
         self.assertIsNotNone(self.secret.metadata)
         self.assertIsNotNone(self.secret.string_data)
-        self.assertEqual(self.secret.metadata.name, "TEST_NAME")
-        self.assertEqual(self.secret.metadata.namespace, "TEST_NAMESPACE")
+        self.assertEqual(self.secret.metadata.name, "game-1-token")
+        self.assertEqual(self.secret.metadata.namespace, "default")
         self.assertEqual(self.secret.string_data["token"], "TEST_TOKEN")
