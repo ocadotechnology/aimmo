a91ffd3cfdfb7bfad521f45d8051b67cea58aa0b
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-07-08 13:31:47 +0100

    remove log statments

--- aimmo-game/authentication.py
+++ aimmo-game/authentication.py
@@ -17,10 +17,7 @@ async def initialize_game_token(communicator):
         game_id = os.environ.get("GAME_ID")
 
         secret = api.read_namespaced_secret(f"game-{game_id}-token", "default")
-        LOGGER.info(f"SSSSH, IT'S A SECRET: {secret}")
-        LOGGER.info(f"data: {secret.data}")
-        os.environ["TOKEN"] = str(base64.b64decode(secret.data["token"]))
-        LOGGER.info(f"MIEN TOKENSHAFEN: {os.environ['TOKEN']}")
+        os.environ["TOKEN"] = secret.data["token"]
         LOGGER.info("Token set!")
 
     response = await communicator.patch_token({"token": os.environ["TOKEN"]})
