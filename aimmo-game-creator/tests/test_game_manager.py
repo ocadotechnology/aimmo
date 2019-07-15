44bd11aea49c54ac3d828f44178d4846854b82a4
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-07-15 10:56:23 +0100

    replace none with empty dict

--- aimmo-game-creator/tests/test_game_manager.py
+++ aimmo-game-creator/tests/test_game_manager.py
@@ -104,7 +104,7 @@ class TestGameManager(unittest.TestCase):
 
     def test_make_rc(self):
         # _make_rc is not being called as an instance method here so "self" has to explicitly be passed through
-        game_rc = KubernetesGameManager._make_rc(None, None, 1)
+        game_rc = KubernetesGameManager._make_rc(None, {}, 1)
 
         self.assertTrue(game_rc["spec"])
         self.assertTrue(game_rc["template"])
