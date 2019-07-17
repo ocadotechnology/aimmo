e864df60f855bc04e424a9d38e367a0b9f9f8092
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-07-17 09:30:38 +0100

    fix typo

--- aimmo-game-creator/game_manager.py
+++ aimmo-game-creator/game_manager.py
@@ -431,8 +431,8 @@ class KubernetesGameManager(GameManager):
             delete_resource_function(resource.metadata.name, K8S_NAMESPACE)
 
     def create_game(self, game_id, game_data):
-        LOGGER.info(os.environ)
-        is_minikube = "USE_MINIKUBE" in os.environ
+        is_minikube = "USING_MINIKUBE" in os.environ
+
         self._create_game_secret(game_id)
         self._create_game_service(game_id)
         self._create_game_rc(game_id, game_data, is_minikube)
