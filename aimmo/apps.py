5e5f12452415ae71922767b81a70ccf1db9244e1
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-07-19 15:20:46 +0100

    Use post_migrate signal to wait for database

--- aimmo/apps.py
+++ aimmo/apps.py
@@ -1,13 +1,15 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def clear_tokens(sender, **kwargs):
+    game_model = sender.get_model("Game")
+    games = game_model.objects.all()
+    games.update(auth_token="")
 
 
 class AimmoAppConfig(AppConfig):
     name = "aimmo"
 
     def ready(self):
-        from .models import Game
-        games = Game.objects.all()
-
-        for game in games:
-            game.auth_token = ""
-            game.save()
+        post_migrate.connect(clear_tokens, sender=self)
