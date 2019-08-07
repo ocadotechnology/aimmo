a3028b7f98d956b68438fd1396ea9eb7d7507fa8
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-08-07 17:22:46 +0100

    Add docs for MockEnvironment

--- game_frontend/src/testHelpers/mockEnvironment.ts
+++ game_frontend/src/testHelpers/mockEnvironment.ts
@@ -1,6 +1,15 @@
 import * as BABYLON from 'babylonjs'
 import { Environment } from '../babylon/environment/environment'
 
+/**
+ * Class which mocks the Environment class. This uses the BABYLON NullEngine
+ * so we can test different elements from Babylon without needing a canvas.
+ *
+ * The class still has a canvas property however, but it is not instantiated
+ * in the constructor. This is because some elements might still require the
+ * presence of a canvas to be loaded properly, like the Camera. In thi case,
+ * we mock the canvas element using jest.
+ */
 export class MockEnvironment implements Environment {
   scene: BABYLON.Scene;
   engine: BABYLON.Engine;
