6e11968d72b95132ed0ca2d76a28e0e482b7d902
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2019-08-13 13:34:22 +0100

    ci: simplify version updater (#1135)
    * Simplify version updater

--- version_updater.py
+++ version_updater.py
@@ -7,24 +7,17 @@ from aimmo import __version__
 parser = argparse.ArgumentParser(description="Update version based on branch.")
 parser.add_argument("DEPLOY_TO_DEV", help="True if build to dev")
 parser.add_argument("TRAVIS_BUILD_NUMBER", help="Travis build number")
-parser.add_argument(
-    "TRAVIS_PULL_REQUEST_BRANCH", help="The name of the PR branch, otherwise empty"
-)
 parser.add_argument("TRAVIS_BRANCH", help="Name of the branch the build is on")
 
 args = parser.parse_args()
+version = __version__
 
 if args.DEPLOY_TO_DEV:
     version = __version__ + "dev" + args.TRAVIS_BUILD_NUMBER
-
-if args.TRAVIS_PULL_REQUEST_BRANCH != "":
-    BRANCH = args.TRAVIS_PULL_REQUEST_BRANCH
 else:
     BRANCH = args.TRAVIS_BRANCH
 
-if BRANCH == "development":
-    version = __version__ + ".b" + args.TRAVIS_BUILD_NUMBER
-else:
-    version = __version__
+    if BRANCH == "development":
+        version = __version__ + ".b" + args.TRAVIS_BUILD_NUMBER
 
 set_new_version(version)
