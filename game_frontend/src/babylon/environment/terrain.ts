ea45997f493938bf09588ec88da058c8a0c96b76
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-08-19 11:58:21 +0100

    rename terrain to be more inline with naming convention

--- game_frontend/src/babylon/environment/terrain.ts
+++ game_frontend/src/babylon/environment/terrain.ts
@@ -11,7 +11,7 @@ export default class Terrain implements GameNode {
 
       const mat = new BABYLON.StandardMaterial('Terrain', environment.scene)
       mat.useReflectionOverAlpha = false
-      mat.diffuseTexture = new BABYLON.Texture('/static/babylon/terrain/terrain_future.jpg', environment.scene)
+      mat.diffuseTexture = new BABYLON.Texture('/static/babylon/terrain/ground_future.jpg', environment.scene)
       mat.diffuseTexture.level = 1.2
       mat.specularColor = new BABYLON.Color3(0, 0, 0)
       mat.ambientColor = BABYLON.Color3.White()
