18c883e8546154d240b10805d6eccbed063cea47
Niket Shah <masterniket@gmail.com>
2019-10-22 16:35:16 +0100

    feat: Add new find me icon
    Signed-off-by: Niket Shah <masterniket@gmail.com>

---
+++ game_frontend/src/components/icons/FindMe.js
@@ -0,0 +1,14 @@
+import SvgIcon from '@material-ui/core/SvgIcon'
+import React from 'react'
+
+const FindMe = props => (
+  <SvgIcon {...props}>
+    <g id='Symbols' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
+      <g id='location' fill='#FFFFFF'>
+        <path d='M23.4425409,20.9415284 C24.7489879,23.665963 23.6795935,24.7816627 21.0656363,23.4177835 L16.7540421,21.1719808 C14.1400849,19.8092095 9.86038117,19.8092095 7.246424,21.1719808 L2.93482975,23.4177835 C0.320872587,24.7816627 -0.749584862,23.665963 0.557925229,20.9415284 L9.62332852,2.04332591 C10.9297756,-0.681108635 13.0696275,-0.681108635 14.3771376,2.04332591 L23.4425409,20.9415284 Z' />
+      </g>
+    </g>
+  </SvgIcon>
+)
+
+export default FindMe
