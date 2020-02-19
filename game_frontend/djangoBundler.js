42ee6de1874f77d04598ee9b4cb64affef9d02af
Niket <niket.shah1@ocado.com>
2020-02-19 17:14:36 +0000

    Fix tag manager inclusion code
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- game_frontend/djangoBundler.js
+++ game_frontend/djangoBundler.js
@@ -1,46 +1,65 @@
-const Bundler = require('parcel-bundler')
-const Path = require('path')
-const fs = require('fs')
-const Handlebars = require('handlebars')
+const Bundler = require("parcel-bundler");
+const Path = require("path");
+const fs = require("fs");
+const Handlebars = require("handlebars");
 
-const file = Path.join(__dirname, './public/index.html')
-const outDir = Path.join(__dirname, '../aimmo/static/react')
+const file = Path.join(__dirname, "./public/index.html");
+const outDir = Path.join(__dirname, "../aimmo/static/react");
+
+const cloudEnvironments = new Set(["production", "staging"]);
 
 const options = {
   outDir,
-  outFile: 'index.html',
-  publicUrl: './',
-  watch: process.env.NODE_ENV !== 'production',
-  minify: process.env.NODE_ENV === 'production',
-  target: 'browser',
-  cache: process.env.NODE_ENV === 'production'
+  outFile: "index.html",
+  publicUrl: "./",
+  watch: !cloudEnvironments.has(process.env.NODE_ENV),
+  minify: cloudEnvironments.has(process.env.NODE_ENV),
+  target: "browser",
+  cache: cloudEnvironments.has(process.env.NODE_ENV)
+};
+
+const templateFolder = Path.resolve(
+  Path.join(__dirname, "../aimmo/templates/players")
+);
+const handlebarsTemplatePath = Path.resolve(
+  Path.join(__dirname, "./public/handlebars_template.html")
+);
+
+const bundler = new Bundler(file, options);
+
+function getReactURL(entryPointHTML) {
+  const regex = /(<script src=")(.*\.js)("><\/script>)/g;
+  return regex.exec(entryPointHTML)[2];
 }
 
-const templateFolder = Path.resolve(Path.join(__dirname, '../aimmo/templates/players'))
-const handlebarsTemplatePath = Path.resolve(Path.join(__dirname, './public/handlebars_template.html'))
+Handlebars.registerHelper("if_eq", function(a, b, opts) {
+  if (a === b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
 
-const bundler = new Bundler(file, options)
-
-function getReactURL (entryPointHTML) {
-  const regex = /(<script src=")(.*\.js)("><\/script>)/g
-  return regex.exec(entryPointHTML)[2]
+function generateGameIDEHTML(reactFileName) {
+  const templateString = fs.readFileSync(handlebarsTemplatePath, "utf-8");
+  const template = Handlebars.compile(templateString);
+  return template({
+    reactUrl: `react/${reactFileName}`,
+    environment: process.env.NODE_ENV
+  });
 }
 
-function generateGameIDEHTML (reactFileName) {
-  const templateString = fs.readFileSync(handlebarsTemplatePath, 'utf-8')
-  const template = Handlebars.compile(templateString)
-  return template({ reactUrl: `react/${reactFileName}`, environment: process.env.NODE_ENV })
-}
+bundler.on("bundled", bundle => {
+  const entryPointHTML = fs.readFileSync(bundle.name, "utf-8");
+  const reactUrl = getReactURL(entryPointHTML);
+  const gameIDEHTML = generateGameIDEHTML(reactUrl);
 
-bundler.on('bundled', (bundle) => {
-  const entryPointHTML = fs.readFileSync(bundle.name, 'utf-8')
-  const reactUrl = getReactURL(entryPointHTML)
-  const gameIDEHTML = generateGameIDEHTML(reactUrl)
+  fs.writeFile(`${templateFolder}/game_ide.html`, gameIDEHTML, error => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("game_ide.html django template generated sucessfully");
+  });
+});
 
-  fs.writeFile(`${templateFolder}/game_ide.html`, gameIDEHTML, (error) => {
-    if (error) { return console.log(error) }
-    console.log('game_ide.html django template generated sucessfully')
-  })
-})
-
-bundler.bundle()
+bundler.bundle();
