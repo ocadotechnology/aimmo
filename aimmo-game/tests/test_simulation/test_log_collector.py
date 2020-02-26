f2513a81314279dad241d63f26b698eceab19b99
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-02-26 13:50:54 +0000

    Add log collector and socketio tests

---
+++ aimmo-game/tests/test_simulation/test_log_collector.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+
+from simulation.workers.worker import Worker
+from simulation.avatar.avatar_wrapper import AvatarWrapper
+from simulation.log_collector import LogCollector
+
+
+class MockWorker(Worker):
+    def _create_worker(self):
+        pass
+
+
+class TestLogCollector(TestCase):
+    def test_should_send_logs(self):
+        log_collector = LogCollector()
+
+        self.assertEquals(log_collector.should_send_logs(), False)
+
+        log_collector.player_logs = "Test logs"
+
+        self.assertEquals(log_collector.should_send_logs(), True)
+
+    def test_collect_logs(self):
+        log_collector = LogCollector()
+        worker = MockWorker(None, None)
+        log_collector.worker = worker
+        worker.log = "Worker test log"
+
+        avatar = AvatarWrapper(None, None, None)
+        log_collector.avatar = avatar
+
+        log_collector.collect_logs()
+
+        self.assertEquals(log_collector.player_logs, "Worker test log")
+
+        avatar.log = "Avatar test log"
+
+        log_collector.collect_logs()
+
+        self.assertEquals(log_collector.player_logs, "Worker test logAvatar test log")
--- aimmo-game/tests/test_socketio.py
+++ aimmo-game/tests/test_socketio.py
@@ -8,13 +8,27 @@ import socketio
 
 import service
 from simulation.game_runner import GameRunner
+from simulation.avatar.avatar_wrapper import AvatarWrapper
 from .test_simulation.mock_communicator import MockCommunicator
 from .test_simulation.mock_worker_manager import MockWorkerManager
 
 TIME_TO_PROCESS_SOME_EVENT_LOOP = 0.1
 
 
+class MockAvatarManager(object):
+    avatars_by_id = {}
+
+    def add_avatar(self, player_id):
+        avatar = AvatarWrapper(player_id, None, None)
+        self.avatars_by_id[player_id] = avatar
+        return avatar
+
+    def get_avatar(self, user_id):
+        return self.avatars_by_id[user_id]
+
+
 class MockGameState(object):
+    avatar_manager = MockAvatarManager()
     turn_count = 0
 
     def serialize(self):
@@ -74,6 +88,7 @@ async def test_socketio_emit_called_when_worker_ready(
     mock_game_state_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("game-state", mock_game_state_listener)
 
@@ -99,6 +114,7 @@ async def test_socketio_emit_not_called_if_worker_not_ready(
     mock_game_state_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("game-state", mock_game_state_listener)
 
@@ -122,6 +138,7 @@ async def test_send_updates_for_one_user(game_api, client, socketio_server, loop
     mock_log_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("log", mock_log_listener)
 
@@ -142,12 +159,44 @@ async def test_send_updates_for_one_user(game_api, client, socketio_server, loop
     )
 
 
+async def test_send_worker_and_avatar_logs_for_one_user(
+    game_api, client, socketio_server, loop
+):
+    socketio_client = socketio.AsyncClient(reconnection=False)
+    mock_log_listener = mock.MagicMock()
+
+    game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
+
+    socketio_client.on("log", mock_log_listener)
+
+    worker = game_api.worker_manager.player_id_to_worker[1]
+    worker.log = "Worker log"
+
+    avatar = game_api.game_state.avatar_manager.get_avatar(1)
+    avatar.log = "Avatar log"
+
+    await socketio_client.connect(
+        f"http://{client.server.host}:{client.server.port}?avatar_id=1&EIO=3&transport=polling&t=MJhoMgb"
+    )
+
+    await game_api.send_updates_to_all()
+
+    await socketio_server.sleep(TIME_TO_PROCESS_SOME_EVENT_LOOP)
+    await socketio_client.disconnect()
+
+    mock_log_listener.assert_has_calls(
+        [mock.call({"message": "Worker logAvatar log", "turn_count": 0})]
+    )
+
+
 async def test_no_logs_not_emitted(game_api, client, socketio_server, loop):
     """ If there are no logs for an avatar, no logs should be emitted. """
     socketio_client = socketio.AsyncClient(reconnection=False)
     mock_log_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("log", mock_log_listener)
 
@@ -169,6 +218,7 @@ async def test_empty_logs_not_emitted(game_api, client, socketio_server, loop):
     mock_log_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("log", mock_log_listener)
 
@@ -194,7 +244,9 @@ async def test_send_updates_for_multiple_users(game_api, client, socketio_server
     mock_log_listener2 = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
     game_api.worker_manager.add_new_worker(2)
+    game_api.game_state.avatar_manager.add_avatar(2)
 
     socketio_client.on("log", mock_log_listener)
     socketio_client2.on("log", mock_log_listener2)
@@ -231,6 +283,7 @@ async def test_send_code_changed_flag(game_api, client, socketio_server, loop):
     mock_avatar_updated_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("feedback-avatar-updated", mock_avatar_updated_listener)
 
@@ -254,6 +307,7 @@ async def test_send_false_flag_not_sent(game_api, client, socketio_server, loop)
     mock_avatar_updated_listener = mock.MagicMock()
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     socketio_client.on("feedback-avatar-updated", mock_avatar_updated_listener)
 
@@ -276,6 +330,7 @@ async def test_remove_session_id_on_disconnect(game_api, client, socketio_server
     socketio_client = socketio.AsyncClient(reconnection=False)
 
     game_api.worker_manager.add_new_worker(1)
+    game_api.game_state.avatar_manager.add_avatar(1)
 
     await socketio_client.connect(
         f"http://{client.server.host}:{client.server.port}?avatar_id=1&EIO=3&transport=polling&t=MJhoMgb"
