5f18c9b3682a47dce6aaef8d78785aa5818b7f72
Niket Shah <masterniket@gmail.com>
2020-03-09 12:09:00 +0000

    simplify assetPack.ts code and fix typo in it
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- game_frontend/src/babylon/assetPack.ts
+++ game_frontend/src/babylon/assetPack.ts
@@ -1,80 +1,63 @@
-import { StandardMaterial } from "babylonjs"
-
 const tileSizes = {
-    'future': { w: 31, h: 31 },
-    'prehistory': { w: 1, h: 1 },
-    'grid': { w: 31, h: 31 }
+  future: { w: 31, h: 31 },
+  prehistory: { w: 1, h: 1 },
+  grid: { w: 31, h: 31 }
 }
 
-export class StandardAsset {
-    name: string
-    modelURL: string
-    modelName: string
-    textureURL: string
-    materialName: string
-
-    constructor(name: string, modelURL: string, modelName: string, textureURL: string, materialName: string): void {
-        this.name = name
-        this.modelURL = modelURL
-        this.modelName = modelName
-        this.textureURL = textureURL
-        this.materialName = materialName
-    }
+export interface StandardAsset {
+  name: string,
+  modelURL: string,
+  modelName: string,
+  textureURL: string,
+  materialName: string
 }
 
-export class PlaneAsset {
-    name: string
-    tileSize: any
-    materialName: string
-    textureURL: string
-
-    constructor(name: string, tileSize: any, materialName: string, textureURL: string): void {
-        this.name = name
-        this.tileSize = tileSizes
-        this.materialName = materialName
-        this.textureURL = textureURL
-    }
+export interface PlaneAsset {
+  name: string,
+  tileSize: any,
+  materialName: string,
+  textureURL: string
 }
 
 /**
  * Used to store game assets and map them to eras
  */
 export class AssetPack {
-    obstacles: StandardAsset
-    terrain: PlaneAsset
-    grid: PlaneAsset
+  obstacles: StandardAsset
+  terrain: PlaneAsset
+  grid: PlaneAsset
 
-    constructor (era: string) {
-        this.createObstacles(era)
-        this.createTerrain(era)
-        this.createGrid()
+  constructor (era: string) {
+    this.createObstacles(era)
+    this.createTerrain(era)
+    this.createGrid()
+  }
+
+  createObstacles (era: string): void {
+    this.obstacles = {
+      name: 'obstacle',
+      modelURL: '/static/babylon/obstacles/',
+      modelName: `obstacle_model_${era}.babylon`,
+      textureURL: `/static/babylon/obstacles/obstacle_${era}.jpg`,
+      materialName: `obstacle_material_${era}`
     }
+  }
 
-    createObstacles(era: string): void {
-        var name = 'obstacle'
-        var modelURL = '/static/babylon/obstacles/'
-        var modelName = 'obstacle_model_' + era + '.babylon'
-        var textureURL = '/static/babylon/obstacles/obstacle_' + era + '.jpg'
-        var materialName = 'obstacle_material_' + era
-
-        this.obstacles = new StandardAsset(name, modelURL, modelName, textureURL, materialName)
+  createTerrain (era: string): void {
+    this.terrain = {
+      name: 'terrain',
+      tileSize: tileSizes[era],
+      materialName: `terrain_material_${era}`,
+      textureURL: `/static/babylon/terrain/terrain_${era}.jpg`
     }
+  }
 
-    createTerrain(era: string): void {
-        var name = 'terrain'
-        var tileSize = tileSizes[era]
-        var materialName = 'terrain_material_' + era
-        var textureURL = '/static/babylon/terrain/terrain_' + era + '.jpg'
-
-        this.terrain = new PlaneAsset(name, tileSize, materialName, textureURL)
+  createGrid (): void {
+    this.grid = {
+      name: 'grid',
+      tileSize: tileSizes['grid'],
+      materialName: 'grid_material',
+      textureURL: '/static/babylon/terrain/grid.png'
     }
-
-    createGrid(): void {
-        var name = 'grid'
-        var tileSize = tileSizes['grid']
-        var materialName = 'grid_material'
-        var textureURL = '/static/babylon/grid.png'
-
-        this.grid = new PlaneAsset(name, tileSize, materialName, textureURL)
-    }
-}  
+  }
+}
