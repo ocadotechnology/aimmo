c476da7add9db3d06bacc71d15a9640652bf3476
Niket <niket.shah1@ocado.com>
2020-05-07 15:13:18 +0100

    have a dummy version when avatar_api package is not being published
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo-game-worker/setup.py
+++ aimmo-game-worker/setup.py
@@ -2,17 +2,23 @@
 from setuptools import find_packages, setup
 import importlib.util
 
-version_spec = importlib.util.spec_from_file_location(
-    "aimmo_version", "../aimmo/__init__.py"
-)
-aimmo_version_module = importlib.util.module_from_spec(version_spec)
-version_spec.loader.exec_module(aimmo_version_module)
+
+def get_version():
+    try:
+        version_spec = importlib.util.spec_from_file_location(
+            "aimmo_version", "../aimmo/__init__.py"
+        )
+        aimmo_version_module = importlib.util.module_from_spec(version_spec)
+        version_spec.loader.exec_module(aimmo_version_module)
+        return aimmo_version_module.__version__
+    except Exception:
+        return "0.0.0"
 
 
 setup(
     name="aimmo-avatar-api",
     packages=find_packages("simulation"),
-    version=aimmo_version_module.__version__,
+    version=get_version(),
     include_package_data=True,
     tests_require=["httmock", "mock"],
     test_suite="tests",
