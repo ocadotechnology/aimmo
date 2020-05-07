27fc71a13f736ac54ed1fe21a30a0a16cd1ca374
Niket <niket.shah1@ocado.com>
2020-05-07 13:46:48 +0100

    make sure avatar_api setup.py can fetch correct version
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo-game-worker/setup.py
+++ aimmo-game-worker/setup.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
-import sys
-import os
+import importlib.util
 
+version_spec = importlib.util.spec_from_file_location(
+    "aimmo_version", "../aimmo/__init__.py"
+)
+aimmo_version_module = importlib.util.module_from_spec(version_spec)
+version_spec.loader.exec_module(aimmo_version_module)
 
-withcoverage = os.environ.get("WITH_COVERAGE")
 
 setup(
     name="aimmo-avatar-api",
     packages=find_packages("simulation"),
+    version=aimmo_version_module.__version__,
     include_package_data=True,
     tests_require=["httmock", "mock"],
     test_suite="tests",
-    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
