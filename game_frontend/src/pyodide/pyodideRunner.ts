edb21f7f1233981c081a13d7125e5b4841907fc3
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-05-15 13:37:10 +0100

    Handle globals

--- game_frontend/src/pyodide/pyodideRunner.ts
+++ game_frontend/src/pyodide/pyodideRunner.ts
@@ -1,8 +1,13 @@
-import * as pyodide from './pyodide'
-
 let avatarCode = `
+turns = 0
 def next_turn(world_state, avatar_state):
+    global turns
+    turns = turns + 1
+    print("I've had", turns, "turn(s)")
     return MoveAction(direction.NORTH)`
+
+let globals = []
+let globalFinder = /^(?!def\s|import\s)[\w\d]+\s*=\s*.*/gm
 
 export async function initialisePyodide () {
   await languagePluginLoader
@@ -23,10 +28,10 @@ export async function runAvatarCode (userCode, pyodideInitialised) {
   if (!pyodideInitialised) {
     return { action_type: 'wait' }
   }
-  if (avatarCode.includes('import')) {
-    console.log('Import cannot be used')
-    return { action_type: 'wait' }
-  }
+  let turn_globals = avatarCode.match(globalFinder)
+  await initialiseGlobals(turn_globals)
+
+  let globallessCode = avatarCode.replace(globalFinder, '')
 
   try {
     return Promise.race([
@@ -36,11 +41,20 @@ export async function runAvatarCode (userCode, pyodideInitialised) {
           resolve({ action_type: 'wait' })
         }, 2000)
       ),
-      await runTheCode(avatarCode)
+      await runTheCode(globallessCode)
     ])
   } catch (error) {
     console.log('python code incorrect')
     console.log(error)
+  }
+}
+
+async function initialiseGlobals (turn_globals) {
+  for (const turn_global of turn_globals) {
+    if (!globals.includes(turn_global)) {
+      await pyodide.runPythonAsync(turn_global)
+      globals.push(turn_global)
+    }
   }
 }
 
