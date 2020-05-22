8277b7c35f624d74759e265fd1d2bf5cf0269bbd
Niket <niket.shah1@ocado.com>
2020-05-22 15:02:38 +0100

    include avatar_api package in aimmo package and use that in the frontend
    Signed-off-by: Niket <niket.shah1@ocado.com>

---
+++ aimmo_runner/build_worker_wheel.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# build the wheel
+cd aimmo-game-worker
+python setup.py bdist_wheel
+cd ..
+
+# Copy wheel to django's static directory
+cp aimmo-game-worker/dist/aimmo_avatar_api-0.0.0-py3-none-any.whl aimmo/static/worker/aimmo_avatar_api-0.0.0-py3-none-any.whl
