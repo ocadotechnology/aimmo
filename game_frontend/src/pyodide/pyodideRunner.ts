567dbf385f5044b44aa46661018da20c079d7e72
Niket <niket.shah1@ocado.com>
2020-05-26 09:40:28 +0100

    make micropip install wheel from origin, not just localhost
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- game_frontend/src/pyodide/pyodideRunner.ts
+++ game_frontend/src/pyodide/pyodideRunner.ts
@@ -6,7 +6,7 @@ export async function initializePyodide () {
   await pyodide.runPythonAsync(`
   import micropip
 
-  micropip.install("http://localhost:8000/static/worker/aimmo_avatar_api-0.0.0-py3-none-any.whl")
+  micropip.install("${window.location.origin}/static/worker/aimmo_avatar_api-0.0.0-py3-none-any.whl")
   `)
 
   await pyodide.runPythonAsync(`
