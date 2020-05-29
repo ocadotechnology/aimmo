7656dee76af453fcf286de048a9ff137770cbcf9
Niket <niket.shah1@ocado.com>
2020-05-29 15:15:22 +0100

    Combine pyodide worker logs with game logs
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- game_frontend/src/pyodide/pyodideRunner.ts
+++ game_frontend/src/pyodide/pyodideRunner.ts
@@ -1,5 +1,11 @@
 import { defer } from 'rxjs'
 
+interface ComputedTurnResult {
+  action: object
+  log: string
+  turnCount: number
+}
+
 export async function initializePyodide () {
   await languagePluginLoader
   await pyodide.loadPackage(['micropip'])
@@ -15,27 +21,92 @@ from simulation import location
 from simulation.action import MoveAction, PickupAction, WaitAction
 from simulation.world_map import WorldMapCreator
 from simulation.avatar_state import AvatarState
+from io import StringIO
+import contextlib
+
+@contextlib.contextmanager
+def capture_output(stdout=None, stderr=None):
+    """Temporarily switches stdout and stderr to stringIO objects or variable."""
+    old_out = sys.stdout
+    old_err = sys.stderr
+
+    if stdout is None:
+        stdout = StringIO()
+    if stderr is None:
+        stderr = StringIO()
+    sys.stdout = stdout
+    sys.stderr = stderr
+    yield stdout, stderr
+
+    sys.stdout = old_out
+    sys.stderr = old_err
 `)
 }
 
-async function computeNextAction (gameState, avatarState) {
+async function computeNextAction (gameState, avatarState): Promise<ComputedTurnResult> {
   try {
+    // return Promise.resolve({ action: { action_type: 'wait' }, log: '', turnNumber: 0 })
     return await pyodide.runPythonAsync(`
 game_state = ${JSON.stringify(gameState)}
 world_map = WorldMapCreator.generate_world_map_from_game_state(game_state)
 avatar_state = AvatarState(**${JSON.stringify(avatarState)})
-next_turn(world_map, avatar_state).serialise()
-`)
+with capture_output() as output:
+    action = next_turn(world_map, avatar_state)
+    if action is None:
+      raise Exception("Make sure you are returning an action")
+    action.serialise()
+stdout, stderr = output
+logs = stdout.getvalue() + stderr.getvalue()
+{"action": action, "log": logs, "turnCount": game_state["turnCount"]}
+    `)
   } catch (error) {
-    console.warn('python code incorrect')
-    console.warn(error)
-    return { action_type: 'wait' }
+    return Promise.resolve({
+      action: { action_type: 'wait' },
+      log: simplifyErrorMessageInLog(error.toString()),
+      turnCount: gameState.turnCount + 1
+    })
   }
 }
 
-export const computeNextAction$ = (gameState, avatarState) =>
+export function simplifyErrorMessageInLog (log: string): string {
+  const regexToFindNextTurnErrors = /.*line (\d+), in next_turn\n((?:.|\n)*)/
+  const matches = log.match(regexToFindNextTurnErrors)
+  if (matches?.length >= 2) {
+    return `Uh oh! Something isn't correct on line ${matches[1]}. Here's the error we got:\n${matches[2]}`
+  }
+  return log
+    .split('\n')
+    .slice(-2)
+    .join('\n')
+}
+
+export const computeNextAction$ = (gameState: object, avatarState: object) =>
   defer(() => computeNextAction(gameState, avatarState))
 
-export async function updateAvatarCode (userCode) {
-  await pyodide.runPythonAsync(userCode)
+export async function updateAvatarCode (
+  userCode: string,
+  turnCount: number
+): Promise<ComputedTurnResult> {
+  try {
+    return await pyodide.runPythonAsync(userCode)
+  } catch (error) {
+    return Promise.resolve({
+      action: { action_type: 'wait' },
+      log: simplifyErrorMessageInLog(error.toString()),
+      turnCount: turnCount + 1
+    })
+  }
+  //   const regex = /^(?!\s*$)/gm
+  //   const indentedUserCode = userCode.replace(regex, ' '.repeat(4))
+  //   const logs = await pyodide.runPythonAsync(`
+  // with capture_output() as output:
+  // ${indentedUserCode}
+
+  // stdout, stderr = output
+  // output_log = stdout.getvalue()
+  // error_log = stderr.getvalue()
+  // {"output": output_log, "error": error_log}
+  //   `)
+
+  //   console.log(logs)
 }
--- game_frontend/src/redux/features/ConsoleLog/reducers.js
+++ game_frontend/src/redux/features/ConsoleLog/reducers.js
@@ -1,16 +1,31 @@
 import types from './types'
+import { avatarWorkerTypes } from 'redux/features/AvatarWorker'
 
 export const MAX_NUMBER_OF_STORED_LOGS = 600
 
-const consoleLogReducer = (state = { logs: [] }, action) => {
+const consoleLogReducer = (state = { logs: [], workerLogs: {} }, action) => {
   switch (action.type) {
     case types.SOCKET_CONSOLE_LOG_RECEIVED: {
-      let logs = [...state.logs, action.payload.log]
+      const logsFromGame = action.payload.log
+      const workerLog = state.workerLogs[logsFromGame.turn_count] ?? ''
+      if (workerLog) {
+        logsFromGame.message = `${workerLog}\n${logsFromGame.message}`
+      }
+      let logs = [...state.logs, logsFromGame]
       logs = logs.slice(-MAX_NUMBER_OF_STORED_LOGS)
 
       return {
         ...state,
         logs: logs
+      }
+    }
+    case avatarWorkerTypes.AVATAR_CODE_UPDATED:
+    case avatarWorkerTypes.AVATARS_NEXT_ACTION_COMPUTED: {
+      const workerLogs = state.workerLogs
+      workerLogs[action.payload.turnCount] = action.payload.log
+      return {
+        ...state,
+        workerLogs
       }
     }
     case types.CLEAR_CONSOLE_LOGS:
