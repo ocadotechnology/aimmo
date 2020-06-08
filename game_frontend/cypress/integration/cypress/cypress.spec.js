8c3e03fd924b3251dd3ce3160d803092a610511f
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-06-08 17:51:54 +0100

    Add wait action test

--- game_frontend/cypress/integration/cypress/cypress.spec.js
+++ game_frontend/cypress/integration/cypress/cypress.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import { DEFAULT_CODE } from '../../../src/redux/features/constants'
+import { SocketIO, Server } from 'mock-socket'
 
 describe('Cypress for aimmo', () => {
   // it('can login, add and delete games', () => {
@@ -26,6 +27,7 @@ describe('Cypress for aimmo', () => {
   //     codeOnServer: DEFAULT_CODE
   //   })
   // })
+  // http://localhost:8000/kurono/api/games/3/connection_parameters/
 
   it('changes direction', () => {
     cy.login()
@@ -33,16 +35,15 @@ describe('Cypress for aimmo', () => {
     cy.updateCode(DEFAULT_CODE)
 
     cy.visitAGame()
+    
+    cy.wait(5000)
 
-    let badCode = `def next_turn(world_state, avatar_state):
-return False`
+    cy.fixture("avatar_code").then(code => {
+      cy.window().its("store").invoke("dispatch", {type: "features/Editor/CHANGE_CODE", payload: code})
+    })
 
-    let editor = cy.get(".ace_text-input").first().focus()
-
-    editor.clear()
-    editor.type(badCode)
-
-    cy.get('#post-code-button').click()
+    cy.window().its("store").invoke("dispatch", {type: "features/Editor/POST_CODE_SUCCESS"})
+    cy.wait(5000)
 
     const store = cy.window().its('store').invoke('getState')
     const actionType = store.its('avatarAction').its('avatarAction')
