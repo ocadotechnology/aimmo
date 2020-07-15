c96e744dc5cefeecddbb78e2482c25470aba4275
Niket <niket.shah1@ocado.com>
2020-07-15 13:24:05 +0100

    Add tests for get_user_metadata
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo-game/tests/conftest.py
+++ aimmo-game/tests/conftest.py
@@ -1,14 +1,21 @@
 import os
 
 import pytest
+from aioresponses import aioresponses
 
-from service import setup_application, setup_socketIO_server, GameAPI
+from service import GameAPI, setup_application, setup_socketIO_server
 from simulation.game_runner import GameRunner
 from turn_collector import TurnCollector
 
 from .test_simulation.mock_avatar_manager import MockAvatarManager
 from .test_simulation.mock_communicator import MockCommunicator
 from .test_simulation.mock_game_state import MockGameState
+
+
+@pytest.fixture
+def mock_aioresponse():
+    with aioresponses() as mocked:
+        yield mocked
 
 
 @pytest.fixture
