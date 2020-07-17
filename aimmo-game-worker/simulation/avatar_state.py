558a5fbaa106f728485a088cb568936cbe31575c
Niket <niket.shah1@ocado.com>
2020-07-17 13:28:10 +0100

    Fix bug with avatar state returned to user
    Location was a dict instead of a class
    
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo-game-worker/simulation/avatar_state.py
+++ aimmo-game-worker/simulation/avatar_state.py
@@ -6,6 +6,6 @@ from typing import Dict
 def create_avatar_state(avatar_state_json: Dict):
     avatar_state_dict = avatar_state_json.copy()
     avatar_state_dict["location"] = Location(**avatar_state_json["location"])
-    return namedtuple("AvatarState", avatar_state_json.keys())(
-        *avatar_state_json.values()
+    return namedtuple("AvatarState", avatar_state_dict.keys())(
+        *avatar_state_dict.values()
     )
