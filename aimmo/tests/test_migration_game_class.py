8c97d9a60c5e6817d012f2d9afa14ca541ae80df
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-03 12:59:51 +0100

    Add class to game

---
+++ aimmo/tests/test_migration_game_class.py
@@ -0,0 +1,12 @@
+from .base_test_migration import MigrationTestCase
+
+
+class TestMigrationLinkWorksheetWithGame(MigrationTestCase):
+    start_migration = "0015_game_worksheet"
+    dest_migration = "0016_game_class"
+
+    def test_game_has_worksheet_as_foreign_key(self):
+        Game = self.django_application.get_model(self.app_name, "Game")
+
+        self.assertEquals(
+            Game._meta.get_field("game_class").get_internal_type(), "ForeignKey")
