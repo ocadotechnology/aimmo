8a683c34ce15520bfc21a920c59c38c504fe7a88
Niket <niket.shah1@ocado.com>
2020-08-12 14:44:24 +0100

    try tests without pyodide loaded check
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- game_frontend/cypress/support/commands.ts
+++ game_frontend/cypress/support/commands.ts
@@ -86,14 +86,14 @@ Cypress.Commands.add('loadGameWithAvatarCode', avatarCode => {
     .its('store')
     .invoke('dispatch', { type: 'features/AvatarWorker/INITIALIZE_PYODIDE' })
 
-  const getRunCodeButtonStatus = win => {
-    const state = win.store.getState()
-    return state.editor.runCodeButton.status
-  }
+  // const getRunCodeButtonStatus = win => {
+  //   const state = win.store.getState()
+  //   return state.editor.runCodeButton.status
+  // }
 
-  cy.window()
-    .pipe(getRunCodeButtonStatus, { timeout: 10000 })
-    .should('eq', 'done')
+  // cy.window()
+  //   .pipe(getRunCodeButtonStatus, { timeout: 10000 })
+  //   .should('eq', 'done')
 })
 //
 //
