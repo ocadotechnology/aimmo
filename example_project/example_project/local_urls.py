a529a4d54692e2a22cf1d33c1cca2018253cce0a
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-25 00:35:09 +0100

    Create local settings and urls and make Cypress use those

--- example_project/example_project/local_urls.py
+++ example_project/example_project/local_urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from game import urls as game_urls
-from portal import urls as portal_urls
+from django.views.generic.base import RedirectView
 
 from aimmo import urls as aimmo_urls
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r"^", include(portal_urls)),
     url(r"^administration/", include(admin.site.urls)),
-    url(r"^rapidrouter/", include(game_urls)),
     url(r"^kurono/", include(aimmo_urls)),
+    url(r"^$", RedirectView.as_view(url="/kurono")),
 ]
