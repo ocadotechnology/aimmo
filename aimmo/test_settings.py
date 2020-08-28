9fd03002eef9efd3734ab2d73a139f664a0796ca
Niket <niket.shah1@ocado.com>
2020-08-28 15:55:12 +0100

    fix test settings
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo/test_settings.py
+++ aimmo/test_settings.py
@@ -10,12 +10,19 @@ DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": db_name
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
+    "portal",
     "aimmo",
     "aimmo_runner",
 ]
 
+USE_I18N = True
+USE_L10N = True
+
+TIME_ZONE = "Europe/London"
+LANGUAGE_CODE = "en-gb"
+
 PIPELINE_ENABLED = False
-ROOT_URLCONF = "django_autoconfig.autourlconf"
+ROOT_URLCONF = "example_project.example_project.urls"
 STATIC_ROOT = ".tests_static/"
 SECRET_KEY = "test_key"
 GAME_SERVER_URL_FUNCTION = lambda game_id: ("base %s" % game_id, "path %s" % game_id)
