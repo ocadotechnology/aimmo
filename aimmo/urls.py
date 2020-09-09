2d5d39a7ab1f0b6128fdb5c562e7a4778db37ecb
Niket <niket.shah1@ocado.com>
2020-09-09 13:53:04 +0100

    Remove unused views
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo/urls.py
+++ aimmo/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.views.generic import RedirectView, TemplateView
 from django_js_reverse.views import urls_js
@@ -15,23 +14,6 @@ router.register(r"games", views.GameViewSet)
 
 
 urlpatterns = [
-    url(
-        r"^$",
-        login_required(TemplateView.as_view(template_name="players/home.html")),
-        name="kurono/home",
-    ),
-    url(r"^accounts/login/$", auth_views.login, name="kurono/login"),
-    url(
-        r"^accounts/logout/$",
-        auth_views.logout,
-        {"next_page": "kurono/logout_success"},
-        name="kurono/logout",
-    ),
-    url(
-        r"^accounts/logout_success/$",
-        TemplateView.as_view(template_name="registration/success_logout.html"),
-        name="kurono/logout_success",
-    ),
     url(
         r"^play/(?P<id>[0-9]+)/$", login_required(views.watch_game), name="kurono/play"
     ),
