df4a77d27e00da5dadc844a1d55c2377719fffac
Niket Shah <niket.shah1@ocado.com>
2020-09-25 12:00:55 +0100

    fix: typo in fetching game for starter code (#1401)
    * fix: read default starter code from worksheets
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * code review changes
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * merge development in
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Merge branch 'development' into Kurono-Starter-code-1398
    
    * fix: typo in fetching game for starter code
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Merge branch 'development' into Kurono-Starter-code-1398
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    # Conflicts:
    #	aimmo/game_creator.py

--- aimmo/game_creator.py
+++ aimmo/game_creator.py
@@ -39,7 +39,7 @@ def create_avatar_for_user(user, game_id):
     :param game_id: The id of the game in which the Avatar is created.
     :return: The initialised Avatar object.
     """
-    game: Game = Game.objects.get(id=1)
+    game: Game = Game.objects.get(id=game_id)
     initial_code = game.worksheet.starter_code
     avatar = Avatar.objects.create(owner=user, code=initial_code, game_id=game_id)
     return avatar
