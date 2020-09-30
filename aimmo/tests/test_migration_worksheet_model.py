dd8697e897dc950081c1d8947dbc55260ccf1c81
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2020-09-30 13:34:16 +0100

    feat: Add worksheet info to worksheet model (#1403)
    * update lock file
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * register Worksheet as a wagtail snippet
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * alter worksheet model
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Add image path + sort order to Worksheet model
    
    * Merge branch 'development' into Add-worksheet-info-to-worksheet-model-1400
    
    * Remove sort order
    
    * Add short description, active image and bring back sort order
    
    * Merge branch 'development' into Add-worksheet-info-to-worksheet-model-1400

--- aimmo/tests/test_migration_worksheet_model.py
+++ aimmo/tests/test_migration_worksheet_model.py
@@ -24,3 +24,38 @@ class TestMigrationAddWorksheetModel(MigrationTestCase):
         self.assertEquals(
             model._meta.get_field("starter_code").get_internal_type(), "TextField"
         )
+
+
+class TestMigrationAddInfoToWorksheetModel(MigrationTestCase):
+    start_migration = "0019_game_has_worksheet_and_class"
+    dest_migration = "0020_add_info_to_worksheet"
+
+    def test_worksheet_has_new_fields_added(self):
+        Worksheet = self.django_application.get_model(self.app_name, "Worksheet")
+
+        assert (
+            Worksheet._meta.get_field("description").get_internal_type() == "TextField"
+        )
+        assert (
+            Worksheet._meta.get_field("short_description").get_internal_type()
+            == "TextField"
+        )
+        assert (
+            Worksheet._meta.get_field("image_path").get_internal_type() == "CharField"
+        )
+        assert (
+            Worksheet._meta.get_field("active_image_path").get_internal_type()
+            == "CharField"
+        )
+        assert (
+            Worksheet._meta.get_field("thumbnail_image_path").get_internal_type()
+            == "CharField"
+        )
+        assert (
+            Worksheet._meta.get_field("thumbnail_text").get_internal_type()
+            == "CharField"
+        )
+        assert (
+            Worksheet._meta.get_field("sort_order").get_internal_type()
+            == "IntegerField"
+        )
