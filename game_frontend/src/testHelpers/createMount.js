b857b6773adae31d1c5a43d9727125b6d048d84c
Niket <niket.shah1@ocado.com>
2020-10-13 11:20:18 +0100

    Update styled-components
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- game_frontend/src/testHelpers/createMount.js
+++ game_frontend/src/testHelpers/createMount.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import { createShallow, createMount } from '@material-ui/core/test-utils'
+import { createMount } from '@material-ui/core/test-utils'
 import themeVariants from 'theme'
 import { ThemeProvider } from 'styled-components'
+import PropTypes from 'prop-types'
+
+const ThemeProviderWrapper = ({ children, theme }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+)
+
+ThemeProviderWrapper.propTypes = {
+  children: PropTypes.component,
+  theme: PropTypes.object
+}
 
 /**
  * Creates a component using the given vairant of our theme. this renders the component in a "headless browser",
@@ -11,12 +21,8 @@ import { ThemeProvider } from 'styled-components'
  * @param {boolean} dive
  */
 export default function createMountWithTheme (component, themeVariant = 'light', dive = false) {
-  const context = createShallow({ dive })(<ThemeProvider theme={themeVariants[themeVariant]} />)
-    .instance()
-    .getChildContext()
-
   return createMount({ dive })(component, {
-    context,
-    childContextTypes: ThemeProvider.childContextTypes
+    wrappingComponent: ThemeProviderWrapper,
+    wrappingComponentProps: { theme: themeVariants[themeVariant] }
   })
 }
