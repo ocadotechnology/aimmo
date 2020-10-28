5c1c8536f5848d88f3642d33eee2f68057a67044
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-10-28 17:40:15 +0000

    Fix test names

--- aimmo/tests/test_migration_remove_level_attempt.py
+++ aimmo/tests/test_migration_remove_level_attempt.py
@@ -1,11 +1,11 @@
 from .base_test_migration import MigrationTestCase
 
 
-class TestMigrationAddWorksheetModel(MigrationTestCase):
+class TestMigrationRemoveLevelAttemptModel(MigrationTestCase):
     start_migration = "0021_add_pdf_names_to_worksheet"
     dest_migration = "0022_remove_level_attempt"
 
-    def test_worksheet_model_exists(self):
+    def test_level_attempt_model_removed(self):
         model_names = [
             model._meta.db_table for model in self.django_application.get_models()
         ]
