8aceeafd1ffe78319cb26dd057c27498757b4425
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-11-10 14:29:02 +0000

    Dummy commit

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,8 +1,9 @@
-from aimmo import urls as aimmo_urls
 from django.conf.urls import include, url
 from django.contrib import admin
 from game import urls as game_urls
 from portal import urls as portal_urls
+
+from aimmo import urls as aimmo_urls
 
 admin.autodiscover()
 
