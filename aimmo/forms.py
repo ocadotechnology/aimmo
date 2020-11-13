30d85224055fc68a7149c563a3a9b4eb544baf1b
Niket <niket.shah1@ocado.com>
2020-11-13 09:56:19 +0000

    fix: Validate unique togther constraint in AddGameForm
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- aimmo/forms.py
+++ aimmo/forms.py
@@ -35,6 +35,13 @@ class AddGameForm(ModelForm):
             "start_width",
             "status",
         ]
+    
+    def full_clean(self) -> None:
+        super(AddGameForm, self).full_clean()
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            self._update_errors(e)
 
     def clean(self):
         game_class: Class = self.cleaned_data.get("game_class")
