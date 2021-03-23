5241faf086f80e63f6290ce5c337bf8c35af35e4
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2021-03-23 09:44:00 +0000

    fix: Fix worksheets not changing on game server (#1487)
    * Fix worksheets not changing on game server
    Fixes #1486
    
    * fix test
    
    * test game manager
    
    * code review updates
    
    * commented mock_game_manager

--- aimmo/tests/test_views.py
+++ aimmo/tests/test_views.py
@@ -440,7 +440,8 @@ class TestViews(TestCase):
         avatar = game.avatar_set.get(owner=client.session["_auth_user_id"])
         assert avatar is not None
 
-    def test_update_game_worksheet_updates_avatar_codes(self):
+    @patch("aimmo.serializers.GameManager")
+    def test_update_game_worksheet_updates_avatar_codes(self, mock_game_manager):
         # Set up the first avatar
         first_user = self.user
         models.Avatar(owner=first_user, code=self.CODE, game=self.game).save()
@@ -461,6 +462,8 @@ class TestViews(TestCase):
             content_type="application/json",
         )
 
+        # GameManager is called when a game is edited.
+        assert mock_game_manager.called
         assert response.status_code == 200
 
         game = models.Game.objects.get(id=1)
