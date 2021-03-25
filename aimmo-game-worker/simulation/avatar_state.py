94d43f16dd639d1deebb8c0962bd1747acfe1b4e
Dioni Zhong <dioni.zhong@ocado.com>
2021-03-25 17:55:58 +0000

    check backpack existence

--- aimmo-game-worker/simulation/avatar_state.py
+++ aimmo-game-worker/simulation/avatar_state.py
@@ -7,11 +7,13 @@ def create_avatar_state(avatar_state_json: Dict):
     avatar_state_dict = avatar_state_json.copy()
     avatar_state_dict["location"] = Location(**avatar_state_json["location"])
 
-    # use namedtuple for artefacts to allow accessing fields by name
-    avatar_state_dict["backpack"] = [
-        namedtuple("Artefact", artefact.keys())(*artefact.values())
-        for artefact in avatar_state_json["backpack"]
-    ]
+    # check backpack as it doesn't always exist (i.e. worksheet 1)
+    if avatar_state_json.get("backpack"):
+        # use namedtuple for artefacts to allow accessing fields by name
+        avatar_state_dict["backpack"] = [
+            namedtuple("Artefact", artefact.keys())(*artefact.values())
+            for artefact in avatar_state_json["backpack"]
+        ]
 
     return namedtuple("AvatarState", avatar_state_dict.keys())(
         *avatar_state_dict.values()
