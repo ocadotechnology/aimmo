e37e8511d54de5c268beb684c3e7a06238201d57
Dioni Zhong <dioni.zhong@ocado.com>
2021-03-29 15:31:01 +0100

    package rename and cleanup

--- aimmo-game-worker/setup.py
+++ aimmo-game-worker/setup.py
@@ -1,31 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
-import importlib.util
-
-
-def get_version():
-    try:
-        version_spec = importlib.util.spec_from_file_location(
-            "aimmo_version", "../aimmo/__init__.py"
-        )
-        aimmo_version_module = importlib.util.module_from_spec(version_spec)
-        version_spec.loader.exec_module(aimmo_version_module)
-        return aimmo_version_module.__version__
-    except Exception:
-        return "0.0.0"
-
 
 setup(
-    name="aimmo-avatar-api",
+    name="aimmo-game-worker",
     packages=find_packages(),
     version="0.0.0",
     include_package_data=True,
     tests_require=["pytest", "httmock", "mock"],
     setup_requires=["pytest-runner"],
     test_suite="tests",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
 )
