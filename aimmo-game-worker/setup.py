591382e8d7e20eeff039068a0318fa48e11ac32a
Dioni Zhong <dioni.zhong@ocado.com>
2021-03-29 15:04:24 +0100

    classifiers and python_requires for pypi info

--- aimmo-game-worker/setup.py
+++ aimmo-game-worker/setup.py
@@ -23,4 +23,9 @@ setup(
     tests_require=["pytest", "httmock", "mock"],
     setup_requires=["pytest-runner"],
     test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
