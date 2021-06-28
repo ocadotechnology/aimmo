a42cc18ac2f317f3c321ad11c92d0242f2ce410b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-06-28 16:02:02 +0100

    Update text on game load

--- game_frontend/src/components/GameView/index.js
+++ game_frontend/src/components/GameView/index.js
@@ -48,6 +48,8 @@ export const PositionedFindMeButton = styled(FindMeButton)`
   right: ${props => props.theme.spacing(3)}px;
 `
 
+const urlContact = (appData && appData.urlContact) ? appData.urlContact : ''
+
 export default class GameView extends Component {
   static propTypes = {
     connectToGame: PropTypes.func,
@@ -95,7 +97,10 @@ export default class GameView extends Component {
       <LoadingBackgroundOverlay>
         <CircularProgress color='inherit' />
         <LoadingText variant='body1' color='inherit'>
-          Building game world...
+          We are building your game... ⏱
+        </LoadingText>
+        <LoadingText variant='body1' color='inherit'>
+          If the game doesn’t load after 60 seconds, please <a href={urlContact}>contact us</a>.
         </LoadingText>
       </LoadingBackgroundOverlay>
     )
