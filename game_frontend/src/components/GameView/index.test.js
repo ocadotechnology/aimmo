b2ad7218f67f79f2e3a056f3ff88b0ffac8fdac4
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2021-07-06 16:46:43 +0100

    fix: Use Freshdesk widget in load screen (#1546)
    * fix: Use Freshdesk widget for contact us link instead
    
    * Update snapshot test
    
    * Use Freshdesk widget template
    
    * Update snapshot
    
    * Use styles instead of href
    
    * Update snapshot
    
    * Fix linting errors
    
    * Fix spacing
    
    * Handle widget opening in React
    
    * Declare widget
    
    * Update snapshot
    
    * Add missing snapshot test
    
    * Merge branch 'development' into load_text_link
    
    * Refactor loading text
    
    * Add unit test
    
    * Simplify onClick

--- game_frontend/src/components/GameView/index.test.js
+++ game_frontend/src/components/GameView/index.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 import React from 'react'
-import GameView, { GameViewLayout, Compass, LoadingBackgroundOverlay, PositionedFindMeButton, OverlayElements } from 'components/GameView'
+import GameView, { GameViewLayout, Compass, LoadingBackgroundOverlay, PositionedFindMeButton, OverlayElements, LoadingText } from 'components/GameView'
 import { shallow } from 'enzyme/build/index'
 import createMountWithTheme from 'testHelpers/createMount'
 import createShallowWithTheme from 'testHelpers/createShallow'
@@ -194,6 +194,20 @@ describe('<GameView />', () => {
     const component = shallow(<GameView {...props} />, { disableLifecycleMethods: true })
     expect(component).toMatchSnapshot()
   })
+
+  it('opens the Freshdesk widget when clicking on the contact us link in the loading screen', () => {
+    const connectToGame = jest.fn()
+    const props = {
+      connectToGame,
+      gameLoaded: false
+    }
+    const component = shallow(<GameView {...props} />, { disableLifecycleMethods: true })
+
+    window.FreshworksWidget = jest.fn()
+
+    component.find('.freshdesk__contact-us').simulate('click')
+    expect(window.FreshworksWidget).toHaveBeenCalled()
+  })
 })
 
 describe('<GameViewLayout />', () => {
@@ -227,6 +241,13 @@ describe('<PositionedFindMeButton/>', () => {
 describe('<LoadingBackgroundOverlay />', () => {
   it('matches snapshot', () => {
     const tree = createShallowWithTheme(<LoadingBackgroundOverlay />)
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('<LoadingText />', () => {
+  it('matches snapshot', () => {
+    const tree = createShallowWithTheme(<LoadingText />)
     expect(tree).toMatchSnapshot()
   })
 })
