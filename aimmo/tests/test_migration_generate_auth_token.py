fb4148843b55622e42b99858d9e54ae7ede4e7b7
Razvan Mahu <razvan.mahu@ocado.com>
2021-07-19 17:50:42 +0100

    add another test

--- aimmo/tests/test_migration_generate_auth_token.py
+++ aimmo/tests/test_migration_generate_auth_token.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_old_game_has_auth_token(migrator):
+def test_generate_auth_token_for_games(migrator):
     old_state = migrator.apply_initial_migration(
         ("aimmo", "0024_unique_class_per_game")
     )
@@ -17,3 +17,19 @@ def test_old_game_has_auth_token(migrator):
     Game = new_state.apps.get_model("aimmo", "Game")
     game = Game.objects.get(id=game_id)
     assert game.auth_token != "" and len(game.auth_token) > 0
+
+
+@pytest.mark.django_db
+def test_clear_auth_token_for_games(migrator):
+    old_state = migrator.apply_initial_migration(("aimmo", "0025_generate_auth_token"))
+
+    Game = old_state.apps.get_model("aimmo", "Game")
+    game = Game.objects.create()
+    game.save()
+    game_id = game.id
+
+    new_state = migrator.apply_tested_migration(("aimmo", "0024_unique_class_per_game"))
+
+    Game = new_state.apps.get_model("aimmo", "Game")
+    game = Game.objects.get(id=game_id)
+    assert game.auth_token == ""
