0fd2a4b81294071530e0704108a11bd4e671de9b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-11-23 19:26:24 +0000

    Refactor config

--- aimmo/autoconfig.py
+++ aimmo/autoconfig.py
@@ -1,5 +1,6 @@
 """aimmo autoconfig"""
-from common.app_settings import domain, MODULE_NAME
+
+from .csp_config import CSP_CONFIG
 
 DEFAULT_SETTINGS = {"AUTOCONFIG_INDEX_VIEW": "aimmo/home", "STATIC_URL": "/static/"}
 
@@ -27,27 +28,6 @@ SETTINGS = {
         }
     ],
     "USE_TZ": True,
-    # ----------------------------------------------------------------------------------
-    # CSP CONFIG
-    # ----------------------------------------------------------------------------------
-    "CSP_DEFAULT_SRC": ("'self'",),
-    "CSP_IMG_SRC": (
-        f"{domain()}/static/",
-        "https://p.typekit.net/",
-    ),
-    "CSP_FONT_SRC": (
-        "https://use.typekit.net/",
-    ),
-    "CSP_SCRIPT_SRC": (
-        "https://use.typekit.net/mrl4ieu.js",
-        "https://pyodide-cdn2.iodide.io/v0.15.0/full/",
-    ),
-    "CSP_CONNECT_SRC": (
-        "ws://localhost:41949/",
-        "ws://192.168.58.2:7959/socket.io/",
-        "http://192.168.58.2:7959/socket.io/",
-        "https://pyodide-cdn2.iodide.io/v0.15.0/full/",
-        f"wss://{MODULE_NAME}-aimmo.codeforlife.education/",
-        f"https://{MODULE_NAME}-aimmo.codeforlife.education/",
-    ),
 }
+
+SETTINGS.update(CSP_CONFIG)
---
+++ aimmo/csp_config.py
@@ -0,0 +1,26 @@
+"""CSP config"""
+
+from common.app_settings import domain, MODULE_NAME
+
+CSP_CONFIG = {
+    "CSP_DEFAULT_SRC": ("'self'",),
+    "CSP_IMG_SRC": (
+        f"{domain()}/static/",
+        "https://p.typekit.net/",
+    ),
+    "CSP_FONT_SRC": (
+        "https://use.typekit.net/",
+    ),
+    "CSP_SCRIPT_SRC": (
+        "https://use.typekit.net/mrl4ieu.js",
+        "https://pyodide-cdn2.iodide.io/v0.15.0/full/",
+    ),
+    "CSP_CONNECT_SRC": (
+        "ws://localhost:41949/",
+        "ws://192.168.58.2:7959/socket.io/",
+        "http://192.168.58.2:7959/socket.io/",
+        "https://pyodide-cdn2.iodide.io/v0.15.0/full/",
+        f"wss://{MODULE_NAME}-aimmo.codeforlife.education/",
+        f"https://{MODULE_NAME}-aimmo.codeforlife.education/",
+    ),
+}
