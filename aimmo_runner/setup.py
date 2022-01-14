b4001fc97ceb5bd7b579fb8dd33c5e1d0055da01
Florian Aucomte <f.aucomte@hotmail.co.uk>
2022-01-14 17:00:33 +0000

    fix: Upgrade Django and Django Rest Framework

--- aimmo_runner/setup.py
+++ aimmo_runner/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "django >= 1.8.3, < 1.9.0",
+        "django==2.2.26",
         "django-autoconfig >= 0.3.6, < 1.0.0",
         "django-forms-bootstrap",
         "django-js-reverse",
