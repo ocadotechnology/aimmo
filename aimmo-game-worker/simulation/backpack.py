d5a60830e0cc9c981031f5f93d85bd305412c525
Razvan Mahu <razvan.mahu@ocado.com>
2022-05-16 11:33:06 +0100

    code review

--- aimmo-game-worker/simulation/backpack.py
+++ aimmo-game-worker/simulation/backpack.py
@@ -7,12 +7,13 @@ T = TypeVar("T")
 
 class Backpack(List[T]):
     """
-    A list of artefacts that has a `find` method to find the index of an artefact type.
+    A list of artefacts that has a `find` method to find the first index of an artefact type.
     """
 
     def find(self, artefact_type: str) -> int:
         """
-        Finds an artefact of type `artefact_type` and returns its index or -1 if there's no artefact of that type.
+        Finds the first artefact of type `artefact_type` and returns its index or -1 if there's no artefact of that
+        type.
 
         Args:
             artefact_type: The type of artefact to find.
--- aimmo-game-worker/tests/tests_simulation/test_avatar_state.py
+++ aimmo-game-worker/tests/tests_simulation/test_avatar_state.py
@@ -9,10 +9,22 @@ def avatar_state_json():
     return {"location": {"x": 0, "y": 0}, "backpack": [{"type": "key"}]}
 
 
+@pytest.fixture
+def avatar_state_json_no_backpack():
+    return {"location": {"x": 1, "y": 1}}
+
+
 def test_create_avatar_state(avatar_state_json):
     avatar_state = create_avatar_state(avatar_state_json)
     avatar_loc = avatar_state_json["location"]
 
     assert avatar_state.location == Location(avatar_loc["x"], avatar_loc["y"])
-
     assert avatar_state.backpack[0].type == avatar_state_json["backpack"][0]["type"]
+
+
+def test_create_avatar_state_no_backpack(avatar_state_json_no_backpack):
+    avatar_state = create_avatar_state(avatar_state_json_no_backpack)
+    avatar_loc = avatar_state_json_no_backpack["location"]
+
+    assert avatar_state.location == Location(avatar_loc["x"], avatar_loc["y"])
+    assert not hasattr(avatar_state, "backpack")
