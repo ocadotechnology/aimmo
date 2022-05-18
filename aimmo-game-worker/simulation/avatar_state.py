bd51f2d2ae33acd2359481c18d42fec3d9c1c35f
KamilPawel <kamil.sosinski@ocado.com>
2022-05-18 14:50:51 +0100

    scale cars

--- aimmo-game-worker/simulation/avatar_state.py
+++ aimmo-game-worker/simulation/avatar_state.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
-from simulation.location import Location
 from typing import Dict
+
+from .backpack import Backpack
+from .location import Location
 
 
 def create_avatar_state(avatar_state_json: Dict):
@@ -8,13 +10,10 @@ def create_avatar_state(avatar_state_json: Dict):
     avatar_state_dict["location"] = Location(**avatar_state_json["location"])
 
     # check backpack as it doesn't always exist (i.e. worksheet 1)
-    if avatar_state_json.get("backpack"):
+    if "backpack" in avatar_state_json:
         # use namedtuple for artefacts to allow accessing fields by name
-        avatar_state_dict["backpack"] = [
-            namedtuple("Artefact", artefact.keys())(*artefact.values())
-            for artefact in avatar_state_json["backpack"]
-        ]
+        avatar_state_dict["backpack"] = Backpack(
+            [namedtuple("Artefact", artefact.keys())(*artefact.values()) for artefact in avatar_state_json["backpack"]]
+        )
 
-    return namedtuple("AvatarState", avatar_state_dict.keys())(
-        *avatar_state_dict.values()
-    )
+    return namedtuple("AvatarState", avatar_state_dict.keys())(*avatar_state_dict.values())
