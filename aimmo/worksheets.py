79412f05c598480ec404e75059d40da473bc752c
KamilPawel <kamil.sosinski@ocado.com>
2022-05-18 14:04:16 +0100

    edit the starter code

--- aimmo/worksheets.py
+++ aimmo/worksheets.py
@@ -189,7 +189,14 @@ def next_turn(world_state, avatar_state):
 #
 #  New commands:
 #       - DropAction(index)
-#       - backpack.find(ARTEFACT)
+#       - backpack.find(ARTEFACT_TYPE)
+#
+#  Previous commands:
+#       - world_state.scan_nearby() -> LIST OF ARTEFACTS
+#       - MoveTowardsAction(ARTEFACT)
+#       - avatar_state.backpack -> LIST OF HELD ARTEFACTS
+#       - artefact.type -> TYPE (e.g. artefact_types.KEY or artefact_types.CHEST)
+
 #-------------------------------------------------------------------------------
 
 def next_turn(world_state, avatar_state):
@@ -261,19 +268,11 @@ def next_turn(world_state, avatar_state):
 
 
 def get_complete_worksheets() -> List[Worksheet]:
-    return [
-        worksheet
-        for worksheet in WORKSHEETS.values()
-        if worksheet.thumbnail_text != "Coming Soon"
-    ]
+    return [worksheet for worksheet in WORKSHEETS.values() if worksheet.thumbnail_text != "Coming Soon"]
 
 
 def get_incomplete_worksheets() -> List[Worksheet]:
-    return [
-        worksheet
-        for worksheet in WORKSHEETS.values()
-        if worksheet.thumbnail_text == "Coming Soon"
-    ]
+    return [worksheet for worksheet in WORKSHEETS.values() if worksheet.thumbnail_text == "Coming Soon"]
 
 
 def get_worksheets_excluding_id(id: int) -> List[Worksheet]:
