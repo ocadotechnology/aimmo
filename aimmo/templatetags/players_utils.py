3ceedb05e4410f849875f372be46e353ceaa8d49
KamilPawel <70412950+KamilPawel@users.noreply.github.com>
2022-10-13 16:36:44 +0100

    fix: fix created_by migration field (#1713)
    * :(
    
    * fix: fix migrations
    
    * fix: if statement
    
    * YES
    
    * this should work

--- aimmo/templatetags/players_utils.py
+++ aimmo/templatetags/players_utils.py
@@ -1,7 +1,7 @@
 from aimmo.models import Game
 from django import template
 
-from common.permissions import logged_in_as_student, logged_in_as_teacher
+from common.permissions import logged_in_as_teacher
 
 register = template.Library()
 
@@ -9,10 +9,13 @@ register = template.Library()
 def get_user_playable_games(context, base_url):
     # Only called by teacher to create games table
     user = context.request.user
+    teacher = user.new_teacher
     if logged_in_as_teacher(user):
-        playable_games = Game.objects.filter(owner=user, is_archived=False).union(
-            Game.objects.filter(game_class__teacher__school=user.userprofile.teacher.school, is_archived=False),
-        )
+        playable_games = list(Game.objects.filter(owner=user, is_archived=False))
+        if teacher.is_admin:
+            playable_games += list(
+                Game.objects.filter(game_class__teacher__school=teacher.school, is_archived=False).exclude(owner=user)
+            )
     else:
         playable_games = Game.objects.none()
     return {
