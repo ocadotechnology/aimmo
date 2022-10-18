419960200a835c2c51ee7af3ac3cb69e78517e25
faucomte97 <florian.aucomt1@ocado.com>
2022-10-18 22:27:56 +0100

    fix: Update deprecated K8s APIs

--- aimmo-game-creator/setup.py
+++ aimmo-game-creator/setup.py
@@ -10,7 +10,7 @@ setup(
     name="aimmo-game-creator",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["eventlet", "kubernetes >= 6.0.0"],
+    install_requires=["eventlet"],
     tests_require=["httmock"],
     test_suite="tests",
     zip_safe=False,
