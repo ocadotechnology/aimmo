d2220d610a1d1c23d6707a61de6d97f257b934b7
KamilPawel <ks6g18@soton.ac.uk>
2023-03-07 16:44:41 +0000

    lower case

--- game_frontend/src/components/ConsoleBar/index.js
+++ game_frontend/src/components/ConsoleBar/index.js
@@ -2,11 +2,8 @@ import styled from 'styled-components'
 import Toolbar from '@material-ui/core/Toolbar/Toolbar'
 import { Typography } from '@material-ui/core'
 import ConsoleIcon from 'components/icons/Console'
-import ClearIcon from 'components/icons/Clear'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-import { MenuButton } from 'containers/IDEEditor'
 
 export const StyledConsoleBar = styled(Toolbar)`
   background-color: ${(props) => props.theme.palette.grey.A700};
@@ -20,12 +17,6 @@ export const StyledConsoleTitle = styled.div`
 
 export const StyledConsoleIcon = styled(ConsoleIcon)`
   padding-right: ${(props) => props.theme.spacing()}px;
-`
-export const ClearButton = styled(MenuButton)`
-  position: absolute;
-  z-index: 10;
-  top: 23vh;
-  right: 0vw;
 `
 
 export default class ConsoleBar extends Component {
@@ -42,13 +33,6 @@ export default class ConsoleBar extends Component {
             Console Log
           </Typography>
         </StyledConsoleTitle>
-        <ClearButton
-          variant="outlined"
-          onClick={this.props.clearConsoleClicked}
-          startIcon={<ClearIcon />}
-        >
-          Clear console
-        </ClearButton>
       </StyledConsoleBar>
     )
   }
