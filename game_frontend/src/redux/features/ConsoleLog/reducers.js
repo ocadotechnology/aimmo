8a1e4d242cc8e36f9ee2adc3147c0ad41b73fbeb
KamilPawel <ks6g18@soton.ac.uk>
2023-03-07 10:21:45 +0000

    implemented razvan command into run.py runner

--- game_frontend/src/redux/features/ConsoleLog/reducers.js
+++ game_frontend/src/redux/features/ConsoleLog/reducers.js
@@ -27,7 +27,6 @@ const consoleLogReducer = (state = { logs: new Map(), gameLog: '' }, action) => 
     }
     case avatarWorkerTypes.AVATAR_CODE_UPDATED:
     case avatarWorkerTypes.AVATARS_NEXT_ACTION_COMPUTED: {
-      console.log(state)
       const gameLog = state.gameLog
       const turnCount = action.payload.turnCount
       const newLogMessage = createNewLogMessage(action.payload.log, gameLog)
