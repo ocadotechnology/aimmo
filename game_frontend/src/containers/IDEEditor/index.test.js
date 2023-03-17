ae58cd2d790a976477b846bc423c3de60adcda90
Chi-En Wei <chi-en.wei@ocado.com>
2023-03-17 14:43:55 +0000

    add tests to IDEEditor

--- game_frontend/src/containers/IDEEditor/index.test.js
+++ game_frontend/src/containers/IDEEditor/index.test.js
@@ -65,6 +65,30 @@ describe('<IDEEditor />', () => {
     component.find('#game-pause-button').simulate('click')
     expect(props.togglePauseGame).toBeCalled()
   })
+
+  it('gameResumed called after clicking post code', () => {
+    const props = {
+      getCode: jest.fn(),
+      postCode: jest.fn(),
+      gameResume: jest.fn()
+    }
+
+    const component = createShallowWithTheme(<IDEEditor {...props} />, 'dark')
+
+    component.instance().postCode();
+    expect(props.gameResume).toBeCalled()
+  })
+
+  it('gameResumed called after clicking post code', () => {
+    const props = {
+      getCode: jest.fn(),
+    }
+
+    const component = createShallowWithTheme(<IDEEditor {...props} />, 'dark')
+
+    component.instance().codeChanged("someCode");
+    expect(component.state('code')).toEqual("someCode");
+  })
 })
 
 describe('<IDEEditorLayout />', () => {
